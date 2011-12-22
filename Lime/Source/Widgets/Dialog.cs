e7508d449b8baeb26691c75dfe3f4d2c1f8ebdec
M.Rojkov <M.Rojkov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2011-12-22 00:21:58 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@166111 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

--- Lime/Source/Widgets/Dialog.cs
+++ Lime/Source/Widgets/Dialog.cs
@@ -2,12 +2,21 @@
 {
 	public class Dialog : Frame
 	{
+		static Vector2 MouseHole = new Vector2 (-100000, -100000);
+
 		public override void Update (int delta)
 		{
+			if (worldShown && !Input.MousePosition.Equals (MouseHole)) {
+				if (Widget.ActiveWidget != null && !Widget.ActiveWidget.ChildOf (this)) {
+					// Discard active widget if it's not a child of the topmost dialog.
+					Widget.ActiveWidget = null;
+				}
+			}
 			base.Update (delta);
 			if (worldShown) {
+				// Cosume all input events and drive mouse into the hole.
 				Input.ConsumeAllKeyEvents (true);
-				Input.HideMouseAway (true);
+				Input.MousePosition = MouseHole;
 			}
 		}
 	}
