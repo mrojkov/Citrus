0eacac858a4e4390b17a1cf4c6a8bb95427810c3
M.Rojkov <M.Rojkov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2012-01-23 02:44:30 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@166451 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ Lime/Source/Locale.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Lime
+{
+	public class LocalizationDictionary
+	{
+		Dictionary<int, string> strings = new Dictionary<int, string>();
+
+		public LocalizationDictionary()
+		{
+		}
+
+		public LocalizationDictionary(Stream stream)
+		{
+			ReadFromStream(stream);
+		}
+
+		public LocalizationDictionary(string file)
+		{
+			using (var stream = AssetsBundle.Instance.OpenFile(file)) {
+				ReadFromStream(stream);
+			}
+		}
+
+		public void Add(int tag, string value)
+		{
+			strings[tag] = value;
+		}
+
+		public bool Contains(int tag)
+		{
+			return strings.ContainsKey(tag);
+		}
+
+		public bool TryGetString(int tag, out string value)
+		{
+			return strings.TryGetValue(tag, out value);
+		}
+
+		public void ReadFromStream(Stream stream)
+		{
+		}
+
+		public void WriteToStream(Stream stream)
+		{
+			using (var w = new StreamWriter(stream)) {
+				foreach(KeyValuePair<int, string> p in strings) {
+					w.WriteLine(string.Format("[{0}]", p.Key));
+					w.WriteLine(p.Value);
+					w.WriteLine();
+				}
+			}
+		}
+	}
+
+	public static class Locale
+	{
+		public static LocalizationDictionary Dictionary = new LocalizationDictionary();
+
+		public static string GetString(string taggedString)
+		{
+			if (string.IsNullOrEmpty(taggedString))
+				return taggedString;
+			if (taggedString[0] == '[') {
+				int closeBrackedPos = 0;
+				for (int i = 1; i < taggedString.Length; i++) {
+					if (taggedString[i] == ']') {
+						closeBrackedPos = i;
+						break;
+					}
+					if (!char.IsDigit(taggedString, i)) {
+						break;
+					}
+				}
+				if (closeBrackedPos >= 1) {
+					string text;
+					if (closeBrackedPos > 1) {
+						int key = ParseInt(taggedString, 1, closeBrackedPos - 1);
+						if (Dictionary.TryGetString(key, out text)) {
+							return text;
+						}
+					}
+					// key/value pair not defined or key is empty ("[]" case).
+					text = taggedString.Substring(closeBrackedPos + 1);
+					return text;
+				}
+			}
+			return taggedString;
+		}
+
+		static int ParseInt(string source, int index, int length)
+		{
+			int v = 0;
+			for (int i = index; i < index + length; i++) {
+				v *= 10;
+				v += (int)source[i] - (int)'0';
+			}
+			return v;
+		}
+	}
+}
---
+++ Orange/Source/DictionaryExtractor.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using Lime;
+
+namespace Orange
+{
+	public class DictionaryExtractor
+	{
+		private CitrusProject project;
+
+		public DictionaryExtractor(CitrusProject project)
+		{
+			this.project = project;
+		}
+		
+		public void ExtractDictionary()
+		{
+			using (new DirectoryChanger(project.AssetsDirectory)) {
+				Console.WriteLine("------------- Localization dictionary update started -------------");
+				var files = Helpers.GetAllFiles(".", "*.scene", true);
+				foreach (string file in files) {
+					Console.WriteLine("* " + file);
+					ProcessSceneFile(file);
+				}
+			}
+		}
+
+		string TagString(string str)
+		{
+			var match = Regex.Match(str, @"^\[(\d*)\](.*)$");
+			if (match.Success) {
+				if (match.Groups[1].Length > 0) {
+					// case of "[123]..."
+					int tag = int.Parse(match.Groups[1].Value);
+					string value = match.Groups[2].Value;
+					if (!Locale.Dictionary.Contains(tag)) {
+						Locale.Dictionary.Add(tag, value);
+					}
+					return str;
+				} else {
+					// case of "[]..."
+					int tag = GenerateTag();
+					string value = match.Groups[2].Value;
+					Locale.Dictionary.Add(tag, value);
+					str = string.Format("[{0}]{1}", tag, value);
+					return str;
+				}
+			} else {
+				int tag = GenerateTag();
+				Locale.Dictionary.Add(tag, str);
+				str = string.Format("[{0}]{1}", tag, str);
+				return str;
+			}
+		}
+
+		int GenerateTag()
+		{
+			string s;
+			for (int tag = 1; ; tag++) {
+				if (!Locale.Dictionary.TryGetString(tag, out s))
+					return tag;
+			}
+		}
+
+		void ProcessSceneFile(string file)
+		{
+			var text = File.ReadAllText(file);
+			var s = Regex.Replace(text, @"^(\s*Text)\s""([^""]*)""$", 
+				(match) => {
+					string prefix = match.Groups[1].Value;
+					string str = match.Groups[2].Value;
+					str = TagString(str);
+					string result = string.Format(@"{0} ""{1}""", prefix, str);
+					return result;
+				}, RegexOptions.Multiline);
+			File.WriteAllText(file + "X", s);
+		}
+	}
+}
