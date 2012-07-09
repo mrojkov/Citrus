a113263c45617f126b0b9b8efc6f71ef710aa8a6
M.Rojkov <M.Rojkov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2012-07-09 00:11:16 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@170796 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ Lime/Source/Widgets/TimeWaiter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lime
+{
+	public class TimeWaiter : Node
+	{
+		private float time;
+		private Event evt;
+
+		public TimeWaiter(float time, Event evt)
+		{
+			this.evt = evt;
+			this.time = time;
+		}
+
+		public override void Update(int delta)
+		{
+			if ((time -= delta * 0.001f) < 0) {
+				evt();
+				Unlink();
+			}
+		}
+
+		public static TimeWaiter Create(Node parent, float time, Event evt)
+		{
+			var trigger = new TimeWaiter(time, evt);
+			parent.Nodes.Add(trigger);
+			return trigger;
+		}
+	}
+}
