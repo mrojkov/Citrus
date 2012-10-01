63740ba6d9665fcd062b3dbe96ea2f39e845fc91
M.Rojkov <M.Rojkov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2012-10-01 06:49:06 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@174526 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ Lime/Source/Application/Logger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+#if iOS
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+#endif
+
+namespace Lime
+{
+	public class Logger
+	{
+#if iOS
+		[DllImport(MonoTouch.Constants.FoundationLibrary)]
+		private extern static void NSLog(IntPtr message);
+
+		public static void Write(string msg)
+		{
+			using (var nss = new NSString(msg)) {
+				NSLog(nss.Handle);
+			}
+		}
+
+		public static void Write(string msg, params object[] args)
+		{
+			Log(string.Format(msg, args));
+		}
+#else
+		public static void Write(string msg)
+		{
+			Console.WriteLine(msg);
+		}
+
+		public static void Write(string msg, params object[] args)
+		{
+			Console.WriteLine(msg, args);
+		}
+#endif
+	}
+}
--- Lime/Source/Application/iOSAppDelegate.cs
+++ Lime/Source/Application/iOSAppDelegate.cs
@@ -9,19 +9,6 @@ using System.Runtime.InteropServices;
 
 namespace Lime
 {
-	public class Logger
-	{
-	    [DllImport(MonoTouch.Constants.FoundationLibrary)]
-	    private extern static void NSLog(IntPtr message);
-
-	    public static void Log(string msg, params object[] args)
-	    {
-	        using (var nss = new NSString(string.Format(msg, args))) {
-	            NSLog(nss.Handle);
-	        }
-	    }
-	}
-
 	/// <summary>
 	/// The UIApplicationDelegate for the application. This class is responsible for launching the 
 	/// User Interface of the application, as well as listening(and optionally responding) to 
