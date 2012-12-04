5e39d9e0c8ca3bb723730f7ccfb0b02587b84edd
atuzhik <atuzhik@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2012-12-04 04:33:56 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@175984 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ Kumquat/Source/Area.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Lime;
+using ProtoBuf;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Kumquat
+{
+	[ProtoContract]
+	public class Area : Frame
+	{
+		[ProtoMember(1)]
+		public string CursorName = "";
+
+		[ProtoMember(2)]
+		public string Tools = "";
+	}
+}
---
+++ Kumquat/Source/ExitArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Lime;
+using ProtoBuf;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Kumquat
+{
+	[ProtoContract]
+	public class ExitArea : Area
+	{
+		[ProtoMember(1)]
+		public string ExitTo = "";
+	}
+}
