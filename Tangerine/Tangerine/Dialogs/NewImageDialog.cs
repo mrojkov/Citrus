c8670a891db11faa35eabc09381a207e2026a1d0
M.Rojkov <M.Rojkov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2012-12-06 22:12:43 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@176205 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ Tangerine/Tangerine/Dialogs/NewImageDialog.cs
@@ -0,0 +1,58 @@
+// 
+// NewImageDialog.cs
+//  
+// Author:
+//       Jonathan Pobst <monkey@jpobst.com>
+// 
+// Copyright (c) 2010 Jonathan Pobst
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using Pinta.Core;
+using Gtk;
+
+namespace Pinta
+{
+	public partial class NewImageDialog : Gtk.Dialog
+	{
+		public NewImageDialog () : base (string.Empty, PintaCore.Chrome.MainWindow, DialogFlags.Modal)
+		{
+			this.Build ();
+
+			this.Icon = PintaCore.Resources.GetIcon (Stock.New, 16);
+			DefaultResponse = ResponseType.Ok;
+			AlternativeButtonOrder = new int[] { (int) ResponseType.Ok, (int) ResponseType.Cancel };
+
+			widthSpinner.ActivatesDefault = true;
+			heightSpinner.ActivatesDefault = true;
+		}
+
+		public int NewImageWidth {
+			get { return widthSpinner.ValueAsInt; }
+			set { widthSpinner.Value = value; }
+		}
+
+		public int NewImageHeight {
+			get { return heightSpinner.ValueAsInt; }
+			set { heightSpinner.Value = value; }
+		}
+	}
+}
+
---
+++ Tangerine/Tangerine/Main.cs
@@ -0,0 +1,169 @@
+// 
+// Main.cs
+//  
+// Author:
+//       Jonathan Pobst <monkey@jpobst.com>
+// 
+// Copyright (c) 2010 Jonathan Pobst
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using Gtk;
+using Mono.Options;
+using System.Collections.Generic;
+using Pinta.Core;
+using Mono.Unix;
+using System.IO;
+using System.Reflection;
+
+namespace Pinta
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string app_dir = Path.GetDirectoryName (Assembly.GetExecutingAssembly ().Location);
+			string locale_dir;
+			bool devel_mode = File.Exists (Path.Combine (Path.Combine (app_dir, ".."), "Pinta.sln"));
+			
+			if (SystemManager.GetOperatingSystem () != OS.X11 || devel_mode)
+				locale_dir = Path.Combine (app_dir, "locale");
+			else {
+				// From MonoDevelop:
+				// Pinta is located at $prefix/lib/pinta
+				// adding "../.." should give us $prefix
+				string prefix = Path.Combine (Path.Combine (app_dir, ".."), "..");
+				//normalise it
+				prefix = Path.GetFullPath (prefix);
+				//catalog is installed to "$prefix/share/locale" by default
+				locale_dir = Path.Combine (Path.Combine (prefix, "share"), "locale");
+			}
+
+			try {
+				Catalog.Init ("pinta", locale_dir);
+			} catch (Exception ex) {
+				Console.WriteLine (ex);
+			}
+
+			int threads = -1;
+			
+			var p = new OptionSet () {
+				{ "rt|render-threads=", Catalog.GetString ("number of threads to use for rendering"), (int v) => threads = v }
+			};
+
+			List<string> extra;
+			
+			try {
+				extra = p.Parse (args);
+			} catch (OptionException e) {
+				Console.Write ("Pinta: ");
+				Console.WriteLine (e.Message);
+				return;
+			}
+
+			GLib.ExceptionManager.UnhandledException += new GLib.UnhandledExceptionHandler (ExceptionManager_UnhandledException);
+			
+			Application.Init ();
+			// var theme = Gtk.Settings.Default.ThemeName;
+			//Gtk.Settings.Default.ThemeName = "Aero-ion3.1";
+
+			MainWindow win = new MainWindow ();
+			//win.Show ();
+			
+			if (threads != -1)
+				Pinta.Core.PintaCore.System.RenderThreads = threads;
+
+			if (SystemManager.GetOperatingSystem () == OS.Mac) {
+				RegisterForAppleEvents ();
+			}
+
+			OpenFilesFromCommandLine (extra);
+			
+			Application.Run ();
+		}
+
+		private static void OpenFilesFromCommandLine (List<string> extra)
+		{
+			// Ignore the process serial number parameter on Mac OS X
+			if (PintaCore.System.OperatingSystem == OS.Mac && extra.Count > 0)
+			{
+				if (extra[0].StartsWith ("-psn_"))
+				{
+					extra.RemoveAt (0);
+				}
+			}
+
+			if (extra.Count > 0)
+			{
+				throw new NotImplementedException();
+				foreach (var file in extra)
+					PintaCore.Workspace.OpenFile (file);
+			}
+			else
+			{
+				// Create a blank document
+				PintaCore.Workspace.NewDocument (new Gdk.Size (800, 600), false);
+			}
+		}
+
+		private static void ExceptionManager_UnhandledException (GLib.UnhandledExceptionArgs args)
+		{
+			ErrorDialog errorDialog = new ErrorDialog (null);
+			
+			Exception ex = (Exception)args.ExceptionObject;
+			
+			try {
+				errorDialog.Message = string.Format ("{0}:\n{1}", "Unhandled exception", ex.Message);
+				errorDialog.AddDetails (ex.ToString (), false);
+				errorDialog.Run ();
+			} finally {
+				errorDialog.Destroy ();
+			}
+		}
+
+		/// <summary>
+		/// Registers for OSX-specific events, like quitting from the dock.
+		/// </summary>
+		static void RegisterForAppleEvents ()
+		{
+			MacInterop.ApplicationEvents.Quit += (sender, e) => {
+				GLib.Timeout.Add (10, delegate {
+					PintaCore.Actions.File.Exit.Activate ();
+					return false;
+				});
+				e.Handled = true;
+			};
+
+			MacInterop.ApplicationEvents.OpenDocuments += (sender, e) => {
+				if (e.Documents != null) {
+					GLib.Timeout.Add (10, delegate {
+						foreach (string filename in e.Documents.Keys) {
+							System.Console.Error.WriteLine ("Opening: {0}", filename);
+							throw new NotImplementedException();
+							// PintaCore.Workspace.OpenFile (filename);
+						}
+						return false;
+					});
+				}
+				e.Handled = true;
+			};
+		}
+	}
+}
