5dcdc06500cce9d461805de5d118a9b53dd708a9
atuzhik <atuzhik@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2012-12-21 06:19:22 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@177147 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ Orange/Source/KumquatCooker/KumquatCooker.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Kumquat;
+
+namespace Orange
+{
+	using LocDict = Dictionary<string, Lime.Frame>;
+
+	public class KumquatCooker
+	{
+		private Lime.AssetsBundle assetsBundle = Lime.AssetsBundle.Instance;
+		private CitrusProject project;
+		private Orange.TargetPlatform platform;
+
+		public KumquatCooker(CitrusProject project, Orange.TargetPlatform platform)
+		{
+			this.project = project;
+			this.platform = platform;
+		}
+
+		public void Cook()
+		{
+			string bundlePath = Path.ChangeExtension(project.AssetsDirectory, Helpers.GetTargetPlatformString(platform));
+			assetsBundle.Open(bundlePath, Lime.AssetBundleFlags.Writable);
+			try {
+				CookLocations();
+			} finally {
+				assetsBundle.Close();
+			}
+		}
+
+		private void CookLocations()
+		{
+			LocDict locations = new LocDict();
+			var locationPaths = GetLocationPaths();
+			foreach (var path in locationPaths)
+				locations.Add(path, new Lime.Frame(path));
+
+			if (locations.Count > 0) {
+				Lime.Serialization.WriteObjectToBundle<Router>(assetsBundle, "Router", new Router(locations));
+			}
+
+		}
+		
+		private List<string> GetLocationPaths()
+		{
+			var cookingRulesMap = CookingRulesBuilder.Build(project.AssetFiles);
+			List<string> list = new List<string>();
+			foreach (var srcFileInfo in project.AssetFiles.Enumerate(".scene")) {
+				CookingRules rules = cookingRulesMap[srcFileInfo.Path];
+				if (rules.KumquatLocation)
+					list.Add(srcFileInfo.Path);
+			}
+			return list;
+		}
+
+	}
+}
