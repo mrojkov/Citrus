6620f5b3b68b8a9af34e16ca3f767f24cc543e6b
M.Rojkov <M.Rojkov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2012-12-22 01:00:02 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@177155 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ TangerineQT/Toolbox/MemoryLeaksChecker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangerine
+{
+	public class MemoryLeaksChecker : IDisposable
+	{
+		long initialAllocated;
+
+		public MemoryLeaksChecker()
+		{
+			System.GC.GetTotalMemory(true);
+			initialAllocated = System.GC.GetTotalMemory(true);
+		}
+
+		void IDisposable.Dispose()
+		{
+			System.GC.GetTotalMemory(true);
+			long leaked = System.GC.GetTotalMemory(true) - initialAllocated;
+			Console.WriteLine("Leaked {0} bytes", leaked);
+		}
+	}
+}
---
+++ TangerineQT/Toolbox/QyotoBooster.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangerine
+{
+	/// <summary>
+	/// Этот класс содержит функции, ускоряющие отрисовку через Qyoto.
+	/// По результатам бенчмарка оверхед на вызов функции уменьшается в 2 раза.
+	/// </summary>
+	public class QyotoBooster
+	{
+		//[DllImport("qyoto-qtcore-native", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+		//static extern ModuleIndex FindMethodId(string className, string mungedName, string signature);
+
+		//[DllImport("qyoto-qtcore-native", CharSet = CharSet.Ansi, EntryPoint = "CallSmokeMethod", CallingConvention = CallingConvention.Cdecl)]
+		//static extern void CallSmokeMethod(IntPtr smoke, int methodId, IntPtr target, IntPtr sp, int items);
+
+		//ModuleIndex drawLineIndex;
+
+		//private void LinedrawingBenchmark(QPainter ptr)
+		//{
+		//	long t = System.Environment.TickCount;
+		//	for (int i = 0; i < 30000; i++) {
+		//		ptr.DrawLine(0, 0, 0, 0);
+		//	}
+		//	t = System.Environment.TickCount - t;
+		//	Console.WriteLine("T1: " + t);
+		//	t = System.Environment.TickCount;
+		//	GCHandle instanceHandle = GCHandle.Alloc(ptr);
+		//	unsafe {
+		//		StackItem* si = stackalloc StackItem[5];
+		//		for (int i = 0; i < 30000; i++) {
+		//			if (drawLineIndex.index == 0) {
+		//				drawLineIndex = FindMethodId("QPainter", "drawLine$$$$", "drawLine(int, int, int, int)");
+		//			}
+		//			si[1].s_int = 0;
+		//			si[2].s_int = 0;
+		//			si[3].s_int = 0;
+		//			si[4].s_int = 0;
+		//			CallSmokeMethod(drawLineIndex.smoke, drawLineIndex.index, (IntPtr)instanceHandle, (IntPtr)si, 4);//(IntPtr)stackPtr, stack.Length);
+		//		}
+		//	}
+		//	t = System.Environment.TickCount - t;
+		//	Console.WriteLine("T2: " + t);
+		//	instanceHandle.Free();
+		//}
+	}
+}
