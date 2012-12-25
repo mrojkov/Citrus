f44cfee72340283965ce7784027bc128ef902865
atuzhik <atuzhik@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2012-12-25 06:58:22 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@177431 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ Kumquat/Source/Goals.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProtoBuf;
+
+namespace Kumquat
+{
+	[ProtoContract]
+	public class Goals
+	{
+		[ProtoContract]
+		public class Record
+		{
+			[ProtoMember(1)]
+			public Enum Goal;
+
+			[ProtoMember(2)]
+			public bool IsStrike = false;
+
+			public Record(Enum goal)
+			{
+				Goal = goal;
+			}
+		}
+
+		[ProtoMember(1)]
+		public List<Record> Records = new List<Record>();
+
+		public void Add(Enum goal)
+		{
+			Records.Add(new Record(goal));
+		}
+
+		public void Strike(Enum goal)
+		{
+			foreach (var record in Records)
+				if (record.Goal == goal)
+					record.IsStrike = true;
+		}
+	}
+}
