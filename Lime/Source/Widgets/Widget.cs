c5160c2eb2f869267bab50d76f54d21bd143c245
atuzhik <atuzhik@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2012-12-27 23:36:03 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@177745 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

--- Lime/Source/Widgets/Widget.cs
+++ Lime/Source/Widgets/Widget.cs
@@ -255,7 +255,7 @@ namespace Lime
 			}
 		}
 
-		private void ApplyAnchors()
+		public void ApplyAnchors()
 		{
 			Vector2 s = Parent.Widget.Size;
 			if (parentSize.HasValue && !parentSize.Value.Equals(s)) {
