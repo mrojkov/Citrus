4fcc6f33a5a57276fcabbefc024c429d7c06570a
M.Rojkov <M.Rojkov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2013-04-20 23:21:12 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@181549 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ TangerineXwt/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xwt;
+using Xwt.Drawing;
+
+namespace Tangerine
+{
+	public class App
+	{
+		[STAThread]
+		static void Main()
+		{
+			//Application.Initialize("XwtPlus.WPFBackend.WPFEngine, XwtPlus.WPF, Version=1.00");
+			Application.Initialize("XwtPlus.GtkBackend.GtkEngine, XwtPlus.Gtk, Version=1.00");
+
+			//Application.Initialize(ToolkitType.Wpf);
+			//Workspace.Open("f:/Work/Zzz/Kill3.citproj");
+			//The.Workspace.OpenDocument("f:/Work/Zzz/Data/Game/Alien.tan");
+			//The.Workspace.OpenDocument("f:/Work/Zzz/Data/Game/Levels/C_Bridge/Bridge02.tan");
+			//ShowTimeline();
+			var mainWindow = new MainWindow();
+			//mainWindow.Show();
+			XwtPlus.CommandManager.Realize();
+			Application.Run();
+			//mainWindow.Dispose();
+		}
+
+		private static void ShowTimeline()
+		{
+			The.Document.RebuildRows();
+			The.Timeline.Refresh();
+		}
+	}
+}
