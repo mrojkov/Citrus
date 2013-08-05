789de065d8d1d5a13c733055830048af33172d9d
M.Rojkov <M.Rojkov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2013-08-05 05:36:33 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@183397 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ Orange/Source/Actions/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Orange
+{
+	public static class Test
+	{
+		[MenuItem("Test", 1)]
+		public static void Test()
+		{
+		}
+	}
+}
