e98cf46a5489cead7f271c590739f450f593ecaf
M.Rojkov <M.Rojkov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2013-08-05 05:03:59 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@183395 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ Orange/Source/Actions/UpgradeOrange.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Orange
+{
+	static class UpgradeOrange
+	{
+		[MenuItem("Upgrade Orange")]
+		public static void UpgradeOrangeAction()
+		{
+			var path = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Toolbox.GetApplicationDirectory())));
+			var builder = new SolutionBuilder(TargetPlatform.Desktop, path, "Orange");
+			builder.SvnUpdate();
+#if WIN
+			CsprojSynchronization.SynchronizeProject(Path.Combine(path, "Orange.Win.csproj"));
+#elif MAC
+			CsprojSynchronization.SynchronizeProject(Path.Combine(path, "Orange.Mac.csproj"));
+#endif
+			builder.Build();
+		}
+	}
+}
