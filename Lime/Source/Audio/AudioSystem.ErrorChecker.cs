d2e9e8dc5c452521d42362dd42cfaaf9c4747358
M.Rojkov <M.Rojkov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2013-09-10 00:37:56 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@183941 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ Lime/Source/Audio/AudioSystem.ErrorChecker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+#if OPENAL
+using OpenTK.Audio;
+using OpenTK.Audio.OpenAL;
+#endif
+
+namespace Lime
+{
+	public static partial class AudioSystem
+	{
+		public class ErrorChecker : IDisposable
+		{
+			string comment;
+			bool throwException;
+
+			public ErrorChecker(string comment = null, bool throwException = true)
+			{
+				this.comment = comment;
+				this.throwException = throwException;
+				// Clear current error
+				AL.GetError();
+			}
+
+			void IDisposable.Dispose()
+			{
+#if OPENAL
+				var error = AL.GetError();
+				if (error != ALError.NoError) {
+					string message = "OpenAL error: " + AL.GetErrorString(error);
+					if (comment != null) {
+						message += string.Format(" ({0})", comment);
+					}
+					if (throwException) {
+						throw new Exception(message);
+					} else {
+						Logger.Write(comment);
+					}
+				}
+#endif
+			}
+		}
+	}
+}
