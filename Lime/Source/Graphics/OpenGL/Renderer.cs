19fc01fc5d84fb2c4ea1a411ca505717d0576854
M.Rojkov <M.Rojkov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2013-12-23 05:38:10 +0000

    Reduced Lime start-up time
    git-svn-id: svn://srv-dc/svn2/Citrus@185214 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

--- Lime/Source/Graphics/OpenGL/Renderer.cs
+++ Lime/Source/Graphics/OpenGL/Renderer.cs
@@ -424,9 +424,9 @@ namespace Lime
 			GL.LoadMatrix(value.ToFloatArray());
 			GL.MatrixMode(All.Modelview);
 #else
-			GL.MatrixMode(MatrixMode.Projection);
-			GL.LoadMatrix(value.ToFloatArray());
-			GL.MatrixMode(MatrixMode.Modelview);
+			OGL.MatrixMode(MatrixMode.Projection);
+			OGL.LoadMatrix(value.ToFloatArray());
+			OGL.MatrixMode(MatrixMode.Modelview);
 #endif
 
 		}
