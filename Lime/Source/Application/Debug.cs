4e1b718f8045025a7cdb418376003e732c55b1e0
M.Rojkov <M.Rojkov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2014-02-26 05:29:52 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@186146 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ Lime/Source/Application/Debug.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Lime
+{
+	public static class Debug
+	{
+		public static bool BreakOnButtonClick;
+		public static void Write(string message)
+		{
+			Debugger.Log(0, null, message);
+		}
+	}
+}
