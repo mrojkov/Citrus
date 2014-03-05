7f6e8c8e249f3ba0aaf7b70b598e0fb9e0fdb9d7
M.Rojkov <M.Rojkov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2014-03-05 04:36:24 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@186249 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ Lime/Source/Application/Sdl2.cs
@@ -0,0 +1,48 @@
+﻿#if WIN
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+
+namespace Lime
+{
+    static class Sdl
+    {
+		const string lib = "SDL2.dll";
+
+		[SuppressUnmanagedCodeSecurity]
+		[DllImport(lib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_CreateColorCursor")]
+		public static extern IntPtr CreateColorCursor(IntPtr surface, int hotX, int hotY);
+
+		[SuppressUnmanagedCodeSecurity]
+		[DllImport(lib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_SetCursor")]
+		public static extern void SetCursor(IntPtr cursor);
+
+		[SuppressUnmanagedCodeSecurity]
+		[DllImport(lib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_CreateRGBSurfaceFrom")]
+		public static extern IntPtr CreateRGBSurfaceFrom(IntPtr pixels,
+					int width, int height, int depth, int pitch,
+					uint Rmask, uint Gmask, uint Bmask, uint Amask);
+
+		[SuppressUnmanagedCodeSecurity]
+		[DllImport(lib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_FreeSurface", ExactSpelling = true)]
+		public static extern void FreeSurface(IntPtr surface);
+
+		public static string GetError()
+		{
+			return IntPtrToString(GetErrorInternal());
+		}
+
+		[SuppressUnmanagedCodeSecurity]
+		[DllImport(lib, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_GetError", ExactSpelling = true)]
+		static extern IntPtr GetErrorInternal();
+		
+		static string IntPtrToString(IntPtr ptr)
+		{
+			return Marshal.PtrToStringAnsi(ptr);
+		}
+    }
+}
+#endif
