e7831fb8e4241415b1ae997ae682d2bf05bf8448
M.Rojkov <M.Rojkov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2014-03-25 10:42:01 +0000

    git-svn-id: svn://srv-dc/svn2/Citrus@187062 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

--- Lime/Source/Widgets/Animation/Animator.cs
+++ Lime/Source/Widgets/Animation/Animator.cs
@@ -1,59 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Runtime.Serialization;
-using Lime;
 using ProtoBuf;
 
 namespace Lime
 {
-	public static class AnimationUtils
-	{
-		public const int FramesPerSecond = 16;
-
-		public static int MsecsToFrames(int msecs)
-		{
-			return msecs >> 6;
-		}
-
-		public static int FramesToMsecs(int frames)
-		{
-			return frames << 6;
-		}
-
-		public struct PropertyData
-		{
-			public Type OwnerType;
-			public PropertyInfo Info;
-			public bool Triggerable;
-		}
-		
-		private static Dictionary<string, List<PropertyData>> propertyCache = new Dictionary<string, List<PropertyData>>();
-		
-		internal static PropertyData GetProperty(Type ownerType, string propertyName)
-		{
-			List<PropertyData> plist;
-			if (!propertyCache.TryGetValue(propertyName, out plist)) {
-				plist = new List<PropertyData>();
-				propertyCache[propertyName] = plist;
-			}
-			foreach (PropertyData i in plist) {
-				if (ownerType == i.OwnerType) {
-					return i;
-				}
-			}
-			var p = new PropertyData();
-			p.Info = ownerType.GetProperty(propertyName);
-			p.OwnerType = ownerType;
-			if (p.Info == null) {
-				throw new Lime.Exception("Property '{0}' doesn't exist for class '{1}'", propertyName, ownerType);
-			}
-			p.Triggerable = p.Info.GetCustomAttributes(typeof(TriggerAttribute), false).Length > 0;
-			plist.Add(p);
-			return p;
-		}
-	}
-
 	[ProtoContract]
 	[ProtoInclude(101, typeof(Animator<string>))]
 	[ProtoInclude(102, typeof(Animator<int>))]
--- Lime/Source/Widgets/Animation/AnimatorCollection.cs
+++ Lime/Source/Widgets/Animation/AnimatorCollection.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
 using System.Reflection;
-using Lime;
 using ProtoBuf;
 
 namespace Lime
