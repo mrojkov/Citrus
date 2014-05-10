f91b00720b92bc8a933cd140215964fc23057a7f
M.Rojkov <M.Rojkov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2014-05-10 11:48:22 +0000

    Added X/Y direct access for point objects
    git-svn-id: svn://srv-dc/svn2/Citrus@189929 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

--- Lime/Source/Widgets/PointObject.cs
+++ Lime/Source/Widgets/PointObject.cs
@@ -11,8 +11,13 @@ namespace Lime
 	[ProtoInclude(102, typeof(DistortionMeshPoint))]
 	public class PointObject : Node
 	{
+		private Vector2 position;
+
 		[ProtoMember(1)]
-		public Vector2 Position { get; set; }
+		public Vector2 Position { get { return position; } set { position = value; } }
+
+		public float X { get { return position.X; } set { position.X = value; } }
+		public float Y { get { return position.Y; } set { position.Y = value; } }
 
 		[ProtoMember(2)]
 		public SkinningWeights SkinningWeights { get; set; }
