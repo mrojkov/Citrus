9c9dbe31d109f4871fd87bc0eda7f81d9b83ab65
grisha <grisha@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2014-05-12 23:36:55 +0000

    RAVE linking problem fix
    git-svn-id: svn://srv-dc/svn2/Citrus@190089 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

--- 3rdParty/BigFish_iOS_SDK/libbfg_iOS_sdk.linkwith.cs
+++ 3rdParty/BigFish_iOS_SDK/libbfg_iOS_sdk.linkwith.cs
@@ -3,7 +3,7 @@ using MonoTouch.ObjCRuntime;
 
 [assembly: LinkWith("libbfg_iOS_sdk.a", 
    	LinkTarget.ArmV7 | LinkTarget.Simulator,
-    ForceLoad = true,
+    //ForceLoad = true,
 	LinkerFlags = "-lz -licucore",
     Frameworks = "CoreGraphics CoreText CoreTelephony Foundation GameKit iAd MessageUI OpenGLES Security StoreKit SystemConfiguration UIKit MobileCoreServices",
     WeakFrameworks = "AdSupport")
