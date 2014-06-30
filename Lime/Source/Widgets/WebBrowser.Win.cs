fa506a9b7db7ff401b72e3daf2157146bc9044fd
aklenin <aklenin@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2014-06-30 19:07:08 +0000

    Lime: Implement a web browser stub for windows
    git-svn-id: svn://srv-dc/svn2/Citrus@192837 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

--- Lime/Source/Widgets/WebBrowser.Win.cs
+++ Lime/Source/Widgets/WebBrowser.Win.cs
@@ -1,27 +1,55 @@
 ﻿#if !iOS
 using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace Lime
 {
-	public class WebBrowser : Widget, IDisposable
+	public class WebBrowser : Image, IDisposable
 	{
-		public Uri Url { get { return null; } set { SetUrl(value); } }
+		public Uri Url { get { return browser.Url; } set { SetUrl(value); } }
 
-		public WebBrowser()
+		private System.Windows.Forms.WebBrowser browser;
+
+		public WebBrowser() : base(new Texture2D())
 		{
+			browser = new System.Windows.Forms.WebBrowser();
+			browser.DocumentCompleted += browser_DocumentCompleted;
 		}
 
-		public WebBrowser(Widget parentWidget)
+		public WebBrowser(Widget parentWidget): this()
 		{
 			parentWidget.AddNode(this);
+			Size = parentWidget.Size;
+			browser.Width = (int)Width;
+			browser.Height = (int)Height;
+			browser.ScrollBarsEnabled = false;
+		}
 
+		private Texture2D browserImage = new Texture2D();
+
+		void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+		{
+			// FIXME: It's hard to make a browser to work over an OpenGL surface, so at least draw it as a picture.
+			var bitmap = new Bitmap((int)Width, (int)Height);
+			browser.DrawToBitmap(bitmap, new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height));
+			var m = new MemoryStream(1024 * 1024);
+			bitmap.Save(m, System.Drawing.Imaging.ImageFormat.Png);
+			m.Position = 0;
+			(Texture as Texture2D).LoadImage(m);
 		}
 		
 		private void SetUrl(Uri value)
 		{
-		}	
+			browser.Navigate(value);
+		}
 
-		public void Dispose() { }
+		public void Dispose()
+		{
+			if (browser != null)
+				browser.Dispose();
+		}
 	}
 }
 #endif
