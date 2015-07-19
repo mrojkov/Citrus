175f2df75aff4c668f1ae9336efbbf2cfef65b73
Alexander S. Klenin <klenin@gmail.com>
2015-07-19 18:10:59 +1000

    Initial structure

---
+++ YuzuTest/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Yuzu;
+
+namespace YuzuTest
+{
+	[TestClass]
+	public class TestMain
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var yuzu = new Yuzu.Yuzu();
+			Assert.IsTrue(true);
+		}
+
+		public static void Main()
+		{
+			
+		}
+	}
+}
