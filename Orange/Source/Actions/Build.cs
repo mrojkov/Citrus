b9d68bae9fa3e44488c1b1c7c428a8e031b76612
aandreev <aandreev@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2015-07-30 04:39:15 +0000

    Orange: Add missing Action
    git-svn-id: svn://srv-dc/svn2/Citrus@221937 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ Orange/Source/Actions/Build.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Orange
+{
+	public static partial class Actions
+	{
+		[MenuItem("Build", 0)]
+		public static void BuildAction()
+		{
+			AssetCooker.CookForActivePlatform();
+			BuildGame();
+		}
+	}
+}
