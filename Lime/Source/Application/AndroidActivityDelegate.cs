5d6bf222369d5a1f2245409ec55e7a8dad4a15f6
aandreev <aandreev@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2015-07-31 00:01:56 +0000

    AndroidActivityDelegate: Weaken condition for "Invalid accelerometer data" Exception
    git-svn-id: svn://srv-dc/svn2/Citrus@222002 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

--- Lime/Source/Application/AndroidActivityDelegate.cs
+++ Lime/Source/Application/AndroidActivityDelegate.cs
@@ -1,5 +1,6 @@
 #if ANDROID
 using System;
+using System.Linq;
 using Android.App;
 using Android.OS;
 using Android.Content.PM;
@@ -26,12 +27,26 @@ namespace Lime
 
 			public void OnSensorChanged(SensorEvent e)
 			{
-				if (e.Values.Count != 3) {
-					throw new Lime.Exception("Invalid accelerometer data");
+				if (e.Values == null)
+				{
+					throw new Exception("Invalid accelerometer data");
 				}
+				var vector = new Vector3();
+                if (e.Values.Count > 0)
+                {
+                    vector.X = e.Values[0];
+                }
+                if (e.Values.Count > 1)
+                {
+                    vector.Y = e.Values[1];
+                }
+                if (e.Values.Count > 2)
+                {
+                    vector.Z = e.Values[2];
+                }
 				// Translate into iOS accelerometer units: g-forces instead of m/s^2
-				var a = -new Lime.Vector3(e.Values[0], e.Values[1], e.Values[2]) / 9.81f;
-				Lime.Input.Acceleration = a;
+				var a = -vector / 9.81f;
+				Input.Acceleration = a;
 			}
 
 			public static void StartListening()
