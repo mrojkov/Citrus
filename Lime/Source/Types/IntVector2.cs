b8619e94680c58b28bd972602a44edb626765764
aandreev <aandreev@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2015-08-23 06:31:30 +0000

    IntVector2: Documentation UPGRADE!
    git-svn-id: svn://srv-dc/svn2/Citrus@223399 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

--- Lime/Source/Types/IntVector2.cs
+++ Lime/Source/Types/IntVector2.cs
@@ -4,125 +4,220 @@ using ProtoBuf;
 namespace Lime
 {
 	/// <summary>
-	/// Представляет точку в двумерном пространстве. Компоненты вектора представлены целыми числами
+	/// Representation of integer 2D vectors and points.
 	/// </summary>
 	[ProtoContract]
 	[System.Diagnostics.DebuggerStepThrough]
 	public struct IntVector2 : IEquatable<IntVector2>
 	{
+		/// <summary>
+		/// X component of this <see cref="IntVector2"/>.
+		/// </summary>
 		[ProtoMember(1)]
 		public int X;
-		
+
+		/// <summary>
+		/// Y component of this <see cref="IntVector2"/>.
+		/// </summary>
 		[ProtoMember(2)]
 		public int Y;
 
 		/// <summary>
-		/// Возвращает вектор (0, 0)
+		/// Returns a <see cref="IntVector2"/> with components 0, 0.
 		/// </summary>
 		public static readonly IntVector2 Zero = new IntVector2(0, 0);
 
 		/// <summary>
-		/// Возвращает вектор (1, 1)
+		/// Returns a <see cref="IntVector2"/> with components 1, 1.
 		/// </summary>
 		public static readonly IntVector2 One = new IntVector2(1, 1);
 
 		/// <summary>
-		/// Возвращает вектор (0, -1)
+		/// Returns a <see cref="IntVector2"/> with components 0, -1.
 		/// </summary>
 		public static readonly IntVector2 Up = new IntVector2(0, -1);
 
 		/// <summary>
-		/// Возвращает вектор (0, 1)
+		/// Returns a <see cref="IntVector2"/> with components 0, 1.
 		/// </summary>
 		public static readonly IntVector2 Down = new IntVector2(0, 1);
 
 		/// <summary>
-		/// Возвращает вектор (-1, 0)
+		/// Returns a <see cref="IntVector2"/> with components -1, 0.
 		/// </summary>
 		public static readonly IntVector2 Left = new IntVector2(-1, 0);
 
 		/// <summary>
-		/// Возвращает вектор (1, 0)
+		/// Returns a <see cref="IntVector2"/> with components 1, 0.
 		/// </summary>
 		public static readonly IntVector2 Right = new IntVector2(1, 0);
 
+		/// <summary>
+		/// Constructs a 2D vector with X and Y from two values.
+		/// </summary>
+		/// <param name="x">The X coordinate in 2D-space.</param>
+		/// <param name="y">The Y coordinate in 2D-space.</param>
 		public IntVector2(int x, int y)
 		{
 			X = x;
 			Y = y;
 		}
 
-		public static explicit operator Size(IntVector2 v)
+		/// <summary>
+		/// Explicit cast from <see cref="IntVector2"/> to <see cref="Size"/>.
+		/// </summary>
+		/// <param name="value">Source <see cref="IntVector2"/>.</param>
+		/// <returns>
+		/// New <see cref="Size"/> with X and Y of source <see cref="IntVector2"/>.
+		/// </returns>
+		public static explicit operator Size(IntVector2 value)
 		{
-			return new Size(v.X, v.Y);
+			return new Size(value.X, value.Y);
 		}
 
-		public static explicit operator Vector2(IntVector2 v)
+		/// <summary>
+		/// Explicit cast from <see cref="IntVector2"/> to <see cref="Vector2"/>.
+		/// </summary>
+		/// <param name="value">Source <see cref="IntVector2"/>.</param>
+		/// <returns>
+		/// New <see cref="Vector2"/> with X and Y of source <see cref="IntVector2"/>.
+		/// </returns>
+		public static explicit operator Vector2(IntVector2 value)
 		{
-			return new Vector2((float)v.X, (float)v.Y);
+			return new Vector2(value.X, value.Y);
 		}
 
-		bool IEquatable<IntVector2>.Equals(IntVector2 rhs)
+		/// <summary>
+		/// Compares whether current instance is equal to specified <see cref="IntVector2"/>.
+		/// </summary>
+		/// <param name="other">The <see cref="IntVector2"/> to compare.</param>
+		/// <returns><c>true</c> if the instances are equal; <c>false</c> otherwise.</returns>
+		public bool Equals(IntVector2 other)
 		{
-			return X == rhs.X && Y == rhs.Y;
+			return X == other.X && Y == other.Y;
 		}
 
-		public override bool Equals(object o)
+		/// <summary>
+		/// Compares whether current instance is equal to specified <see cref="Object"/>.
+		/// </summary>
+		/// <param name="obj">The <see cref="Object"/> to compare.</param>
+		/// <returns><c>true</c> if the instances are equal; <c>false</c> otherwise.</returns>
+		public override bool Equals(object obj)
 		{
-			var rhs = (IntVector2)o;
-			return X == rhs.X && Y == rhs.Y;
+			return obj is IntVector2 && Equals((IntVector2)obj);
 		}
 
+		/// <summary>
+		/// Gets the hash code of this <see cref="IntVector2"/>.
+		/// </summary>
+		/// <returns>Hash code of this <see cref="IntVector2"/>.</returns>
 		public override int GetHashCode()
 		{
 			return X.GetHashCode() ^ Y.GetHashCode();
 		}
 
-		public static bool operator ==(IntVector2 lhs, IntVector2 rhs)
+		/// <summary>
+		/// Compares whether two <see cref="IntVector2"/> instances are equal.
+		/// </summary>
+		/// <param name="left"><see cref="IntVector2"/> instance on the left of the equal sign.</param>
+		/// <param name="right"><see cref="IntVector2"/> instance on the right of the equal sign.</param>
+		/// <returns><c>true</c> if the instances are equal; <c>false</c> otherwise.</returns>
+		public static bool operator ==(IntVector2 left, IntVector2 right)
 		{
-			return lhs.X == rhs.X && lhs.Y == rhs.Y;
+			return left.X == right.X && left.Y == right.Y;
 		}
 
-		public static bool operator !=(IntVector2 lhs, IntVector2 rhs)
+		/// <summary>
+		/// Compares whether two <see cref="IntVector2"/> instances are not equal.
+		/// </summary>
+		/// <param name="left"><see cref="IntVector2"/> instance on the left of the not equal sign.</param>
+		/// <param name="right"><see cref="IntVector2"/> instance on the right of the not equal sign.</param>
+		/// <returns><c>true</c> if the instances are not equal; <c>false</c> otherwise.</returns>	
+		public static bool operator !=(IntVector2 left, IntVector2 right)
 		{
-			return lhs.X != rhs.X || lhs.Y != rhs.Y;
+			return left.X != right.X || left.Y != right.Y;
 		}
 
-		public static IntVector2 operator +(IntVector2 lhs, IntVector2 rhs)
+		/// <summary>
+		/// Adds the components of two <see cref="IntVector2"/> instances.
+		/// </summary>
+		/// <param name="left">Source <see cref="IntVector2"/> on the left of the add sign.</param>
+		/// <param name="right">Source <see cref="IntVector2"/> on the right of the add sign.</param>
+		/// <returns>The result of the <see cref="IntVector2"/> sum.</returns>
+		public static IntVector2 operator +(IntVector2 left, IntVector2 right)
 		{
-			return new IntVector2(lhs.X + rhs.X, lhs.Y + rhs.Y);
+			return new IntVector2(left.X + right.X, left.Y + right.Y);
 		}
 
-		public static IntVector2 operator -(IntVector2 lhs, IntVector2 rhs)
+		/// <summary>
+		/// Subtracts the components of another <see cref="IntVector2"/> 
+		/// from components of the <see cref="IntVector2"/>.
+		/// </summary>
+		/// <param name="left">Source <see cref="IntVector2"/> on the left of the sub sign.</param>
+		/// <param name="right">Divisor <see cref="IntVector2"/> on the right of the sub sign.</param>
+		/// <returns>The result of the <see cref="IntVector2"/> subtract.</returns>
+		public static IntVector2 operator -(IntVector2 left, IntVector2 right)
 		{
-			return new IntVector2(lhs.X - rhs.X, lhs.Y - rhs.Y);
+			return new IntVector2(left.X - right.X, left.Y - right.Y);
 		}
 
-		public static IntVector2 operator -(IntVector2 v)
+		/// <summary>
+		/// Inverts values in the specified <see cref="IntVector2"/>.
+		/// </summary>
+		/// <param name="value">Source <see cref="IntVector2"/> on the right of the sub sign.</param>
+		/// <returns>Result of the inversion.</returns>
+		public static IntVector2 operator -(IntVector2 value)
 		{
-			return new IntVector2(-v.X, -v.Y);
+			return new IntVector2(-value.X, -value.Y);
 		}
 
-		public static IntVector2 operator /(IntVector2 lhs, int rhs)
+		/// <summary>
+		/// Divides the components of the <see cref="IntVector2"/> by a scalar.
+		/// </summary>
+		/// <param name="value">Source <see cref="IntVector2"/> on the left of the div sign.</param>
+		/// <param name="divider">Divisor scalar on the right of the div sign.</param>
+		/// <returns>The result of dividing the <see cref="IntVector2"/> by a scalar.</returns>
+		public static IntVector2 operator /(IntVector2 value, int divider)
 		{
-			return new IntVector2(lhs.X / rhs, lhs.Y / rhs);
+			return new IntVector2(value.X / divider, value.Y / divider);
 		}
 
-		public static IntVector2 operator *(int lhs, IntVector2 rhs)
+		/// <summary>
+		/// Multiplies the components of <see cref="IntVector2"/> by a scalar.
+		/// </summary>
+		/// <param name="scaleFactor">Scalar value on the left of the mul sign.</param>
+		/// <param name="value">Source <see cref="IntVector2"/> on the right of the mul sign.</param>
+		/// <returns>The result of the <see cref="IntVector2"/> multiplication with a scalar.</returns>
+		public static IntVector2 operator *(int scaleFactor, IntVector2 value)
 		{
-			return new IntVector2(lhs * rhs.X, lhs * rhs.Y);
+			return new IntVector2(scaleFactor * value.X, scaleFactor * value.Y);
 		}
 
-		public static IntVector2 operator *(IntVector2 lhs, int rhs)
+		/// <summary>
+		/// Multiplies the components of <see cref="IntVector2"/> by a scalar.
+		/// </summary>
+		/// <param name="value">Source <see cref="IntVector2"/> on the right of the mul sign.</param>
+		/// <param name="scaleFactor">Scalar value on the left of the mul sign.</param>
+		/// <returns>The result of the <see cref="IntVector2"/> multiplication with a scalar.</returns>
+		public static IntVector2 operator *(IntVector2 value, int scaleFactor)
 		{
-			return new IntVector2(rhs * lhs.X, rhs * lhs.Y);
+			return new IntVector2(scaleFactor * value.X, scaleFactor * value.Y);
 		}
 
+		/// <summary>
+		/// Returns the <see cref="String"/> representation of this <see cref="IntVector2"/> in the format:
+		/// "[<see cref="X"/>], [<see cref="Y"/>]".
+		/// </summary>
+		/// <returns>The <see cref="String"/> representation of this <see cref="IntVector2"/>.</returns>
 		public override string ToString()
 		{
-			return String.Format("{0}, {1}", X, Y);
+			return string.Format("{0}, {1}", X, Y);
 		}
 
+		/// <summary>
+		/// Returns the <see cref="Vector2"/> representation of this <see cref="IntVector2"/>.
+		/// </summary>
+		/// <returns>The <see cref="Vector2"/> representation of this <see cref="IntVector2"/>.</returns>
 		public Vector2 ToVector2()
 		{
 			return new Vector2(X, Y);
