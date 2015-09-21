35579f3b2eacfe884e10826eb79c0a6b8923badd
drotanov <drotanov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2015-09-21 06:58:05 +0000

    AndroidActivityDelegate.cs: fix coding style
    git-svn-id: svn://srv-dc/svn2/Citrus@225450 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

--- Lime/Source/Application/AndroidActivityDelegate.cs
+++ Lime/Source/Application/AndroidActivityDelegate.cs
@@ -23,27 +23,23 @@ namespace Lime
 		{
 			private static AccelerometerListener listener;
 
-			public void OnAccuracyChanged (Sensor sensor, SensorStatus accuracy) { }
+			public void OnAccuracyChanged(Sensor sensor, SensorStatus accuracy) { }
 
 			public void OnSensorChanged(SensorEvent e)
 			{
-				if (e.Values == null)
-				{
+				if (e.Values == null) {
 					throw new Exception("Invalid accelerometer data");
 				}
 				var vector = new Vector3();
-                if (e.Values.Count > 0)
-                {
-                    vector.X = e.Values[0];
-                }
-                if (e.Values.Count > 1)
-                {
-                    vector.Y = e.Values[1];
-                }
-                if (e.Values.Count > 2)
-                {
-                    vector.Z = e.Values[2];
-                }
+				if (e.Values.Count > 0) {
+					vector.X = e.Values[0];
+				}
+				if (e.Values.Count > 1) {
+					vector.Y = e.Values[1];
+				}
+				if (e.Values.Count > 2) {
+					vector.Z = e.Values[2];
+				}
 				// Translate into iOS accelerometer units: g-forces instead of m/s^2
 				var a = -vector / 9.81f;
 				Input.Acceleration = a;
@@ -92,7 +88,7 @@ namespace Lime
 		public event Action<TrimMemory> TrimmingMemory;
 #endif
 		public event Action LowMemory;
-		
+
 		public event Action<Android.Content.Res.Configuration> ConfigurationChanged;
 		public event BackButtonDelegate BackPressed;
 		public event ActivityResultDelegate ActivityResult;
@@ -185,7 +181,7 @@ namespace Lime
 				ConfigurationChanged(newConfig);
 			}
 		}
-		
+
 		public virtual void OnActivityResult(int requestCode, Result resultCode, Intent data)
 		{
 			if (ActivityResult != null) {
