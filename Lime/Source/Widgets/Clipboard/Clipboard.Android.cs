0e5a9a48bf3969df500644c4a12c49bd88d8ddc3
aandrianov <aandrianov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2015-10-27 07:40:46 +0000

    Add Mock clipboards for iOS and Android
    git-svn-id: svn://srv-dc/svn2/Citrus@227905 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ Lime/Source/Widgets/Clipboard/Clipboard.Android.cs
@@ -0,0 +1,17 @@
+﻿#if ANDROID
+namespace Lime
+{
+	//Mock Android clipboard implementation
+	public static partial class Clipboard
+	{
+		private static string GetTextImpl()
+		{
+		}
+
+		private static void PutTextImpl(string text)
+		{
+		}
+	}
+}
+#endif
+
---
+++ Lime/Source/Widgets/Clipboard/Clipboard.iOS.cs
@@ -0,0 +1,17 @@
+﻿#if iOS
+namespace Lime
+{
+	//Mock iOS clipboard implementation
+	public static partial class Clipboard
+	{
+		private static string GetTextImpl()
+		{
+		}
+
+		private static void PutTextImpl(string text)
+		{
+		}
+	}
+}
+#endif
+
