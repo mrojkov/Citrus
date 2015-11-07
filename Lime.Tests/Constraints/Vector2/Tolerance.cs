bfeeb7ce6163d07bd996fe18530394a77c7151de
aandreev <aandreev@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2015-11-07 04:10:00 +0000

    Downgrade target framework
    git-svn-id: svn://srv-dc/svn2/Citrus@228632 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

--- Lime.Tests/Constraints/Vector2/Tolerance.cs
+++ Lime.Tests/Constraints/Vector2/Tolerance.cs
@@ -4,7 +4,7 @@ namespace Lime.Tests
 {
 	public class Vector2Tolerance : Tolerance
 	{
-		public Vector2 Amount { get; }
+		public Vector2 Amount { get; set; }
 
 		public Vector2Tolerance(Vector2 amount) 
 			: base(amount)
