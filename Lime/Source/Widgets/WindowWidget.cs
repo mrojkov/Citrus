4874bfc5fd53538b86051338ee54c1a25b17abc3
M.Rojkov <M.Rojkov@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2015-11-08 06:44:01 +0000

    WindowWidget: Put Window reference and WidgetContext creation in constructor back
    git-svn-id: svn://srv-dc/svn2/Citrus@228658 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

--- Lime/Source/Widgets/WindowWidget.cs
+++ Lime/Source/Widgets/WindowWidget.cs
@@ -11,35 +11,43 @@ namespace Lime
 		private IKeyboardInputProcessor prevActiveTextWidget;
 #endif
 		private RenderChain renderChain = new RenderChain();
+		
+		public IWindow Window { get; private set; }
+		public IWidgetContext Context { get; private set; }
+		
+		public WindowWidget(IWindow window)
+		{
+			Window = window;
+			Context = new WidgetContext(window, this);
+			Window.Context = Context;
+		}
 
 		public override void Update(float delta)
 		{
-			var context = WidgetContext.Current;
 			WidgetInput.RemoveInvalidatedCaptures();
 			ParticleEmitter.NumberOfUpdatedParticles = 0;
-			context.IsActiveTextWidgetUpdated = false;
-			context.DistanceToNodeUnderCursor = float.MaxValue;
+			Context.IsActiveTextWidgetUpdated = false;
+			Context.DistanceToNodeUnderCursor = float.MaxValue;
 			base.Update(delta);
-			if (!context.IsActiveTextWidgetUpdated) {
-				context.ActiveTextWidget = null;
+			if (!Context.IsActiveTextWidgetUpdated) {
+				Context.ActiveTextWidget = null;
 			}
 #if iOS || ANDROID
 			if (Application.CurrentThread.IsMain()) {
-				bool showKeyboard = context.ActiveTextWidget != null && context.ActiveTextWidget.Visible;
-				if (prevActiveTextWidget != context.ActiveTextWidget) {
-					Application.SoftKeyboard.Show(showKeyboard, context.ActiveTextWidget != null ? context.ActiveTextWidget.Text : "");
+				bool showKeyboard = Context.ActiveTextWidget != null && Context.ActiveTextWidget.Visible;
+				if (prevActiveTextWidget != Context.ActiveTextWidget) {
+					Application.SoftKeyboard.Show(showKeyboard, Context.ActiveTextWidget != null ? Context.ActiveTextWidget.Text : "");
 				}
 #if ANDROID
 				if (!Application.SoftKeyboard.Visible) {
-					context.ActiveTextWidget = null;
+					Context.ActiveTextWidget = null;
 				}
 #endif
 				// Handle switching between various text widgets
-				if (prevActiveTextWidget != context.ActiveTextWidget && context.ActiveTextWidget != null && prevActiveTextWidget != null) {
-					Application.SoftKeyboard.ChangeText(context.ActiveTextWidget.Text);
+				if (prevActiveTextWidget != Context.ActiveTextWidget && Context.ActiveTextWidget != null && prevActiveTextWidget != null) {
+					Application.SoftKeyboard.ChangeText(Context.ActiveTextWidget.Text);
 				}
-
-				prevActiveTextWidget = context.ActiveTextWidget;
+				prevActiveTextWidget = Context.ActiveTextWidget;
 			}
 #endif
 		}
