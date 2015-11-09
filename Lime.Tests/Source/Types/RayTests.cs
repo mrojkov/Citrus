a11a73a6740191a4111f38828316a828342db9cd
aandreev <aandreev@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2015-11-09 03:07:34 +0000

    Add Ray tests
    git-svn-id: svn://srv-dc/svn2/Citrus@228690 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

--- Lime.Tests/Source/Types/RayTests.cs
+++ Lime.Tests/Source/Types/RayTests.cs
@@ -1,6 +1,31 @@
-﻿namespace Lime.Tests.Source.Types
+﻿using NUnit.Framework;
+
+namespace Lime.Tests.Source.Types
 {
-	class RayTests
+	[TestFixture]
+	public class RayTests
 	{
+		private static Ray unitRay = new Ray(Vector3.Zero, new Vector3(1, 0, 0));
+
+		[Test]
+		public void EqualsTest()
+		{
+			Assert.That(unitRay.Equals(unitRay));
+			Assert.That(unitRay.Equals((object) unitRay));
+			Assert.That(unitRay == unitRay);
+			Assert.That(unitRay != new Ray(Vector3.Zero, Vector3.Zero));
+			Assert.That(unitRay.GetHashCode(), Is.EqualTo(unitRay.GetHashCode()));
+		}
+
+		[Test]
+		public void IntersectsTest()
+		{
+			var disjoiningSphere = new BoundingSphere(new Vector3(-2, 0, 0), 1);
+			Assert.That(unitRay.Intersects(ref disjoiningSphere), Is.Null);
+			var containingSphere = new BoundingSphere(Vector3.Zero, 1);
+			Assert.That(unitRay.Intersects(ref containingSphere), Is.EqualTo(0));
+			var intersectingSphere = new BoundingSphere(new Vector3(2, 0, 0), 1);
+			Assert.That(unitRay.Intersects(ref intersectingSphere), Is.EqualTo(1));
+		}
 	}
 }
