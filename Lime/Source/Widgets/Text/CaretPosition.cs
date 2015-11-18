3e55369329f06ea733f2bed5fe1e3bb7ad1f37fa
aandreev <aandreev@20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a>
2015-11-18 04:16:21 +0000

    Move Text-related modules to corresponding folder
    git-svn-id: svn://srv-dc/svn2/Citrus@229415 20bf7d6a-bd21-9d4d-a3c4-a2c14bb8c68a

---
+++ Lime/Source/Widgets/Text/CaretPosition.cs
@@ -0,0 +1,96 @@
+namespace Lime
+{
+	internal class CaretPosition: ICaretPosition
+	{
+		public enum ValidState { None, All, LinePos, WorldPos, TextPos };
+		public ValidState Valid;
+		public int RenderingLineNumber;
+		public int RenderingTextPos;
+		public Vector2 NearestCharPos;
+
+		private int line;
+		private int pos;
+		private int textPos;
+		private Vector2 worldPos;
+		private bool isVisible;
+
+		public int Line {
+			get { return line; }
+			set {
+				if (line == value) return;
+				Valid = ValidState.LinePos;
+				line = value;
+			}
+		}
+
+		public int Pos {
+			get { return pos; }
+			set {
+				if (pos == value) return;
+				Valid = ValidState.LinePos;
+				pos = value;
+			}
+		}
+
+		public int TextPos
+		{
+			get { return textPos; }
+			set
+			{
+				if (textPos == value) return;
+				Valid = ValidState.TextPos;
+				textPos = value;
+			}
+		}
+
+		public Vector2 WorldPos
+		{
+			get { return worldPos; }
+			set
+			{
+				if (worldPos.Equals(value)) return;
+				Valid = ValidState.WorldPos;
+				worldPos = value;
+			}
+		}
+
+		public bool IsVisible {
+			get { return isVisible && Valid != ValidState.None; }
+			set { isVisible = value; }
+		}
+
+		public void Sync(int index, Vector2 charPos, Vector2 size)
+		{
+			switch (Valid) {
+				case ValidState.None:
+				case ValidState.All:
+					break;
+				case ValidState.LinePos:
+					if (Line == RenderingLineNumber && Pos == index) {
+						TextPos = RenderingTextPos;
+						WorldPos = charPos;
+						Valid = ValidState.All;
+					}
+					break;
+				case ValidState.TextPos:
+					if (TextPos == RenderingTextPos) {
+						Line = RenderingLineNumber;
+						Pos = index;
+						WorldPos = charPos;
+						Valid = ValidState.All;
+					}
+					break;
+				case ValidState.WorldPos:
+					if ((WorldPos - charPos).SqrLength < (WorldPos - NearestCharPos).SqrLength) {
+						Line = RenderingLineNumber;
+						Pos = index;
+						TextPos = RenderingTextPos;
+						NearestCharPos = charPos;
+						Valid = ValidState.WorldPos;
+					}
+					break;
+			}
+			++RenderingTextPos;
+		}
+	}
+}
---
+++ Lime/Source/Widgets/Text/TextLineSplitter.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+
+namespace Lime
+{
+	internal static class TextLineSplitter
+	{
+		public delegate bool MeasureTextLineWidthDelegate(string line, int start, int count);
+
+		public static bool CarryLastWordToNextLine(List<string> strings, int line, bool isWordSplitAllowed, MeasureTextLineWidthDelegate measureHandler)
+		{
+			string lastWord;
+			string lineWithoutLastWord;
+			if (TrySplitLine(strings[line], isWordSplitAllowed, measureHandler, out lineWithoutLastWord, out lastWord)) {
+				PushWordToLine(lastWord, strings, line + 1);
+				strings[line] = lineWithoutLastWord;
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		private static bool TrySplitLine(string line, bool isWordSplitAllowed, MeasureTextLineWidthDelegate measureHandler, out string lineWithoutLastWord, out string lastWord)
+		{
+			return
+				TryCutLastWord(line, out lineWithoutLastWord, out lastWord)
+				|| (
+					(isWordSplitAllowed || line.HasJapaneseSymbols())
+					&& TryCutWordTail(line, measureHandler, out lineWithoutLastWord, out lastWord)
+					);
+		}
+
+		private static bool TryCutLastWord(string text, out string lineWithoutLastWord, out string lastWord)
+		{
+			lineWithoutLastWord = null;
+			lastWord = null;
+			// Use Line Separator character as a soft break
+			int lastSpaceAt = Math.Max(text.LastIndexOf(' '), text.LastIndexOf((char)8232));
+			if (lastSpaceAt < 0) {
+				return false;
+			}
+			if (lastSpaceAt == text.Length - 1) {
+				// Treat a space character as a word
+				lastWord = text.Substring(lastSpaceAt);
+				lineWithoutLastWord = text.Substring(0, lastSpaceAt);
+			} else {
+				lineWithoutLastWord = text.Substring(0, lastSpaceAt + 1);
+				lastWord = text.Substring(lastSpaceAt + 1);
+			}
+			return true;
+		}
+
+		private static bool TryCutWordTail(string textLine, MeasureTextLineWidthDelegate measureHandler, out string currentLinePart, out string nextLinePart)
+		{
+			currentLinePart = null;
+			nextLinePart = null;
+			var cutFrom = CalcFittedCharactersCount(textLine, measureHandler);
+			if (cutFrom > 0) {
+				Toolbox.AdjustLineBreakPosition(textLine, ref cutFrom);
+				nextLinePart = textLine.Substring(cutFrom);
+				currentLinePart = textLine.Substring(0, cutFrom);
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		private static int CalcFittedCharactersCount(string textLine, MeasureTextLineWidthDelegate measureHandler)
+		{
+			int min = 0;
+			int max = textLine.Length;
+			int mid = 0;
+			bool isLineLonger = false;
+
+			do {
+				mid = min + ((max - min) / 2);
+				isLineLonger = !measureHandler(textLine, 0, mid);
+				if (isLineLonger) {
+					max = mid;
+				} else {
+					min = mid;
+				}
+			}
+			while (min < max && !(!isLineLonger && ((max - min) / 2) == 0));
+
+			return mid;
+		}
+
+		private static void PushWordToLine(string word, List<string> strings, int line)
+		{
+			if (line >= strings.Count || EndsWith(word, '\n')) {
+				strings.Insert(line, word);
+			} else {
+				strings[line] = word + strings[line];
+			}
+		}
+
+		private static bool EndsWith(string s, char c)
+		{
+			int i = s.Length;
+			return i > 0 && s[i - 1] == c;
+		}
+	}
+}
