6e03df40410428ce5664982106ea21b590099c83
Alexander S. Klenin <klenin@gmail.com>
2015-11-24 14:23:52 +1000

    Json: Extract JsonGenerators.cs

---
+++ Yuzu/JsonGenerators.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Yuzu
+{
+	public abstract class JsonDeserializerGenBase : JsonDeserializer
+	{
+		public abstract object FromReaderIntPartial(string name);
+
+		public override object FromReaderInt()
+		{
+			return FromReaderIntGenerated();
+		}
+	}
+
+	public class JsonDeserializerGenerator : JsonDeserializer
+	{
+		public static new JsonDeserializerGenerator Instance = new JsonDeserializerGenerator();
+
+		private int indent = 0;
+		public StreamWriter GenWriter;
+
+		public JsonDeserializerGenerator()
+		{
+			Options.Assembly = Assembly.GetCallingAssembly();
+		}
+
+		private void PutPart(string s)
+		{
+			GenWriter.Write(s.Replace("\n", "\r\n"));
+		}
+
+		private void Put(string s)
+		{
+			if (s.StartsWith("}")) // "}\n" or "} while"
+				indent -= 1;
+			if (s != "\n")
+				for (int i = 0; i < indent; ++i)
+					PutPart(JsonOptions.Indent);
+			PutPart(s);
+			if (s.EndsWith("{\n"))
+				indent += 1;
+		}
+
+		private void PutF(string format, params object[] p)
+		{
+			Put(String.Format(format, p));
+		}
+
+		public void GenerateHeader(string namespaceName)
+		{
+			Put("using System;\n");
+			Put("using System.Collections.Generic;\n");
+			Put("using System.Reflection;\n");
+			Put("\n");
+			Put("using Yuzu;\n");
+			Put("\n");
+			PutF("namespace {0}\n", namespaceName);
+			Put("{\n");
+			Put("\n");
+		}
+
+		public void GenerateFooter()
+		{
+			Put("}\n");
+		}
+
+		private int tempCount = 0;
+
+		private string GetTypeSpec(Type t)
+		{
+			return t.IsGenericType ?
+				String.Format("{0}<{1}>",
+					t.Name.Remove(t.Name.IndexOf('`')),
+					String.Join(",", t.GetGenericArguments().Select(GetTypeSpec))) :
+				t.Name;
+		}
+
+		private void GenerateValue(Type t, string name)
+		{
+			if (t == typeof(int)) {
+				PutPart("RequireInt();\n");
+			}
+			else if (t == typeof(uint)) {
+				PutPart("RequireUInt();\n");
+			}
+			else if (t == typeof(string)) {
+				PutPart("RequireString();\n");
+			}
+			else if (t == typeof(bool)) {
+				PutPart("RequireBool();\n");
+			}
+			else if (t == typeof(float)) {
+				PutPart("RequireSingle();\n");
+			}
+			else if (t == typeof(double)) {
+				PutPart("RequireDouble();\n");
+			}
+			else if (t.IsEnum) {
+				PutPart(String.Format(
+					JsonOptions.EnumAsString ?
+						"({0})Enum.Parse(typeof({0}), RequireString());\n" :
+						"({0})RequireInt();\n",
+					t.Name));
+			}
+			else if (t.IsGenericType && t.GetGenericTypeDefinition() == typeof(List<>)) {
+				PutPart(String.Format("new {0}();\n", GetTypeSpec(t)));
+				Put("Require('[');\n");
+				Put("if (SkipSpacesCarefully() == ']') {\n");
+				Put("Require(']');\n");
+				Put("}\n");
+				Put("else {\n");
+				Put("do {\n");
+				tempCount += 1;
+				var tempName = "tmp" + tempCount.ToString();
+				PutF("var {0} = ", tempName);
+				GenerateValue(t.GetGenericArguments()[0], tempName);
+				PutF("{0}.Add({1});\n", name, tempName);
+				Put("} while (Require(']', ',') == ',');\n");
+				Put("}\n");
+			}
+			else if (t.IsArray && !JsonOptions.ArrayLengthPrefix) {
+				PutPart(String.Format("new {0}[0];\n", GetTypeSpec(t.GetElementType())));
+				Put("Require('[');\n");
+				Put("if (SkipSpacesCarefully() == ']') {\n");
+				Put("Require(']');\n");
+				Put("}\n");
+				Put("else {\n");
+				tempCount += 1;
+				var tempListName = "tmp" + tempCount.ToString();
+				PutF("var {0} = new List<{1}>();\n", tempListName, GetTypeSpec(t.GetElementType()));
+				Put("do {\n");
+				tempCount += 1;
+				var tempName = "tmp" + tempCount.ToString();
+				PutF("var {0} = ", tempName);
+				GenerateValue(t.GetElementType(), tempName);
+				PutF("{0}.Add({1});\n", tempListName, tempName);
+				Put("} while (Require(']', ',') == ',');\n");
+				PutF("{0} = {1}.ToArray();\n", name, tempListName);
+				Put("}\n");
+			}
+			else if (t.IsArray && JsonOptions.ArrayLengthPrefix) {
+				PutPart(String.Format("new {0}[0];\n", GetTypeSpec(t.GetElementType())));
+				Put("Require('[');\n");
+				Put("if (SkipSpacesCarefully() != ']') {\n");
+				tempCount += 1;
+				var tempArrayName = "tmp" + tempCount.ToString();
+				PutF("var {0} = new {1}[RequireUInt()];\n", tempArrayName, GetTypeSpec(t.GetElementType()));
+				tempCount += 1;
+				var tempIndexName = "tmp" + tempCount.ToString();
+				PutF("for(int {0} = 0; {0} < {1}.Length; ++{0}) {{\n", tempIndexName, tempArrayName);
+				Put("Require(',');\n");
+				PutF("{0}[{1}] = ", tempArrayName, tempIndexName);
+				GenerateValue(t.GetElementType(), String.Format("{0}[{1}]", tempArrayName, tempIndexName));
+				Put("}\n");
+				PutF("{0} = {1};\n", name, tempArrayName);
+				Put("}\n");
+				Put("Require(']');\n");
+			}
+			else if (t.IsClass && Options.ClassNames) {
+				PutPart(String.Format("({0})base.FromReaderInt();\n", t.Name));
+			}
+			else if (t.IsClass && !Options.ClassNames || Utils.IsStruct(t)) {
+				PutPart(String.Format("({0}){0}_JsonDeserializer.Instance.FromReader(new {0}(), Reader);\n", t.Name));
+			}
+			else {
+				throw new NotImplementedException(t.Name);
+			}
+		}
+
+		public void Generate<T>()
+		{
+			PutF("class {0}_JsonDeserializer : JsonDeserializerGenBase\n", typeof(T).Name);
+			Put("{\n");
+
+			PutF("public static new {0}_JsonDeserializer Instance = new {0}_JsonDeserializer();\n", typeof(T).Name);
+			Put("\n");
+
+			PutF("public {0}_JsonDeserializer()\n", typeof(T).Name);
+			Put("{\n");
+			PutF("Options.Assembly = Assembly.Load(\"{0}\");\n", typeof(T).Assembly.FullName);
+			foreach (var f in Options.GetType().GetFields()) {
+				var v = Utils.CodeValueFormat(f.GetValue(Options));
+				if (v != "") // TODO
+					PutF("Options.{0} = {1};\n", f.Name, v);
+			}
+			foreach (var f in JsonOptions.GetType().GetFields()) {
+				var v = Utils.CodeValueFormat(f.GetValue(JsonOptions));
+				if (v != "") // TODO
+					PutF("JsonOptions.{0} = {1};\n", f.Name, v);
+			}
+			Put("}\n");
+			Put("\n");
+
+			Put("public override object FromReaderInt()\n");
+			Put("{\n");
+			// Since deserializer is dynamically constructed anyway, it is too late to determine object type here.
+			PutF("return FromReaderInt(new {0}());\n", typeof(T).Name);
+			Put("}\n");
+			Put("\n");
+
+			Put("public override object FromReaderIntPartial(string name)\n");
+			Put("{\n");
+			PutF("return ReadFields(new {0}(), name);\n", typeof(T).Name);
+			Put("}\n");
+			Put("\n");
+
+			Put("protected override object ReadFields(object obj, string name)\n");
+			Put("{\n");
+			PutF("var result = ({0})obj;\n", typeof(T).Name);
+			tempCount = 0;
+			foreach (var yi in Utils.GetYuzuItems(typeof(T), Options)) {
+				if (yi.IsOptional) {
+					PutF("if (\"{0}\" == name) {{\n", yi.Name);
+					PutF("result.{0} = ", yi.Name);
+				}
+				else {
+					PutF("if (\"{0}\" != name) throw new YuzuException();\n", yi.Name);
+					PutF("result.{0} = ", yi.Name);
+				}
+				GenerateValue(yi.Type, "result." + yi.Name);
+				Put("name = GetNextName(false);\n");
+				if (yi.IsOptional)
+					Put("}\n");
+			}
+			Put("Require('}');\n");
+			Put("return result;\n");
+			Put("}\n");
+			Put("}\n");
+			Put("\n");
+		}
+
+		public override object FromReaderInt()
+		{
+			return FromReaderIntGenerated();
+		}
+
+		public override object FromReaderInt(object obj)
+		{
+			return FromReaderIntGenerated(obj);
+		}
+	}
+}
