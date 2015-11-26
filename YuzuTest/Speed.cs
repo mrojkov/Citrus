ac7218397ea59fccd5a47e3426bf3ab144267187
Alexander S. Klenin <klenin@gmail.com>
2015-11-26 21:29:57 +1000

    Test: Add large tree test

--- YuzuTest/Speed.cs
+++ YuzuTest/Speed.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ProtoBuf;
@@ -9,6 +10,54 @@ using Yuzu;
 
 namespace YuzuTest
 {
+	[YuzuCompact][ProtoContract]
+	public class Color
+	{
+		[YuzuRequired][ProtoMember(1)]
+		public byte R;
+
+		[YuzuRequired][ProtoMember(2)]
+		public byte G;
+
+		[YuzuRequired][ProtoMember(3)]
+		public byte B;
+	}
+
+	[ProtoContract]
+	public class SamplePerson
+	{
+		public static int Counter = 0;
+
+		[YuzuRequired][ProtoMember(1)]
+		public string Name;
+
+		[YuzuRequired][ProtoMember(2)]
+		public DateTime Birth;
+
+		[YuzuRequired][ProtoMember(3)]
+
+		public List<SamplePerson> Children;
+
+		[YuzuRequired][ProtoMember(4)]
+		public Color EyeColor;
+
+		public SamplePerson(Random rnd, int depth)
+		{
+			Counter++;
+			StringBuilder sb = new StringBuilder();
+			var len = rnd.Next(1, 40);
+			for (int i = 0; i < len; ++i)
+				sb.Append((char)rnd.Next((int)'a', (int)'z' + 1));
+			Name = sb.ToString();
+			Birth = new DateTime(1999, rnd.Next(10) + 1, 13);
+			var childCount = rnd.Next(28 / depth);
+			Children = new List<SamplePerson>();
+			for (int i = 0; i < childCount; ++i)
+				Children.Add(new SamplePerson(rnd, depth + 1));
+			EyeColor = new Color { R = (byte)rnd.Next(256), G = (byte)rnd.Next(256), B = (byte)rnd.Next(256) };
+		}
+	}
+
 
 	[TestClass]
 	public class TestSpeed
@@ -95,6 +144,39 @@ namespace YuzuTest
 			var jdg = new SampleMatrix_JsonDeserializer();
 			var list3 = (SampleMatrix)jdg.FromString(result1);
 			Assert.AreEqual(list1.M.Count, list3.M.Count);
+		}
+
+	}
+
+	[TestClass]
+	public class TestSpeedPerson {
+		private static SamplePerson person;
+
+		[ClassInitialize]
+		public static void Init(TestContext context) {
+			SamplePerson.Counter = 0;
+			Random rnd = new Random(20151125);
+			person = new SamplePerson(rnd, 1);
+		}
+
+		[TestMethod]
+		public void TestProtobuf()
+		{
+			var ms = new MemoryStream();
+			ProtoBuf.Serializer.Serialize(ms, person);
+			Assert.IsTrue(ms.Length > 0);
+		}
+
+		[TestMethod]
+		public void TestJson()
+		{
+			Assert.AreEqual(28076, SamplePerson.Counter);
+			var js = new JsonSerializer();
+			js.JsonOptions.Indent = "";
+			js.JsonOptions.FieldSeparator = "";
+			var ms = new MemoryStream();
+			js.ToStream(person, ms);
+			Assert.IsTrue(ms.Length > 0);
 		}
 	}
 
