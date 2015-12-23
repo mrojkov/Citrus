9f0d911b36615cd8ae7f953bae5fafce2517f56c
Andrey Andreev <aandreev@game-forest.com>
2015-12-23 15:45:12 +1000

    Inline AllocateHelper. Make LinearAllocator class static.

--- Lime/Source/Widgets/Layout/LinearAllocator.cs
+++ Lime/Source/Widgets/Layout/LinearAllocator.cs
@@ -3,57 +3,43 @@ using System.Collections.Generic;
 
 namespace Lime
 {
-	class LinearAllocator
+	static class LinearAllocator
 	{
-		private bool roundSizes;
-
-		public LinearAllocator(bool roundSizes)
-		{
-			this.roundSizes = roundSizes;
-		}
-
-		public float[] Allocate(float availableSize, Constraints[] constraints)
+		public static float[] Allocate(float availableSize, Constraints[] constraints, bool roundSizes)
 		{
 			var indices = new List<int>(constraints.Length);
 			for (int i = 0; i < constraints.Length; i++) {
 				indices.Add(i);
 			}
 			var sizes = new float[constraints.Length];
-			AllocateHelper(availableSize, constraints, sizes, indices);
+			while (indices.Count != 0) {
+				var totalStretch = CalcTotalStretch(constraints, indices);
+				float allocatedSize = 0;
+				foreach (var i in indices) {
+					var c = constraints[i];
+					var s = availableSize * c.Stretch / totalStretch;
+					if (roundSizes) {
+						s = s.Round();
+					}
+					s = Mathf.Clamp(s, c.MinSize, c.MaxSize);
+					allocatedSize += (sizes[i] = s);
+				}
+				if (Math.Abs(allocatedSize - availableSize) < 0.01f) {
+					// We perfectly fit the items into the designated space.
+					return sizes;
+				}
+				var containerOverfilled = allocatedSize > availableSize;
+				for (int i = indices.Count - 1; i >= 0; i--) {
+					var t = constraints[i];
+					if (sizes[i] == (containerOverfilled ? t.MinSize : t.MaxSize)) {
+						availableSize = Math.Max(0, availableSize - sizes[i]);
+						indices.RemoveAt(i);
+					}
+				}
+			}
 			return sizes;
 		}
-
-		private void AllocateHelper(float availableSize, Constraints[] constraints, float[] sizes, List<int> indices)
-		{
-			if (indices.Count == 0) {
-				return;
-			}
-			var totalStretch = CalcTotalStretch(constraints, indices);
-			float allocatedSize = 0;
-			foreach (var i in indices) {
-				var c = constraints[i];
-				var s = availableSize * c.Stretch / totalStretch;
-				if (roundSizes) {
-					s = s.Round();
-				}
-				s = Mathf.Clamp(s, c.MinSize, c.MaxSize);
-				allocatedSize += (sizes[i] = s);
-			}
-			if (Math.Abs(allocatedSize - availableSize) < 0.01f) {
-				// We perfectly fit the items into the designated space.
-				return;
-			}
-			var containerOverfilled = allocatedSize > availableSize;
-			for (int i = indices.Count - 1; i >= 0; i--) {
-				var t = constraints[i];
-				if (sizes[i] == (containerOverfilled ? t.MinSize : t.MaxSize)) {
-					availableSize = Math.Max(0, availableSize - sizes[i]);
-					indices.RemoveAt(i);
-				}
-			}
-			AllocateHelper(availableSize, constraints, sizes, indices);
-		}
-
+		
 		private static float CalcTotalStretch(Constraints[] constraints, List<int> indices)
 		{
 			float ts = 0;
