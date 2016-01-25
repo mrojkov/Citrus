176cffbc09d977798aea8d6adc134ca9bde759a1
Mikhail Rozhkov <mrozhkov@game-forest.com>
2016-01-25 19:20:26 +1000

    Add FileDialog

---
+++ Lime/Source/Application/FileDialog/FileDialog.Mac.cs
@@ -0,0 +1,59 @@
+﻿#if MAC
+using System;
+using AppKit;
+using System.Linq;
+
+namespace Lime
+{
+	public class FileDialog : IFileDialog
+	{
+		public FileDialogMode Mode { get; set; }
+		public bool CanChooseFiles { get; set; }
+		public bool CanChooseDirectories { get; set; }
+		public bool AllowsMultipleSelection { get; set; }
+		public bool CanCreateDirectories { get; set; }
+		public bool ShowHiddenFiles { get; set; }
+		public string FileName { get; private set; }
+		public string[] FileNames { get; private set; }
+		public string[] AllowedFileTypes { get; set; }
+
+		public FileDialog() { }
+
+		public bool RunModal()
+		{
+			FileName = null;
+			FileNames = null;
+			if (Mode == FileDialogMode.Save) {
+				using (var nsPanel = new NSSavePanel()) {
+					if (AllowedFileTypes != null) {
+						nsPanel.AllowedFileTypes = AllowedFileTypes;
+					}
+					nsPanel.ShowsHiddenFiles = ShowHiddenFiles;
+					nsPanel.CanCreateDirectories = CanCreateDirectories;
+					if (nsPanel.RunModal() == (int)NSPanelButtonType.Ok) {
+						FileName = nsPanel.Url.Path;
+						return true;
+					}
+				}
+			} else {
+				using (var nsPanel = new NSOpenPanel()) {
+					if (AllowedFileTypes != null) {
+						nsPanel.AllowedFileTypes = AllowedFileTypes;
+					}
+					nsPanel.AllowsMultipleSelection = AllowsMultipleSelection;
+					nsPanel.CanChooseFiles = Mode == FileDialogMode.Open;
+					nsPanel.CanChooseDirectories = !nsPanel.CanChooseFiles;
+					nsPanel.ShowsHiddenFiles = ShowHiddenFiles;
+					nsPanel.CanCreateDirectories = CanCreateDirectories;
+					if (nsPanel.RunModal() == (int)NSPanelButtonType.Ok) {
+						FileName = nsPanel.Url.Path;
+						FileNames = nsPanel.Urls.Select(i => i.Path).ToArray();
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+	}
+}
+#endif
---
+++ Lime/Source/Application/FileDialog/FileDialog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lime
+{
+	public enum FileDialogMode
+	{
+		Open,
+		Save,
+		SelectFolder,
+	}
+
+	public interface IFileDialog
+	{
+		FileDialogMode Mode { get; set; }
+		bool CanCreateDirectories { get; set; }
+		bool ShowHiddenFiles { get; set; }
+		bool AllowsMultipleSelection { get; set; }
+		string[] AllowedFileTypes { get; set; }
+		string FileName { get; }
+		string[] FileNames { get; }
+		bool RunModal();
+	}
+}
