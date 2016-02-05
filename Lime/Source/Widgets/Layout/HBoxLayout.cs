43af83d14a47f7289dc7df05543642fa9db0bd95
Mikhail Rozhkov <mrozhkov@game-forest.com>
2016-02-05 12:34:17 +1000

    [H/VBoxLayout] fix layout with non-zero padding

--- Lime/Source/Widgets/Layout/HBoxLayout.cs
+++ Lime/Source/Widgets/Layout/HBoxLayout.cs
@@ -41,7 +41,7 @@ namespace Lime
 			DebugRectangles.Clear();
 			var position = new Vector2(widget.Padding.Left, widget.Padding.Top);
 			foreach (var w in widgets) {
-				var size = new Vector2(sizes[i], widget.Height);
+				var size = new Vector2(sizes[i], widget.ContentHeight);
 				TableLayout.LayoutCell(w, position, size, DebugRectangles);
 				position.X += size.X + Spacing;
 				i++;
