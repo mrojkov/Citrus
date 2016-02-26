1781bb7671beb244ff155bd886aa6c7fd07f67c2
konstantin egorov <kegorov@game-forest.com>
2016-02-26 17:01:16 +1000

    Orange: Support .FBX

--- Orange/Source/ModelImport/ModelImporter.cs
+++ Orange/Source/ModelImport/ModelImporter.cs
@@ -37,6 +37,80 @@ namespace Orange
 
 	public class ModelImporter
 	{
+		private class Pivot
+		{
+			public static readonly Pivot Default = new Pivot();
+
+			public Matrix44? Translation;
+			public Matrix44? RotationOffset;
+			public Matrix44? RotationPivot;
+			public Matrix44? PreRotation;
+			public Matrix44? Rotation;
+			public Matrix44? PostRotation;
+			public Matrix44? RotationPivotInverse;
+			public Matrix44? ScalingOffset;
+			public Matrix44? ScalingPivot;
+			public Matrix44? Scaling;
+			public Matrix44? ScalingPivotInverse;
+			public Matrix44? GeometricTranslation;
+			public Matrix44? GeometricRotation;
+			public Matrix44? GeometricScaling;
+
+			public Matrix44 GetTransform(Vector3? scale, Quaternion? rotation, Vector3? translation)
+			{
+				var transform = Matrix44.Identity;
+				if (GeometricScaling.HasValue) {
+					transform *= GeometricScaling.Value;
+				}
+				if (GeometricRotation.HasValue) {
+					transform *= GeometricRotation.Value;
+				}
+				if (GeometricTranslation.HasValue) {
+					transform *= GeometricTranslation.Value;
+				}
+				if (ScalingPivotInverse.HasValue) {
+					transform *= ScalingPivotInverse.Value;
+				}
+				if (scale.HasValue) {
+					transform *= Matrix44.CreateScale(scale.Value);
+				} else if (Scaling.HasValue) {
+					transform *= Scaling.Value;
+				}
+				if (ScalingPivot.HasValue) {
+					transform *= ScalingPivot.Value;
+				}
+				if (ScalingOffset.HasValue) {
+					transform *= ScalingOffset.Value;
+				}
+				if (RotationPivotInverse.HasValue) {
+					transform *= RotationPivotInverse.Value;
+				}
+				if (PostRotation.HasValue) {
+					transform *= PostRotation.Value;
+				}
+				if (rotation.HasValue) {
+					transform *= Matrix44.CreateRotation(rotation.Value);
+				} else if (Rotation.HasValue) {
+					transform *= Rotation.Value;
+				}
+				if (PreRotation.HasValue) {
+					transform *= PreRotation.Value;
+				}
+				if (RotationPivot.HasValue) {
+					transform *= RotationPivot.Value;
+				}
+				if (RotationOffset.HasValue) {
+					transform *= RotationOffset.Value;
+				}
+				if (translation.HasValue) {
+					transform *= Matrix44.CreateTranslation(translation.Value);
+				} else if (Translation.HasValue) {
+					transform *= Translation.Value;
+				}
+				return transform;
+			}
+		}
+
 		private class MaterialImportData
 		{
 			public Dictionary<string, TextureReference> Textures { get; private set; }
@@ -76,6 +150,7 @@ namespace Orange
 		private Assimp.Scene aiScene;
 		private TargetPlatform platform;
 		private Dictionary<string, Assimp.Camera> aiCameras = new Dictionary<string, Assimp.Camera>();
+		private Dictionary<string, Pivot> pivots = new Dictionary<string, Pivot>();
 
 		public ModelNode RootNode { get; private set; }
 
@@ -92,7 +167,7 @@ namespace Orange
 				}
 				aiScene = aiContext.ImportFile(path, postProcess);
 				FindCameras();
-				RootNode = ImportNode(aiScene.RootNode);
+				ImportNodes();
 				ImportAnimations();
 			}
 		}
@@ -102,12 +177,17 @@ namespace Orange
 			aiCameras = aiScene.Cameras.ToDictionary(camera => camera.Name);
 		}
 
-		private ModelNode ImportNode(Assimp.Node aiNode)
+		private void ImportNodes()
+		{
+			RootNode = ImportNodes(aiScene.RootNode, null, null);
+		}
+
+		private ModelNode ImportNodes(Assimp.Node aiNode, Assimp.Node aiParent, Node parent)
 		{
 			ModelNode node = null;
 			if (aiNode.HasMeshes) {
 				var mesh = new ModelMesh { Id = aiNode.Name };
-				mesh.SetLocalTransform(aiNode.Transform.ToLime());
+				mesh.SetLocalTransform(CalcRelativeTransform(aiNode, aiParent));
 				foreach (var aiMeshIndex in aiNode.MeshIndices) {
 					var aiMesh = aiScene.Meshes[aiMeshIndex];
 					if (aiMesh.HasVertices) {
@@ -116,22 +196,68 @@ namespace Orange
 				}
 				mesh.BoundingSphere = BoundingSphere.CreateFromPoints(mesh.Submeshes.SelectMany(submesh => submesh.Geometry.Vertices));
 				node = mesh;
+			} else if (aiNode.Name.Contains("_$AssimpFbx$")) {
+				var ownerNodeName = GetNodeName(aiNode.Name);
+				Pivot pivot;
+				if (!pivots.TryGetValue(ownerNodeName, out pivot)) {
+					pivot = new Pivot();
+					pivots.Add(ownerNodeName, pivot);
+				}
+				Matrix44 transform = aiNode.Transform.ToLime();
+				if (aiNode.Name.EndsWith("_Translation")) {
+					pivot.Translation = transform;
+				} else if (aiNode.Name.EndsWith("_RotationOffset")) {
+					pivot.RotationOffset = transform;
+				} else if (aiNode.Name.EndsWith("_RotationPivot")) {
+					pivot.RotationPivot = transform;
+				} else if (aiNode.Name.EndsWith("_PreRotation")) {
+					pivot.PreRotation = transform;
+				} else if (aiNode.Name.EndsWith("_Rotation")) {
+					pivot.Rotation = transform;
+				} else if (aiNode.Name.EndsWith("_PostRotation")) {
+					pivot.PostRotation = transform;
+				} else if (aiNode.Name.EndsWith("_RotationPivotInverse")) {
+					pivot.RotationPivotInverse = transform;
+				} else if (aiNode.Name.EndsWith("_ScalingOffset")) {
+					pivot.ScalingOffset = transform;
+				} else if (aiNode.Name.EndsWith("_ScalingPivot")) {
+					pivot.ScalingPivot = transform;
+				} else if (aiNode.Name.EndsWith("_Scaling")) {
+					pivot.Scaling = transform;
+				} else if (aiNode.Name.EndsWith("_ScalingPivotInverse")) {
+					pivot.ScalingPivotInverse = transform;
+				} else if (aiNode.Name.EndsWith("_GeometricTranslation")) {
+					pivot.GeometricTranslation = transform;
+				} else if (aiNode.Name.EndsWith("_GeometricRotation")) {
+					pivot.GeometricRotation = transform;
+				} else if (aiNode.Name.EndsWith("_GeometricScaling")) {
+					pivot.GeometricScaling = transform;
+				} else {
+					throw new Lime.Exception("Unknown $AssimpFbx$ node: \"{0}\"", aiNode.Name);
+				}
 			} else {
 				Assimp.Camera camera;
 				if (aiCameras.TryGetValue(aiNode.Name, out camera)) {
-					node = ImportCamera(camera, aiNode);
+					node = ImportCamera(camera, aiNode, aiParent);
 				} else {
 					node = new ModelNode { Id = aiNode.Name };
-					node.SetLocalTransform(aiNode.Transform.ToLime());
+					node.SetLocalTransform(CalcRelativeTransform(aiNode, aiParent));
 				}
 			}
+			if (node != null) {
+				if (parent != null) {
+					parent.Nodes.Add(node);
+				}
+				aiParent = aiNode;
+				parent = node;
+			}
 			foreach (var aiChild in aiNode.Children) {
-				node.Nodes.Add(ImportNode(aiChild));
+				ImportNodes(aiChild, aiParent, parent);
 			}
 			return node;
 		}
 
-		private ModelNode ImportCamera(Assimp.Camera aiCamera, Assimp.Node aiNode)
+		private ModelNode ImportCamera(Assimp.Camera aiCamera, Assimp.Node aiNode, Assimp.Node aiParent)
 		{
 			var camera = new ModelCamera {
 				Id = aiCamera.Name,
@@ -140,7 +266,7 @@ namespace Orange
 				NearClipPlane = aiCamera.ClipPlaneNear,
 				FarClipPlane = aiCamera.ClipPlaneFar,
 			};
-			camera.SetLocalTransform(aiNode.Transform.ToLime());
+			camera.SetLocalTransform(CalcRelativeTransform(aiNode, aiParent));
 			return camera;
 		}
 
@@ -189,7 +315,7 @@ namespace Orange
 
 		private SerializableTexture CreateSerializableTexture(Assimp.TextureSlot texture)
 		{
-			var texturePath = Toolbox.ToUnixSlashes(Path.Combine(Path.GetDirectoryName(path), 
+			var texturePath = Toolbox.ToUnixSlashes(Path.Combine(Path.GetDirectoryName(path),
 				Path.GetFileNameWithoutExtension(Toolbox.ToUnixSlashes(texture.FilePath))));
 			return new SerializableTexture(texturePath);
 		}
@@ -203,28 +329,114 @@ namespace Orange
 
 		private void ImportAnimation(Assimp.Animation aiAnimation)
 		{
-			var animation = new Animation {
-				Id = aiAnimation.Name
-			};
-			foreach (var channel in aiAnimation.NodeAnimationChannels) {
-				var node = RootNode.Find<ModelNode>(channel.NodeName);
-				var scaleAnimator = node.Animators["Scale", animation.Id] as Animator<Vector3>;
-				var rotationAnimator = node.Animators["Rotation", animation.Id] as Animator<Quaternion>;
-				var positionAnimator = node.Animators["Position", animation.Id] as Animator<Vector3>;
-				AddDistinctKeys(
-					scaleAnimator,
-					channel.ScalingKeys.Select(key => new Keyframe<Vector3>(TimeToFrame(key.Time), key.Value.ToLime()))
-				);
-				AddDistinctKeys(
-					rotationAnimator,
-					channel.RotationKeys.Select(key => new Keyframe<Quaternion>(TimeToFrame(key.Time), key.Value.ToLime()))
-				);
-				AddDistinctKeys(
-					positionAnimator,
-					channel.PositionKeys.Select(key => new Keyframe<Vector3>(TimeToFrame(key.Time), key.Value.ToLime()))
-				);
+			var animationId = GetAnimationName(aiAnimation.Name);
+			var channelGroups = aiAnimation
+				.NodeAnimationChannels
+				.GroupBy(channel => GetNodeName(channel.NodeName));
+			foreach (var channelGroup in channelGroups) {
+				var n = RootNode.TryFind<ModelNode>(channelGroup.Key);
+				var aiScaleKeys = new List<Assimp.VectorKey>();
+				var aiRotationKeys = new List<Assimp.QuaternionKey>();
+				var aiTranslationKeys = new List<Assimp.VectorKey>();
+				Pivot pivot;
+				if (!pivots.TryGetValue(n.Id, out pivot)) {
+					pivot = Pivot.Default;
+				}
+				foreach (var aiChannel in channelGroup) {
+					if (aiChannel.NodeName.EndsWith("_$AssimpFbx$_Scaling")) {
+						aiScaleKeys = aiChannel.ScalingKeys;
+					} else if (aiChannel.NodeName.EndsWith("_$AssimpFbx$_Rotation")) {
+						aiRotationKeys = aiChannel.RotationKeys;
+					} else if (aiChannel.NodeName.EndsWith("_$AssimpFbx$_Translation")) {
+						aiTranslationKeys = aiChannel.PositionKeys;
+					} else {
+						aiScaleKeys = aiChannel.ScalingKeys;
+						aiRotationKeys = aiChannel.RotationKeys;
+						aiTranslationKeys = aiChannel.PositionKeys;
+					}
+				}
+				var times = aiScaleKeys.Select(k => k.Time)
+					.Union(aiRotationKeys.Select(k => k.Time))
+					.Union(aiTranslationKeys.Select(k => k.Time))
+					.OrderBy(t => t)
+					.ToList();
+				int prevScaleIndex = -1;
+				int prevRotationIndex = -1;
+				int prevTranslationIndex = -1;
+				double prevScaleTime = 0.0;
+				double prevRotationTime = 0.0;
+				double prevTranslationTime = 0.0;
+				Vector3? prevScale = null;
+				Quaternion? prevRotation = null;
+				Vector3? prevTranslation = null;
+				var scaleKeys = new List<Keyframe<Vector3>>();
+				var rotationKeys = new List<Keyframe<Quaternion>>();
+				var translationKeys = new List<Keyframe<Vector3>>();
+				foreach (var time in times) {
+					Vector3? scale;
+					int scaleIndex = aiScaleKeys.FindIndex(k => k.Time == time);
+					if (scaleIndex != -1) {
+						scale = aiScaleKeys[scaleIndex].Value.ToLime();
+						prevScaleIndex = scaleIndex;
+						prevScaleTime = time;
+						prevScale = scale;
+					} else if (prevScaleIndex != -1 && prevScaleIndex + 1 < aiScaleKeys.Count) {
+						var nextScaleKey = aiScaleKeys[prevScaleIndex + 1];
+						var nextScaleTime = nextScaleKey.Time;
+						var nextScale = nextScaleKey.Value.ToLime();
+						var amount = (float)((time - prevScaleTime) / (nextScaleTime - prevScaleTime));
+						scale = Vector3.Lerp(amount, prevScale.Value, nextScale);
+					} else {
+						scale = prevScale;
+					}
+					Quaternion? rotation;
+					int rotationIndex = aiRotationKeys.FindIndex(k => k.Time == time);
+					if (rotationIndex != -1) {
+						rotation = aiRotationKeys[rotationIndex].Value.ToLime();
+						prevRotationIndex = rotationIndex;
+						prevRotationTime = time;
+						prevRotation = rotation;
+					} else if (prevRotationIndex != -1 && prevRotationIndex + 1 < aiRotationKeys.Count) {
+						var nextRotationKey = aiRotationKeys[prevRotationIndex + 1];
+						var nextRotationTime = nextRotationKey.Time;
+						var nextRotation = nextRotationKey.Value.ToLime();
+						var amount = (float)((time - prevRotationTime) / (nextRotationTime - prevRotationTime));
+						rotation = Quaternion.Slerp(prevRotation.Value, nextRotation, amount);
+					} else {
+						rotation = prevRotation;
+					}
+					Vector3? translation;
+					int translationIndex = aiTranslationKeys.FindIndex(k => k.Time == time);
+					if (translationIndex != -1) {
+						translation = aiTranslationKeys[translationIndex].Value.ToLime();
+						prevTranslationIndex = translationIndex;
+						prevTranslationTime = time;
+						prevTranslation = translation;
+					} else if (prevTranslationIndex != -1 && prevTranslationIndex + 1 < aiTranslationKeys.Count) {
+						var nextTranslationKey = aiTranslationKeys[prevTranslationIndex + 1];
+						var nextTranslationTime = nextTranslationKey.Time;
+						var nextTranslation = nextTranslationKey.Value.ToLime();
+						var amount = (float)((time - prevTranslationTime) / (nextTranslationTime - prevTranslationTime));
+						translation = Vector3.Lerp(amount, prevTranslation.Value, nextTranslation);
+					} else {
+						translation = prevTranslation;
+					}
+					var transform = pivot.GetTransform(scale, rotation, translation);
+					Vector3 finalScale;
+					Quaternion finalRotation;
+					Vector3 finalTranslation;
+					transform.Decompose(out finalScale, out finalRotation, out finalTranslation);
+					scaleKeys.Add(new Keyframe<Vector3>(TimeToFrame(time), finalScale));
+					rotationKeys.Add(new Keyframe<Quaternion>(TimeToFrame(time), finalRotation));
+					translationKeys.Add(new Keyframe<Vector3>(TimeToFrame(time), finalTranslation));
+				}
+				AddDistinctKeys(n.Animators["Scale", animationId] as Animator<Vector3>, scaleKeys);
+				AddDistinctKeys(n.Animators["Rotation", animationId] as Animator<Quaternion>, rotationKeys);
+				AddDistinctKeys(n.Animators["Position", animationId] as Animator<Vector3>, translationKeys);
 			}
-			RootNode.Animations.Add(animation);
+			RootNode.Animations.Add(new Animation {
+				Id = animationId
+			});
 		}
 
 		//TODO: Move to KeyframeCollection
@@ -243,6 +455,28 @@ namespace Orange
 		private static int TimeToFrame(double time)
 		{
 			return AnimationUtils.MsecsToFrames((int)(time * 1000 + 0.5f));
+		}
+
+		private static string GetNodeName(string name)
+		{
+			int index = name.IndexOf("_$AssimpFbx$", StringComparison.Ordinal);
+			return index >= 0 ? name.Remove(index) : name;
+		}
+
+		private static string GetAnimationName(string name)
+		{
+			return name.Replace("AnimStack::", "");
+		}
+
+		private static Matrix44 CalcRelativeTransform(Assimp.Node node, Assimp.Node ancestor)
+		{
+			Assimp.Matrix4x4 transform = node.Transform;
+			Assimp.Node parent = node.Parent;
+			while (parent != null && parent != ancestor) {
+				transform *= parent.Transform;
+				parent = parent.Parent;
+			}
+			return transform.ToLime();
 		}
 	}
 }
