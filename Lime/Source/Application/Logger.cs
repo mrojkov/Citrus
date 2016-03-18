26c26a454f4bc1e5c29d5257c2bf9954392d2fad
nburimenko <bolya2003@bk.ru>
2016-03-18 14:48:29 +1000

    [Logger]: iOS: Remove dll importing, use common Console.WriteLine() to log on iOS.
    This possible should fix app crash caused by NSLog.

--- Lime/Source/Application/Logger.cs
+++ Lime/Source/Application/Logger.cs
@@ -1,39 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Runtime.InteropServices;
-
-#if iOS
-using ObjCRuntime;
-using Foundation;
-using UIKit;
-#endif
 
 namespace Lime
 {
 	public class Logger
 	{
 		public static event Action<string> OnWrite;
-#if iOS
-		[DllImport(Constants.FoundationLibrary)]
-		private extern static void NSLog(IntPtr message);
 
-		public static void Write(string msg)
-		{
-			using (var nss = new NSString(msg)) {
-				NSLog(nss.Handle);
-			}
-			if (OnWrite != null) {
-				OnWrite(msg);
-			}
-		}
-
-		public static void Write(string msg, params object[] args)
-		{
-			Write(string.Format(msg, args));
-		}
-#else
 		public static void Write(string msg)
 		{
 #if UNITY
@@ -50,6 +22,5 @@ namespace Lime
 		{
 			Write(string.Format(format, args));
 		}
-#endif
 	}
 }
