242feb9456155bf58631c7bb4d3d298a72b0a6bb
nburimenko <bolya2003@bk.ru>
2016-04-05 16:16:40 +1000

    [Bitmap]: add HasAlpha property.

--- Lime/Source/Graphics/Bitmap.Android.cs
+++ Lime/Source/Graphics/Bitmap.Android.cs
@@ -1,6 +1,6 @@
 #if ANDROID
 using System.IO;
-
+using System.Linq;
 using Android.Graphics;
 using Android.OS;
 using AndroidBitmap = Android.Graphics.Bitmap;
@@ -26,6 +26,7 @@ namespace Lime
 				colors[i] = Color4.CreateArgb(pixel.A, pixel.R, pixel.G, pixel.B);
 			}
 			Bitmap = AndroidBitmap.CreateBitmap(colors, width, height, AndroidBitmap.Config.Argb8888);
+			Bitmap.HasAlpha = data.Any(color => color.A != 255);
 		}
 
 		private BitmapImplementation(AndroidBitmap bitmap)
@@ -55,6 +56,11 @@ namespace Lime
 					Bitmap.Height > 0 &&
 					Bitmap.Width > 0;
 			}
+		}
+
+		public bool HasAlpha
+		{
+			get { return Bitmap.HasAlpha; }
 		}
 
 		public IBitmapImplementation Clone()
--- Lime/Source/Graphics/Bitmap.Win.cs
+++ Lime/Source/Graphics/Bitmap.Win.cs
@@ -1,23 +1,25 @@
 #if WIN
 using System;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using SD = System.Drawing;
 
 namespace Lime
 {
-	class BitmapImplementation : IBitmapImplementation
+	internal class BitmapImplementation : IBitmapImplementation
 	{
 		private IntPtr data;
 
 		public BitmapImplementation(Stream stream)
 		{
-			// System.Drawing.Bitmap требует, чтобы stream оставался открытым всё время существования битмапа.
-			// http://stackoverflow.com/questions/336387/image-save-throws-a-gdi-exception-because-the-memory-stream-is-closed
-			// Так как мы не можем быть уверены, что снаружи стрим не уничтожат, копируем его.
+			// System.Drawing.Bitmap требует, чтобы поток оставался открытым всё время существования битмапа.
+			// http://goo.gl/oBBW6G
+			// Так как мы не можем быть уверены, что снаружи поток не уничтожат, копируем его.
 			var streamClone = new MemoryStream();
 			stream.CopyTo(streamClone);
 			Bitmap = new SD.Bitmap(streamClone);
+			HasAlpha = IsReallyHasAlpha(Bitmap);
 		}
 
 		public BitmapImplementation(Color4[] colors, int width, int height)
@@ -26,6 +28,7 @@ namespace Lime
 			var stride = 4 * width;
 			data = CreateMemoryCopy(colors);
 			Bitmap = new SD.Bitmap(width, height, stride, Format, data);
+			HasAlpha = colors.Any(color => color.A != 255);
 		}
 
 		private BitmapImplementation(SD.Bitmap bitmap)
@@ -55,6 +58,11 @@ namespace Lime
 					Bitmap.Height > 0 &&
 					Bitmap.Width > 0;
 			}
+		}
+
+		public bool HasAlpha
+		{
+			get; private set;
 		}
 
 		public IBitmapImplementation Clone()
@@ -124,6 +132,24 @@ namespace Lime
 				}
 			}
 			return data;
+		}
+
+		private static bool IsReallyHasAlpha(SD.Bitmap bitmap)
+		{
+			var bmpData = bitmap.LockBits(
+				new SD.Rectangle(0, 0, bitmap.Width, bitmap.Height),
+				SD.Imaging.ImageLockMode.ReadOnly,
+				bitmap.PixelFormat);
+			var bytes = new byte[bmpData.Height * Math.Abs(bmpData.Stride)];
+			Marshal.Copy(bmpData.Scan0, bytes, 0, bytes.Length);
+			for (int p = 3; p < bytes.Length; p += 4) {
+				if (bytes[p] != 255) {
+					bitmap.UnlockBits(bmpData);
+					return true;
+				}
+			}
+			bitmap.UnlockBits(bmpData);
+			return false;
 		}
 
 		#region IDisposable Support
