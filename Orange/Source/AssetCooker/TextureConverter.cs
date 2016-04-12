91a2e1fe47f0b4cff5ca7f647217f70ccc354197
nburimenko <bolya2003@bk.ru>
2016-04-12 10:35:36 +1000

    [Orange]: Textures: Clean code style. Use StringBuilder.

--- Orange/Source/AssetCooker/TextureConverter.cs
+++ Orange/Source/AssetCooker/TextureConverter.cs
@@ -1,15 +1,14 @@
 using System;
 using System.IO;
-using System.Net;
-using Gtk;
-using Lime;
-using System.Runtime.InteropServices;
+using System.Text;
+
+using Gdk;
 
 namespace Orange
 {
 	public static class TextureConverter
 	{
-		public static void ToPVR(Gdk.Pixbuf pixbuf, string dstPath, bool mipMaps, PVRFormat pvrFormat)
+		public static void ToPVR(Pixbuf pixbuf, string dstPath, bool mipMaps, PVRFormat pvrFormat)
 		{
 			int width = pixbuf.Width;
 			int height = pixbuf.Height;
@@ -17,59 +16,61 @@ namespace Orange
 
 			int potWidth = TextureConverterUtils.GetNearestPowerOf2(width, 8, 1024);
 			int potHeight = TextureConverterUtils.GetNearestPowerOf2(height, 8, 1024);
-			string args = "";
+			var args = new StringBuilder();
 			switch (pvrFormat) {
 				case PVRFormat.PVRTC4:
-					if (!pixbuf.HasAlpha)
-						args += " -f PVRTC1_2";
-					 else
-						args += " -f PVRTC1_4";
+					if (!pixbuf.HasAlpha) {
+						args.Append(" -f PVRTC1_2");
+					} else {
+						args.Append(" -f PVRTC1_4");
+					}
 					width = height = Math.Max(potWidth, potHeight);
 					break;
 				case PVRFormat.PVRTC4_Forced:
-					args += " -f PVRTC1_4";
+					args.Append(" -f PVRTC1_4");
 					width = height = Math.Max(potWidth, potHeight);
 					break;
 				case PVRFormat.PVRTC2:
-					args += " -f PVRTC1_2";
+					args.Append(" -f PVRTC1_2");
 					width = height = Math.Max(potWidth, potHeight);
 					break;
 				case PVRFormat.ETC1:
-					args = " -f ETC1 -q etcfast";
+					args.Append(" -f ETC1 -q etcfast");
 					break;
 				case PVRFormat.RGB565:
 					if (hasAlpha) {
-						Console.WriteLine("WARNING: texture has alpha channel. Used 'RGBA4444' format instead of 'RGB565'.");
-						args += " -f r4g4b4a4 -dither";
+						Console.WriteLine("WARNING: texture has alpha channel. " +
+							"Used 'RGBA4444' format instead of 'RGB565'.");
+						args.Append(" -f r4g4b4a4 -dither");
 					} else {
-						args += " -f r5g6b5";
+						args.Append(" -f r5g6b5");
 					}
 					break;
 				case PVRFormat.RGBA4:
-					args += " -f r4g4b4a4 -dither";
+					args.Append(" -f r4g4b4a4 -dither");
 					break;
 				case PVRFormat.ARGB8:
-					args += " -f r8g8b8a8";
+					args.Append(" -f r8g8b8a8");
 					break;
 			}
 			string tga = Path.ChangeExtension(dstPath, ".tga");
 			try {
 				if (pixbuf.HasAlpha) {
-					args += " -l"; // Enable alpha bleed
+					args.Append(" -l"); // Enable alpha bleed
 				}
 				TextureConverterUtils.SwapRGBChannels(pixbuf);
 				TextureConverterUtils.SaveToTGA(pixbuf, tga);
 				if (mipMaps) {
-					args += " -m";
+					args.Append(" -m");
 				}
-				args += String.Format(" -i \"{0}\" -o \"{1}\" -r {2},{3} -shh", tga, dstPath, width, height);
+				args.AppendFormat(" -i \"{0}\" -o \"{1}\" -r {2},{3} -shh", tga, dstPath, width, height);
 #if MAC
 				var pvrTexTool = Path.Combine(Toolbox.GetApplicationDirectory(), "Toolchain.Mac", "PVRTexTool");
 				Mono.Unix.Native.Syscall.chmod(pvrTexTool, Mono.Unix.Native.FilePermissions.S_IXOTH | Mono.Unix.Native.FilePermissions.S_IXUSR);
 #else
 				var pvrTexTool = Path.Combine(Toolbox.GetApplicationDirectory(), "Toolchain.Win", "PVRTexToolCli");
 #endif
-				int result = Process.Start(pvrTexTool, args);
+				int result = Process.Start(pvrTexTool, args.ToString());
 				if (result != 0) {
 					throw new Lime.Exception("Failed to convert '{0}' to PVR format(error code: {1})", tga, result);
 				}
@@ -83,7 +84,7 @@ namespace Orange
 			while (true) {
 				try {
 					File.Delete(file);
-				} catch (System.Exception) {
+				} catch (Exception) {
 					System.Threading.Thread.Sleep(100);
 					continue;
 				}
@@ -91,7 +92,7 @@ namespace Orange
 			}
 		}
 
-		public static void ToDDS(Gdk.Pixbuf pixbuf, string dstPath, DDSFormat format, bool mipMaps)
+		public static void ToDDS(Pixbuf pixbuf, string dstPath, DDSFormat format, bool mipMaps)
 		{
 			bool compressed = format == DDSFormat.DXTi;
 			if (pixbuf.HasAlpha) {
@@ -106,10 +107,11 @@ namespace Orange
 			// Do not delete the bitmap if an exception has occurred
 			File.Delete(tga);
 		}
-		
-		private static void ToDDSTextureHelper(string srcPath, string dstPath, bool hasAlpha, bool compressed, bool mipMaps)
+
+		private static void ToDDSTextureHelper(
+			string srcPath, string dstPath, bool hasAlpha, bool compressed, bool mipMaps)
 		{
-			string mipsFlag = mipMaps ? "" : "-nomips";
+			string mipsFlag = mipMaps ? string.Empty : "-nomips";
 			string compressionMethod;
 			if (compressed) {
 				compressionMethod = hasAlpha ? "-bc3" : "-bc1";
@@ -126,16 +128,16 @@ namespace Orange
 			string nvcompress = Path.Combine(Toolbox.GetApplicationDirectory(), "Toolchain.Mac", "nvcompress");
 			Mono.Unix.Native.Syscall.chmod(nvcompress, Mono.Unix.Native.FilePermissions.S_IXOTH | Mono.Unix.Native.FilePermissions.S_IXUSR);
 #endif
-			srcPath = Path.Combine(System.IO.Directory.GetCurrentDirectory(), srcPath);
-			dstPath = Path.Combine(System.IO.Directory.GetCurrentDirectory(), dstPath);
-			string args = String.Format("{0} {1} \"{2}\" \"{3}\"", mipsFlag, compressionMethod, srcPath, dstPath);
+			srcPath = Path.Combine(Directory.GetCurrentDirectory(), srcPath);
+			dstPath = Path.Combine(Directory.GetCurrentDirectory(), dstPath);
+			string args = string.Format("{0} {1} \"{2}\" \"{3}\"", mipsFlag, compressionMethod, srcPath, dstPath);
 			int result = Process.Start(nvcompress, args, Process.Options.RedirectErrors);
 			if (result != 0) {
 				throw new Lime.Exception("Failed to convert '{0}' to DDS format(error code: {1})", srcPath, result);
 			}
 		}
 
-		public static void ToJPG(Gdk.Pixbuf pixbuf, string dstPath, bool mipMaps)
+		public static void ToJPG(Pixbuf pixbuf, string dstPath, bool mipMaps)
 		{
 			if (pixbuf.HasAlpha) {
 				TextureConverterUtils.BleedAlpha(pixbuf);
@@ -143,7 +145,7 @@ namespace Orange
 			pixbuf.Savev(dstPath, "jpeg", new string[] { "quality" }, new string[] { "80" });
 		}
 
-		public static void ToGrayscaleAlphaPNG(Gdk.Pixbuf pixbuf, string dstPath, bool mipMaps)
+		public static void ToGrayscaleAlphaPNG(Pixbuf pixbuf, string dstPath, bool mipMaps)
 		{
 			TextureConverterUtils.ConvertBitmapToGrayscaleAlphaMask(pixbuf);
 			pixbuf.Save(dstPath, "png");
@@ -152,17 +154,18 @@ namespace Orange
 			// -s - silent
 			// -c 0 - change color type to greyscale
 			dstPath = MakeAbsolutePath(dstPath);
-			var args = String.Format("-ow -s -c 0 \"{0}\"", dstPath);
+			var args = string.Format("-ow -s -c 0 \"{0}\"", dstPath);
 			int result = Process.Start(pngcrushTool, args, Process.Options.RedirectErrors);
 			if (result != 0) {
-				throw new Lime.Exception("Error converting '{0}'\nCommand line: {1}", dstPath, pngcrushTool + " " + args);
+				throw new Lime.Exception(
+					"Error converting '{0}'\nCommand line: {1}", dstPath, pngcrushTool + ' ' + args);
 			}
 		}
 
 		private static string MakeAbsolutePath(string path)
 		{
 			if (!Path.IsPathRooted(path)) {
-				path = Path.Combine(System.IO.Directory.GetCurrentDirectory(), path);
+				path = Path.Combine(Directory.GetCurrentDirectory(), path);
 			}
 			return path;
 		}
