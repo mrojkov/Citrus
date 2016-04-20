6b40f4bf505050df854668d211f7e3ac12fe2aa0
Andrey Andreev <aandreev@game-forest.com>
2016-04-20 14:21:49 +1000

    Add iOS project

---
+++ Samples/EmptyProject/EmptyProject.iOS/AppDelegate.cs
@@ -0,0 +1,29 @@
+﻿using AVFoundation;
+using Foundation;
+using UIKit;
+
+namespace EmptyProject.iOS
+{
+	[Register ("EmptyProjectAppDelegate")]
+	public class EmptyProjectAppDelegate : UIApplicationDelegate
+	{
+
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+		{
+			AVAudioSession.SharedInstance ().SetCategory (AVAudioSessionCategory.Ambient);
+			Lime.Serialization.Serializer = new Serializer ();
+			Lime.Application.Initialize (new Lime.ApplicationOptions {
+				DecodeAudioInSeparateThread = false,
+				UsingDeferredHitTest = true
+			});
+			return true;
+		}
+
+		public static void Main(string[] args)
+		{
+			UIKit.UIApplication.Main (args, null, "EmptyProjectAppDelegate");
+		}
+	}
+}
+
+
