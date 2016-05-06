2e3c726d6157dc0546e895f2148caecef45504c6
Nika Burimenko <bolya2003@bk.ru>
2016-05-06 14:51:44 +1000

    [Project]: Mac: Create Mac project.

---
+++ Samples/EmptyProject/EmptyProject.Mac/Application.cs
@@ -0,0 +1,20 @@
+﻿using AppKit;
+using System;
+
+namespace EmptyProject.Mac
+{
+	static class Application
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			if (Array.IndexOf(args, "--GenerateSerializationAssembly") >= 0) {
+				Lime.Environment.GenerateSerializationAssembly("Serializer", typeof(EmptyProject.Application.AppData));
+				return;
+			}
+			Lime.Application.Initialize(new Lime.ApplicationOptions { DecodeAudioInSeparateThread = true });
+			NSApplication.SharedApplication.Delegate = new AppDelegate();
+			Lime.Application.Run();
+		}
+	}
+}
