47b09173b104a3bf1445231f9c282c43f158cd59
Nika Burimenko <bolya2003@bk.ru>
2016-05-13 11:42:02 +1000

    [Application]: iOS: Fix code style.

--- Samples/EmptyProject/EmptyProject.iOS/AppDelegate.cs
+++ Samples/EmptyProject/EmptyProject.iOS/AppDelegate.cs
@@ -7,22 +7,21 @@ namespace EmptyProject.iOS
 	[Register ("EmptyProjectAppDelegate")]
 	public class EmptyProjectAppDelegate : Lime.AppDelegate
 	{
-
-		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+		public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
 		{
-			AVAudioSession.SharedInstance ().SetCategory (AVAudioSessionCategory.Ambient);
-			Lime.Serialization.Serializer = new Serializer ();
-			Lime.Application.Initialize (new Lime.ApplicationOptions {
+			AVAudioSession.SharedInstance().SetCategory(AVAudioSessionCategory.Ambient);
+			Lime.Serialization.Serializer = new Serializer();
+			Lime.Application.Initialize(new Lime.ApplicationOptions {
 				DecodeAudioInSeparateThread = false,
 				UsingDeferredHitTest = true
 			});
-			new EmptyProject.Application.Application ();
+			new EmptyProject.Application.Application();
 			return true;
 		}
 
 		public static void Main(string[] args)
 		{
-			UIKit.UIApplication.Main (args, null, "EmptyProjectAppDelegate");
+			UIApplication.Main(args, null, "EmptyProjectAppDelegate");
 		}
 	}
 }
