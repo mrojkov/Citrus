9dee06cc14e23d9b40716ceb785ab9e727d20bf1
Mikhail Rozhkov <mrozhkov@game-forest.com>
2016-06-06 13:53:17 +1000

    [LayoutDebugPresenter] Minor rework

--- Lime/Source/Widgets/Layout/LayoutDebugPresenter.cs
+++ Lime/Source/Widgets/Layout/LayoutDebugPresenter.cs
@@ -4,19 +4,22 @@ namespace Lime
 {
 	public class LayoutDebugPresenter : CustomPresenter
 	{
-		public Color4 Color = Color4.Red;
-		public float Thickness;
-		
-		public LayoutDebugPresenter(IPresenter previous) : base(previous) { }
+		public readonly Color4 Color;
+		public readonly float Thickness;
 
+		public LayoutDebugPresenter(Color4 color, float thickness = 0)
+		{
+			Color = color;
+			Thickness = thickness;
+		}
+		
 		public override void Render(Node node)
 		{
-			base.Render(node);
 			var widget = node.AsWidget;
 			if (widget == null || widget.Layout == null)
 				return;
 			widget.PrepareRendererState();
-			var t = Thickness > 0 ? Thickness : 1 / Window.Current.PixelScale;
+			var t = Thickness > 0 ? Thickness : 1 / CommonWindow.Current.PixelScale;
 			foreach (var r in widget.Layout.DebugRectangles) {
 				Renderer.DrawRectOutline(r.A, r.B, Color, t);
 			}
