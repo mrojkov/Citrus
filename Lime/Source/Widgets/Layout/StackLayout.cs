296668858de9106138f93ac90a0d468c8b48299f
Mikhail Rozhkov <mrozhkov@game-forest.com>
2016-06-29 09:16:44 +1000

    Replace ScrollableLayout with StackLayout

--- Lime/Source/Widgets/Layout/StackLayout.cs
+++ Lime/Source/Widgets/Layout/StackLayout.cs
@@ -6,6 +6,9 @@ namespace Lime
 {
 	public class StackLayout : CommonLayout, ILayout
 	{
+		public bool HorizontallySizeable { get; set; }
+		public bool VerticallySizeable { get; set; }
+
 		public StackLayout()
 		{
 			DebugRectangles = new List<Rectangle>();
@@ -20,19 +23,40 @@ namespace Lime
 				widget.MeasuredMaxSize = Vector2.PositiveInfinity;
 				return;
 			}
-			var minSize = new Vector2(widgets.Max(i => i.EffectiveMinSize.X), widgets.Max(i => i.EffectiveMinSize.Y));
-			var maxSize = new Vector2(widgets.Max(i => i.EffectiveMaxSize.X), widgets.Max(i => i.EffectiveMaxSize.Y));
-			widget.MeasuredMinSize = minSize + widget.Padding;
-			widget.MeasuredMaxSize = maxSize + widget.Padding;
+			var minSize = new Vector2(widgets.Max(i => i.EffectiveMinSize.X), widgets.Max(i => i.EffectiveMinSize.Y)) + widget.Padding;
+			var maxSize = new Vector2(widgets.Max(i => i.EffectiveMaxSize.X), widgets.Max(i => i.EffectiveMaxSize.Y)) + widget.Padding;
+			if (HorizontallySizeable) {
+				minSize.X = 0;
+				maxSize.X = float.PositiveInfinity;
+			}
+			if (VerticallySizeable) {
+				minSize.Y = 0;
+				maxSize.Y = float.PositiveInfinity;
+			}
+			widget.MeasuredMinSize = minSize;
+			widget.MeasuredMaxSize = maxSize;
 		}
 
 		public override void ArrangeChildren(Widget widget)
 		{
 			DebugRectangles.Clear();
 			ArrangementValid = true;
-			foreach (var w in GetChildren(widget)) {
-				LayoutWidgetWithinCell(w, widget.ContentPosition, widget.ContentSize, DebugRectangles);
+			foreach (var child in GetChildren(widget)) {
+				var position = widget.ContentPosition;
+				var size = widget.ContentSize;
+				var align = (widget.LayoutCell ?? LayoutCell.Default).Alignment;
+				if (HorizontallySizeable) {
+					position.X = child.X;
+					size.X = child.EffectiveMinSize.X;
+					align.X = HAlignment.Left;
+				}
+				if (VerticallySizeable) {
+					position.Y = child.Y;
+					size.Y = child.EffectiveMinSize.Y;
+					align.Y = VAlignment.Top;
+				}
+				LayoutWidgetWithinCell(child, position, size, align, DebugRectangles);
 			}
 		}
-	}	
+	}
 }
