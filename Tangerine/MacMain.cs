5dc2999f1d07685a611e494cf544b0d812020107
konstantin egorov <kegorov@game-forest.com>
2016-07-08 17:57:29 +1000

    [Tangerine] Simplify projects structure

---
+++ Tangerine/MacMain.cs
@@ -0,0 +1,26 @@
+﻿#if MAC
+using AppKit;
+using Foundation;
+using Tangerine.Core;
+
+namespace Tangerine
+{
+	public class AppDelegate : NSApplicationDelegate
+	{
+		public override void DidFinishLaunching(NSNotification notification)
+		{
+			TangerineApp.Initialize();
+		}
+	}
+	
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			Lime.Application.Initialize();
+			NSApplication.SharedApplication.Delegate = new AppDelegate();
+			Lime.Application.Run();
+		}
+	}
+}
+#endif
