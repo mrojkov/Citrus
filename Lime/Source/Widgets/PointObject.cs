6686ad632c12260e34efd16ffb777006a643fe50
Mikhail Rozhkov <mrozhkov@game-forest.com>
2016-08-06 09:51:31 +1000

    Remove russian comments

--- Lime/Source/Widgets/PointObject.cs
+++ Lime/Source/Widgets/PointObject.cs
@@ -6,9 +6,6 @@ using ProtoBuf;
 
 namespace Lime
 {
-	/// <summary>
-	/// Точечный объект
-	/// </summary>
 	[ProtoContract]
 	[ProtoInclude(101, typeof(SplinePoint))]
 	[ProtoInclude(102, typeof(DistortionMeshPoint))]
@@ -23,25 +20,13 @@ namespace Lime
 			Presenter = null;
 		}
 
-		/// <summary>
-		/// Позиция объекта. Может быть представлена как в пикселях, так и в нормализованном значении (как именно, зависит от класса-наследника)
-		/// </summary>
 		[ProtoMember(1)]
 		public Vector2 Position { get { return position; } set { position = value; } }
 
-		/// <summary>
-		/// Координата X (аналогично Position.X)
-		/// </summary>
 		public float X { get { return position.X; } set { position.X = value; } }
 
-		/// <summary>
-		/// Координата Y (аналогично Position.Y)
-		/// </summary>
 		public float Y { get { return position.Y; } set { position.Y = value; } }
 
-		/// <summary>
-		/// Веса костей (используется для скеленой анимации)
-		/// </summary>
 		[ProtoMember(2)]
 		public SkinningWeights SkinningWeights { get; set; }
 	}
