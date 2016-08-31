c53d2f0623bfc766f97a195cd0d986ae980e0712
Mikhail Rozhkov <mrozhkov@game-forest.com>
2016-08-31 17:30:29 +1000

    [Debug] Make Write method null-aware. Remove russian comments.

--- Lime/Source/Application/Debug.cs
+++ Lime/Source/Application/Debug.cs
@@ -5,36 +5,20 @@ using System.Text;
 
 namespace Lime
 {
-	/// <summary>
-	/// Предоставляет отладочные методы
-	/// </summary>
 	public static class Debug
 	{
-		/// <summary>
-		/// Остановить выполнение программы, когда будет нажата кнопка (виджет типа Button) (аналогично брейкпоинту)
-		/// </summary>
 		public static bool BreakOnButtonClick { get; set; }
 		
-		/// <summary>
-		/// Выводит сообщение в лог
-		/// </summary>
 		public static void Write(string message)
 		{
 			Logger.Write(message);
 		}
 
-		/// <summary>
-		/// Выводит сообщение в лог
-		/// </summary>
-		/// <param name="value">Будет преобразовано в строку</param>
 		public static void Write(object value)
 		{
-			Write(value.ToString());
+			Write(value != null ? value.ToString() : "null");
 		}
 
-		/// <summary>
-		/// Выводит сообщение в лог
-		/// </summary>
 		public static void Write(string msg, params object[] args)
 		{
 			Logger.Write(msg, args);
