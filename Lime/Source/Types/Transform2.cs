ddc475bd224be0900499fdb68fa8158c89f2650d
Mikhail Rozhkov <mrozhkov@game-forest.com>
2016-09-10 12:32:08 +1000

    Move Quadrangle, Transform2 into /Types folder.

---
+++ Lime/Source/Types/Transform2.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Lime
+{
+	public struct Transform2
+	{
+		public Vector2 Position;
+		public Vector2 Scale;
+		public float Rotation;
+
+		public static Transform2 Lerp(float t, Transform2 a, Transform2 b)
+		{
+			return new Transform2 {
+				Position = Mathf.Lerp(t, a.Position, b.Position),
+				Scale = Mathf.Lerp(t, a.Scale, b.Scale),
+				Rotation = Mathf.Lerp(t, a.Rotation, b.Rotation),
+			};
+		}
+
+		public Matrix32 ToMatrix32()
+		{
+			var v = Vector2.CosSin(Rotation * Mathf.DegToRad) * Scale;
+			return new Matrix32 {
+				U = v.X * new Vector2(1, 0) + v.Y * new Vector2(0, 1),
+				V = v.X * new Vector2(0, 1) - v.Y * new Vector2(1, 0),
+				T = Position
+			};
+		}
+	}	
+}
