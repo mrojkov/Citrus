e71908f21091bec36dd9a6fd4355f651c275ecbd
Denis Rotanov <mapholameth@gmail.com>
2016-09-14 16:05:41 +1000

    fix Tangerine compilation for Windows

--- Tangerine/Main.Win.cs
+++ Tangerine/Main.Win.cs
@@ -7,22 +7,9 @@ namespace Tangerine
 {
 	public class MainApplication
 	{
-		private static List<Type> GetSerializationTypes()
-		{
-			var result = new List<Type>() {
-				//typeof(Type)
-			};
-			return result;
-		}
-
 		[STAThread]
 		public static void Main(string[] args)
 		{
-			if (Array.IndexOf(args, "--GenerateSerializationAssembly") >= 0) {
-				Lime.Environment.GenerateSerializationAssembly("Serializer", GetSerializationTypes().ToArray());
-				return;
-			}
-
 			var thisExe = System.Reflection.Assembly.GetExecutingAssembly();
 			string [] resources = thisExe.GetManifestResourceNames();
 			Lime.Application.Initialize(new ApplicationOptions {
