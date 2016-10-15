57cba1180f2ebe313d4659680123abb3105d4e3e
Mikhail Rozhkov <mrozhkov@game-forest.com>
2016-10-15 16:19:51 +1000

    [Lime] Add FolderBegin/FolderEnd classes for compatibility with HotStudio.

---
+++ Lime/Source/Widgets/Folder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lime
+{
+	public class FolderBegin : Node
+	{
+		public bool Expanded { get; set; }
+
+		public FolderBegin()
+		{
+			Expanded = true;
+		}
+	}
+
+	public class FolderEnd : Node
+	{
+	}
+}
