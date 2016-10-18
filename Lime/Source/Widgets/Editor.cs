mrojkov/Citrus

commit 38b30b25e8a6851a3a220f970da6f61aed2e8931

[Editor] Do not insert characters missing from the font
This usually includes emoji.

Lime/Source/Widgets/Editor.cs
@@ -115,6 +115,7 @@ namespace Lime
 		float PasswordLastCharShowTime { get; set; }
 		Predicate<string> AcceptText { get; set; }
 		ScrollView Scroll { get; set; }
+		bool AllowNonDisplayableChars { get; set; }
 
 		bool IsAcceptableLength(int length);
 		bool IsAcceptableLines(int lines);
@@ -130,6 +131,7 @@ namespace Lime
 		public float PasswordLastCharShowTime { get; set; }
 		public Predicate<string> AcceptText { get; set; }
 		public ScrollView Scroll { get; set; }
+		public bool AllowNonDisplayableChars { get; set; }
 
 		public EditorParams()
 		{
@@ -228,6 +230,7 @@ namespace Lime
 		{
 			if (caretPos.TextPos < 0 || caretPos.TextPos > Text.Text.Length) return;
 			if (!EditorParams.IsAcceptableLength(Text.Text.Length + 1)) return;
+			if (!EditorParams.AllowNonDisplayableChars && !Text.CanDisplay(ch)) return;
 			var newText = Text.Text.Insert(caretPos.TextPos, ch.ToString());
 			if (EditorParams.AcceptText != null && !EditorParams.AcceptText(newText)) return;
 			if (EditorParams.MaxHeight > 0 && !EditorParams.IsAcceptableHeight(CalcTextHeight(newText))) return;

commit 23e12d4389c951677819ff15d109469cef878cfa

[Editor] Fix inserting line breaks
Broken by 38b30b25e8a6851a3a220f970da6f61aed2e8931

Lime/Source/Widgets/Editor.cs
@@ -235,7 +235,7 @@ namespace Lime
 		{
 			if (caretPos.TextPos < 0 || caretPos.TextPos > Text.Text.Length) return;
 			if (!EditorParams.IsAcceptableLength(Text.Text.Length + 1)) return;
-			if (!EditorParams.AllowNonDisplayableChars && !Text.CanDisplay(ch)) return;
+			if (ch != '\n' && !EditorParams.AllowNonDisplayableChars && !Text.CanDisplay(ch)) return;
 			var newText = Text.Text.Insert(caretPos.TextPos, ch.ToString());
 			if (EditorParams.AcceptText != null && !EditorParams.AcceptText(newText)) return;
 			if (EditorParams.MaxHeight > 0 && !EditorParams.IsAcceptableHeight(CalcTextHeight(newText))) return;