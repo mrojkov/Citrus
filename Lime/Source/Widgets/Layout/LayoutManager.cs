e7faa15e673211464f527ab5c51510434f1b81c0
Mikhail Rozhkov <mrozhkov@game-forest.com>
2016-10-19 10:30:59 +1000

    [LayoutManager] Add LayoutChanged event.

--- Lime/Source/Widgets/Layout/LayoutManager.cs
+++ Lime/Source/Widgets/Layout/LayoutManager.cs
@@ -5,6 +5,8 @@ namespace Lime
 {
 	public class LayoutManager
 	{
+		public event Action LayoutChanged;
+
 		// ArrangeQueue has root-nodes first order to minimize ArrangeChildren calls.
 		private DepthOrderedQueue arrangeQueue = new DepthOrderedQueue(rootToLeavesOrder: true);
 		// MeasureQueue has leaf-nodes first order, because widget size constraints depends only on the widget's children constraints.
@@ -24,6 +26,7 @@ namespace Lime
 
 		public void Layout()
 		{
+			var changed = false;
 			while (true) {
 				var w = measureQueue.Dequeue();
 				if (w == null) {
@@ -42,6 +45,10 @@ namespace Lime
 				// Keep in mind: ArrangeChildren could force a child re-arrangement when changes a child size.
 				// See ILayout.OnSizeChanged implementation.
 				w.Layout.ArrangeChildren(w);
+				changed = true;
+			}
+			if (changed) {
+				LayoutChanged?.Invoke();
 			}
 		}
 
