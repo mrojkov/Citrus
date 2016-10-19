c5b5b83c62f07010e76bb06145fdbb089fabf513
Mikhail Rozhkov <mrozhkov@game-forest.com>
2016-10-19 19:27:24 +1000

    [Tangerine] Add ClampScrollPosOnContainerChange processor.

---
+++ Tangerine/Tangerine.UI.Timeline/Processors/GridPane/ClampScrollPosOnContainerChange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Lime;
+using Tangerine.Core;
+
+namespace Tangerine.UI.Timeline
+{
+	public class ClampScrollPosOnContainerChange
+	{
+		public IProcessor GetProcessor()
+		{
+			return new Property<Node>(() => Document.Current.Container).DistinctUntilChanged().Consume(_ => {
+				var rows = Document.Current.SelectedRows;
+				if (rows.Count > 0) {
+					Timeline.Instance.EnsureRowVisible(rows[0]);
+				}
+			});
+		}
+	}
+}
