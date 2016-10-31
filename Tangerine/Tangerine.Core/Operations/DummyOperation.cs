842110ef05959332ffdc924a1d56dce3c3b5d261
Mikhail Rozhkov <mrozhkov@game-forest.com>
2016-10-31 11:11:48 +1000

    [Tangerine] Add DummyOperation.
    Useful for updating the document state, e.g. after document loading or changing the timeline layout (it is need to update the visible column count).

---
+++ Tangerine/Tangerine.Core/Operations/DummyOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tangerine.Core.Operations
+{
+	public class Dummy : Operation
+	{
+		public override bool IsChangingDocument => false;
+
+		public static void Perform()
+		{
+			Document.Current.History.Perform(new Dummy());
+		}
+
+		private Dummy() { }
+	}
+}
