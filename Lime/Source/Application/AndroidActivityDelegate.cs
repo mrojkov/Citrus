0fda23624b88c440d31bf478fe391f6467ea3bac
nburimenko <bolya2003@bk.ru>
2016-11-03 15:07:04 +1000

    [Android]: Fix screen flicker when showing keyboard with enabled fullscreen.

--- Lime/Source/Application/AndroidActivityDelegate.cs
+++ Lime/Source/Application/AndroidActivityDelegate.cs
@@ -79,6 +79,10 @@ namespace Lime
 			if (Created != null) {
 				Created(Activity, bundle);
 			}
+
+			Activity.Window.Attributes.Flags |=
+				Android.Views.WindowManagerFlags.LayoutInScreen |
+				Android.Views.WindowManagerFlags.LayoutNoLimits;
 		}
 
 		private void RemoveGameViewFromParent()
