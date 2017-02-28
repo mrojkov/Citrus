5ffc745f756adf96be2e2b4d84a66ff07f523f4a
Mikhail Rozhkov <mrozhkov@game-forest.com>
2017-02-28 17:15:41 +1000

    [SceneView] Fix node icon size.

--- Tangerine/Tangerine.UI.SceneView/Presenters/SelectedWidgetsPresenter.cs
+++ Tangerine/Tangerine.UI.SceneView/Presenters/SelectedWidgetsPresenter.cs
@@ -26,10 +26,11 @@ namespace Tangerine.UI.SceneView
 				return;
 			}
 			// Render node icons.
+			var iconSize = new Vector2(16, 16);
 			foreach (var widget in widgets) {
 				var t = NodeIconPool.GetTexture(widget.GetType());
 				var p = widget.CalcPositionInSpaceOf(canvas);
-				Renderer.DrawSprite(t, Color4.White, p - Vector2.Floor((Vector2)t.ImageSize / 2), (Vector2)t.ImageSize, Vector2.Zero, Vector2.One);
+				Renderer.DrawSprite(t, Color4.White, p - iconSize / 2, iconSize, Vector2.Zero, Vector2.One);
 			}
 			Quadrangle hull;
 			Vector2 pivot;
