e8f08c35941a2a6f79dce38371b1733d5d436c87
Mikhail Rozhkov <mrozhkov@game-forest.com>
2017-04-11 11:56:00 +1000

    [Tangerine] Refactor MouseSelectionProcessor.

--- Tangerine/Tangerine.UI.SceneView/Processors/MouseSelectionProcessor.cs
+++ Tangerine/Tangerine.UI.SceneView/Processors/MouseSelectionProcessor.cs
@@ -6,67 +6,61 @@ using Tangerine.Core;
 
 namespace Tangerine.UI.SceneView
 {
-	class MouseSelectionProcessor : ITaskProvider
+	public class MouseSelectionProcessor : ITaskProvider
 	{
-		public IEnumerator<object> Task()
+		public static readonly List<ISelectionTester> SelectionTesters = new List<ISelectionTester>();
+
+		static MouseSelectionProcessor()
 		{
-			var sv = SceneView.Instance;
-			var input = sv.Input;
-			while (true) {
-				if (input.WasMousePressed() && !input.IsKeyPressed(Key.Shift)) {
-					var rect = new Rectangle(sv.MousePosition, sv.MousePosition);
-					var presenter = new DelegatePresenter<Widget>(w => {
-						w.PrepareRendererState();
-						var t = sv.Scene.CalcTransitionToSpaceOf(sv.Frame);
-						Renderer.DrawRectOutline(rect.A * t, rect.B * t, ColorTheme.Current.SceneView.MouseSelection, 1);
-					});
-					sv.Frame.CompoundPostPresenter.Add(presenter);
-					input.CaptureMouse();
-					var clicked = true;
-					var selectedNodes = Document.Current.SelectedNodes().Editable().ToList();
-					Document.Current.History.BeginTransaction();
-					while (input.IsMousePressed()) {
-						rect.B = sv.MousePosition;
-						clicked &= (rect.B - rect.A).Length <= 5;
-						if (!clicked) {
-							RefreshSelectedNodes(rect, selectedNodes);
-							CommonWindow.Current.Invalidate();
-						}
-						yield return null;
-					}
-					input.ReleaseMouse();
-					if (clicked) {
-						SelectByClick(rect.A);
-					}
-					sv.Frame.CompoundPostPresenter.Remove(presenter);
-					CommonWindow.Current.Invalidate();
-					Document.Current.History.EndTransaction();
-				}
-				yield return null;
-			}
+			SelectionTesters.Add(new WidgetSelectionTester());
+			SelectionTesters.Add(new PointObjectSelectionTester());
 		}
 
-		void SelectByClick(Vector2 point)
+		public IEnumerator<object> Task()
 		{
-			var sv = SceneView.Instance;
-			var ctrlPressed = sv.Input.IsKeyPressed(Key.Control);
-			if (!ctrlPressed) {
-				Core.Operations.ClearRowSelection.Perform();
-			}
-			foreach (var widget in Document.Current.Container.Nodes.Editable().OfType<Widget>()) {
-				var hull = widget.CalcHullInSpaceOf(sv.Scene);
-				if (hull.Contains(point)) {
-					Core.Operations.SelectNode.Perform(widget,
-						!ctrlPressed || !Document.Current.SelectedNodes().Contains(widget));
-					break;
+			var sceneView = SceneView.Instance;
+			var input = sceneView.Input;
+			while (true) {
+				if (input.WasMousePressed() && !input.IsKeyPressed(Key.Shift)) {
+					var rect = new Rectangle(sceneView.MousePosition, sceneView.MousePosition);
+					var presenter = new DelegatePresenter<Widget>(w => {
+						w.PrepareRendererState();
+						var t = sceneView.Scene.CalcTransitionToSpaceOf(sceneView.Frame);
+						Renderer.DrawRectOutline(rect.A * t, rect.B * t, ColorTheme.Current.SceneView.MouseSelection);
+					});
+					sceneView.Frame.CompoundPostPresenter.Add(presenter);
+					input.CaptureMouse();
+					Document.Current.History.BeginTransaction();
+					try {
+						var clicked = true;
+						var selectedNodes = Document.Current.SelectedNodes().Editable().ToList();
+						while (input.IsMousePressed()) {
+							rect.B = sceneView.MousePosition;
+							clicked &= (rect.B - rect.A).Length <= 5;
+							if (!clicked)
+								RefreshSelectedNodes(rect, selectedNodes);
+							CommonWindow.Current.Invalidate();
+							yield return null;
+						}
+						if (clicked) {
+							var controlPressed = SceneView.Instance.Input.IsKeyPressed(Key.Control);
+							if (!controlPressed)
+								Core.Operations.ClearRowSelection.Perform();
+							foreach (var node in Document.Current.Container.Nodes.Editable()) {
+								if (Probe(node, rect.A)) {
+									Core.Operations.SelectNode.Perform(node, !controlPressed || !Document.Current.SelectedNodes().Contains(node));
+									break;
+								}
+							}
+						}
+						sceneView.Frame.CompoundPostPresenter.Remove(presenter);
+						CommonWindow.Current.Invalidate();
+					} finally {
+						input.ReleaseMouse();
+						Document.Current.History.EndTransaction();
+					}
 				}
-			}
-			foreach (var pobject in Document.Current.Container.Nodes.Editable().OfType<PointObject>()) {
-				var pos = pobject.CalcPositionInSpaceOf(sv.Scene);
-				if ((pos - point).Length <= 5) {
-					Core.Operations.SelectNode.Perform(pobject, !ctrlPressed || !Document.Current.SelectedNodes().Contains(pobject));
-					break;
-				}
+				yield return null;
 			}
 		}
 
@@ -75,7 +69,7 @@ namespace Tangerine.UI.SceneView
 			var ctrlPressed = SceneView.Instance.Input.IsKeyPressed(Key.Control);
 			var currentSelection = Document.Current.SelectedNodes();
 			var newSelection = Document.Current.Container.Nodes.Editable().Where(n =>
-				ctrlPressed ? TestNode(rect, n) ^ originalSelection.Contains(n) : TestNode(rect, n));
+				ctrlPressed ? Probe(n, rect) ^ originalSelection.Contains(n) : Probe(n, rect));
 			if (!newSelection.SequenceEqual(currentSelection)) {
 				Core.Operations.ClearRowSelection.Perform();
 				foreach (var node in newSelection) {
@@ -84,26 +78,60 @@ namespace Tangerine.UI.SceneView
 			}
 		}
 
-		bool TestNode(Rectangle rect, Node node)
+		bool Probe(Node node, Vector2 point) => SelectionTesters.Any(i => i.Probe(node, point));
+		bool Probe(Node node, Rectangle rectangle) => SelectionTesters.Any(i => i.Probe(node, rectangle));
+
+		public interface ISelectionTester
 		{
-			var nrect = rect.Normalized;
-			var canvas = SceneView.Instance.Scene;
-			if (node is Widget) {
-				var widget = (Widget)node;
+			bool Probe(Node node, Vector2 point);
+			bool Probe(Node node, Rectangle rectangle);
+		}
+
+		public abstract class SelectionTester<T> : ISelectionTester where T: Node
+		{
+			public bool Probe(Node node, Vector2 point) => (node is T) && ProbeInternal((T)node, point);
+			public bool Probe(Node node, Rectangle rectangle) => (node is T) && ProbeInternal((T)node, rectangle);
+
+			protected abstract bool ProbeInternal(T node, Vector2 point);
+			protected abstract bool ProbeInternal(T node, Rectangle rectangle);
+		}
+
+		public class WidgetSelectionTester : SelectionTester<Widget>
+		{
+			protected override bool ProbeInternal(Widget widget, Vector2 point)
+			{
+				var hull = widget.CalcHullInSpaceOf(SceneView.Instance.Scene);
+				return hull.Contains(point);
+			}
+
+			protected override bool ProbeInternal(Widget widget, Rectangle rectangle)
+			{
+				var canvas = SceneView.Instance.Scene;
 				var hull = widget.CalcHullInSpaceOf(canvas);
 				for (int i = 0; i < 4; i++) {
-					if (nrect.Contains(hull[i])) {
+					if (rectangle.Contains(hull[i])) {
 						return true;
 					}
 				}
 				var pivot = widget.CalcPositionInSpaceOf(canvas);
-				return nrect.Contains(pivot);
-			} else if (node is PointObject) {
-				var p = ((PointObject)node).TransformedPosition;
-				var t = node.Parent.AsWidget.CalcTransitionToSpaceOf(canvas);
-				return nrect.Contains(t * p);
+				return rectangle.Contains(pivot);
 			}
-			return false;
+		}
+
+		public class PointObjectSelectionTester : SelectionTester<PointObject>
+		{
+			protected override bool ProbeInternal(PointObject pobject, Vector2 point)
+			{
+				var pos = pobject.CalcPositionInSpaceOf(SceneView.Instance.Scene);
+				return SceneView.Instance.HitTestControlPoint(pos, 5);
+			}
+
+			protected override bool ProbeInternal(PointObject pobject, Rectangle rectangle)
+			{
+				var p = pobject.TransformedPosition;
+				var t = ((Widget)pobject.Parent).CalcTransitionToSpaceOf(SceneView.Instance.Scene);
+				return rectangle.Contains(t * p);
+			}
 		}
 	}
 }
