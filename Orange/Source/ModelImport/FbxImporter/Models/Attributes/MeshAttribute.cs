617081c1984d522ea3f22e4e704c98dc876c3c3c
rshutov <rshutov@game-forest.com>
2017-05-19 18:11:38 +1000

    [Orange][Fbx] Rework mesh attribute

--- Orange/Source/ModelImport/FbxImporter/Models/Attributes/MeshAttribute.cs
+++ Orange/Source/ModelImport/FbxImporter/Models/Attributes/MeshAttribute.cs
@@ -10,77 +10,64 @@ namespace Orange.FbxImporter
 {
 	public class MeshAttribute : NodeAttribute
 	{
-		#region Pinvokes
+		public int[] Indices { get; private set; }
 
-		[DllImport(ImportConfig.LibName, CallingConvention = CallingConvention.Cdecl)]
-		public static extern IntPtr CalcRelativeTransform(IntPtr node);
+		public Mesh3D.Vertex[] Vertices { get; private set; }
+
+		public override FbxNodeType Type { get; } = FbxNodeType.MESH;
+
+		public MeshAttribute(IntPtr ptr) : base(ptr)
+		{
+			var mesh = GetMeshAttribute(NativePtr).To<MeshData>();
+			Indices = mesh.vertices.ToIntArray(mesh.verticesCount);
+			Vertices = new Mesh3D.Vertex[mesh.verticesCount];
+			var colors = mesh.colors.ToStructArray<Vec4>(mesh.colorsCount);
+			var verices = mesh.points.ToStructArray<Vec3>(mesh.pointsCount);
+			var uv = mesh.uvCoords.ToStructArray<Vec2>(mesh.uvCount);
+			for (var i = 0; i < mesh.pointsCount; i++) {
+				var vertex = 
+				Vertices[i].Pos = new Vector3(
+					verices[i].V1,
+					verices[i].V2,
+					verices[i].V3);
+				Vertices[i].Color = colors.Length != 0 ?
+					Color4.FromFloats(
+						colors[i].V1,
+						colors[i].V2,
+						colors[i].V3,
+						colors[i].V4) :
+					Color4.White;
+				Vertices[i].UV1 = new Vector2(
+					uv[i].V1,
+					uv[i].V2
+				);
+			}
+		}	
+
+		public override string ToString(int level)
+		{
+			StringBuilder builder = new StringBuilder();
+			builder.Append("Points: ".ToLevel(level + 1));
+			if (Vertices == null) {
+				builder.AppendLine("None");
+			} else {
+				builder.AppendLine("[".ToLevel(level + 2));
+				for (int i =0; i < Vertices.Length; i+=3) {
+					builder.AppendLine($"[{Vertices[i]}, {Vertices[i + 1]}, {Vertices[i + 2]}],".ToLevel(level + 3));
+				}
+				builder.AppendLine("[".ToLevel(level + 2));
+			}
+			return builder.ToString();
+		}
+
+		#region Pinvokes
 
 		[DllImport(ImportConfig.LibName, CallingConvention = CallingConvention.Cdecl)]
 		public static extern IntPtr GetMeshAttribute(IntPtr node);
 
 		#endregion
 
-		public int[] Indices { get; private set; }
-		public Mesh3D.Vertex[] Points { get; private set; }
-		public Color4[] Colors { get; private set; }
-
-		//TODO: Add Lime Vector3
-
-		public MeshAttribute(IntPtr ptr) : base(ptr)
-		{
-			//TODO Fix this
-			var mesh = GetMeshAttribute(NativePtr).To<MeshData>();
-			Indices = mesh.vertices.ToIntArray(mesh.verticesCount);
-			Points = new Mesh3D.Vertex[mesh.pointsCount];
-			Colors = new Color4[mesh.colorsCount];
-			var points = mesh.points.ToDoubleArray(mesh.pointsCount);
-			for (int i = 0; i < points.Length * 4; i +=3 ) {
-				try {
-					Points[i / 3] = new Mesh3D.Vertex {
-						Pos = new Vector3(
-								(float)points[i],
-								(float)points[i + 1],
-								(float)points[i + 2])
-					};
-				} catch(System.Exception e) {
-					var j = i;
-				}
-				
-			}
-
-			var colors = mesh.colors.ToDoubleArray(mesh.colorsCount);
-			if (colors != null) {
-				for (int i = 0; i < colors.Length * 2; i += 4) {
-					Colors[i / 4] = Color4.FromFloats(
-						(float)colors[i],
-						(float)colors[i + 1],
-						(float)colors[i + 2],
-						(float)colors[i + 3]);
-				}
-			}
-		}	
-
-		//TODO: Add Lime matrix as return type
-		public Matrix44 CalcGlobalTransform()
-		{
-			return CalcRelativeTransform(NativePtr).To<Mat4x4>().ToLime();
-		}
-
-		public override string ToString(int level)
-		{
-			StringBuilder builder = new StringBuilder();
-			builder.Append("Points: ".ToLevel(level + 1));
-			if (Points == null) {
-				builder.AppendLine("None");
-			} else {
-				builder.AppendLine("[".ToLevel(level + 2));
-				for (int i =0; i < Points.Length; i+=3) {
-					builder.AppendLine($"[{Points[i]}, {Points[i + 1]}, {Points[i + 2]}],".ToLevel(level + 3));
-				}
-				builder.AppendLine("[".ToLevel(level + 2));
-			}
-			return builder.ToString();
-		}
+		#region MarshalingStructures
 
 		[StructLayout(LayoutKind.Sequential)]
 		private class MeshData
@@ -113,5 +100,6 @@ namespace Orange.FbxImporter
 				Utils.ReleaseNative(points);
 			}
 		}
+		#endregion
 	}
 }
