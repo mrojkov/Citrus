73a6178bfb06ca82b98c632de21e8953a3d9e2a7
rshutov <rshutov@game-forest.com>
2017-05-19 18:14:05 +1000

    [Orange][Fbx] Extract common marshaling structures

---
+++ Orange/Source/ModelImport/FbxImporter/Models/MarshalingStructures.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orange.FbxImporter
+{
+	[StructLayout(LayoutKind.Sequential)]
+	public class Vec2
+	{
+		[MarshalAs(UnmanagedType.R4)]
+		public float V1;
+
+		[MarshalAs(UnmanagedType.R4)]
+		public float V2;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public class Vec3 : Vec2
+	{
+		[MarshalAs(UnmanagedType.R4)]
+		public float V3;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public class Vec4 : Vec3
+	{
+		[MarshalAs(UnmanagedType.R4)]
+		public float V4;
+	}
+}
