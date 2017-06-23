04d5c4be6fbb359b587162dbdf840f9cc4602d4b
rshutov <rshutov@game-forest.com>
2017-06-23 15:48:32 +1000

    [TAN-75] Add close property to spline

--- Lime/Source/Widgets/Spline.cs
+++ Lime/Source/Widgets/Spline.cs
@@ -1,44 +1,52 @@
 using System.Collections.Generic;
+using Yuzu;
 
 namespace Lime
 {
 	[AllowedChildrenTypes(typeof(SplinePoint))]
 	public class Spline : Widget
 	{
+		[YuzuMember]
+		public bool Closed { get; set; }
+
 		public Spline()
 		{
 			RenderChainBuilder = null;
 		}
 
+		private SplinePoint GetPoint(int index)
+		{
+			return (SplinePoint)Nodes[index % Nodes.Count];
+		}
+
 		public float CalcLengthRough()
 		{
 			float length = 0;
-			SplinePoint p = null;
-			foreach (Node node in Nodes) {
-				SplinePoint v = node as SplinePoint;
-				if (v == null)
-					continue;
-				if (p != null) {
-					length += ((v.Position - p.Position) * Size).Length;
-				}
-				p = v;
+			var segmentCount = GetSegmentCount();
+			for (int i = 0; i < segmentCount; i++) {
+				length += ((GetPoint(i + 1).Position - GetPoint(i).Position) * Size).Length;
 			}
 			return length;
+		}
+
+		public int GetSegmentCount()
+		{
+			if (Nodes.Count > 1) {
+				return Closed ? Nodes.Count : Nodes.Count - 1;
+			}
+			return 0;
 		}
 
 		public List<Vector2> ApproximateByPolyline(int approximateCount)
 		{
 			var result = new List<Vector2>();
-			SplinePoint prevPoint = null;
-			foreach (SplinePoint curPoint in Nodes) {
-				if (prevPoint != null) {
-					for (int i = 1; i < approximateCount; i++) {
-						Vector2 position = Interpolate(prevPoint, curPoint, (float)(i) / approximateCount);
-						result.Add(position);
-					}
+			var segmentCount = GetSegmentCount();
+			for (int i = 0; i < segmentCount; i++) {
+				for (int j = 1; j < approximateCount; j++) {
+					Vector2 position = Interpolate(GetPoint(i), GetPoint(i + 1), (float)(j) / approximateCount);
+					result.Add(position);
 				}
-				result.Add(curPoint.Position);
-				prevPoint = curPoint;
+				result.Add(GetPoint(i + 1).Position);
 			}
 			return result;
 		}
@@ -59,60 +67,51 @@ namespace Lime
 		public float CalcLengthAccurate(int approximateCount)
 		{
 			float length = 0;
-			SplinePoint prevPoint = null;
-			foreach (SplinePoint curPoint in Nodes) {
-				if (prevPoint != null) {
-					length += CalcSegmentLengthAccurate(prevPoint, curPoint, approximateCount);
-				}
-				prevPoint = curPoint;
+			var segmentCount = GetSegmentCount();
+			for (int i = 0; i < segmentCount; i++) {
+				length += CalcSegmentLengthAccurate(GetPoint(i), GetPoint(i + 1), approximateCount);
 			}
 			return length;
 		}
 
 		public Vector2 CalcPoint(float lengthFromBeginnning)
 		{
-			SplinePoint pre = null;
 			float segStart = 0;
-			foreach (var node in Nodes) {
-				var cur = node as SplinePoint;
-				if (cur == null)
-					continue;
+			var segmentCount = GetSegmentCount();
+			for (int i = 0; i < segmentCount; i++) {
+				var start = GetPoint(i);
+				var end = GetPoint(i + 1);
 				if (lengthFromBeginnning < 0)
-					return cur.Position * Size;
-				if (pre != null) {
-					float segLength = ((cur.Position - pre.Position) * Size).Length;
-					if (segStart <= lengthFromBeginnning && lengthFromBeginnning < segStart + segLength) {
-						return Interpolate(pre, cur, (lengthFromBeginnning - segStart) / segLength);
-					}
-					segStart += segLength;
+					return start.Position * Size;
+
+				float segLength = ((end.Position - start.Position) * Size).Length;
+				if (segStart <= lengthFromBeginnning && lengthFromBeginnning < segStart + segLength) {
+					return Interpolate(start, end, (lengthFromBeginnning - segStart) / segLength);
 				}
-				pre = cur;
+				segStart += segLength;
 			}
-			return pre != null ? pre.Position * Size : Vector2.Zero;
+
+			return Nodes.Count > 0 ? GetPoint(segmentCount % Nodes.Count).Position * Size : Vector2.Zero;
 		}
 
 		public IEnumerable<Vector2> CalcPoints(float step)
 		{
-			SplinePoint pre = null;
 			float segStart = 0;
 			float totalLength = 0;
-			foreach (var node in Nodes) {
-				var cur = node as SplinePoint;
-				if (cur == null)
-					continue;
-				if (pre != null) {
-					var segLength = ((cur.Position - pre.Position) * Size).Length;
-					while (totalLength < segStart + segLength) {
-						var t = (totalLength - segStart) / segLength;
-						yield return Interpolate(pre, cur, t);
-						totalLength += step;
-					}
-					segStart += segLength;
+			var segmentCount = GetSegmentCount();
+			for (int i = 0; i < segmentCount; i++) {
+				var start = GetPoint(i);
+				var end = GetPoint(i + 1);
+				float segLength = ((end.Position - start.Position) * Size).Length;
+				while (totalLength < segStart + segLength) {
+					var t = (totalLength - segStart) / segLength;
+					yield return Interpolate(start, end, t);
+					totalLength += step;
 				}
-				pre = cur;
+				segStart += segLength;
 			}
-			if (pre != null) {
-				yield return pre.Position * Size;
+			if (Nodes.Count > 0) {
+				yield return GetPoint(segmentCount % Nodes.Count).Position* Size;
 			}
 		}
 
@@ -139,38 +138,35 @@ namespace Lime
 		public float CalcSplineLengthToNearestPoint(Vector2 point)
 		{
 			float length = 0;
-			SplinePoint p = null;
+			
 			float minDistance = float.MaxValue;
 			float offset = 0;
-			foreach (Node node in Nodes) {
-				SplinePoint v = node as SplinePoint;
-				if (v == null)
-					continue;
-				if (p != null) {
-					float segmentLength = ((v.Position - p.Position) * Size).Length;
-					float minDistance_, offset_;
-					if (CalcSplineLengthToNearestPointHelper(p, v, point, out minDistance_, out offset_)) {
-						if (minDistance_ < minDistance) {
-							offset = offset_ * segmentLength + length;
-							minDistance = minDistance_;
-						}
+			var segmentCount = GetSegmentCount();
+			for (int i = 0; i < segmentCount; i++) {
+				var start = GetPoint(i);
+				var end = GetPoint(i + 1);
+				float segmentLength = ((end.Position - start.Position) * Size).Length;
+				float minDistance_, offset_;
+				if (CalcSplineLengthToNearestPointHelper(start, end, point, out minDistance_, out offset_)) {
+					if (minDistance_ < minDistance) {
+						offset = offset_ * segmentLength + length;
+						minDistance = minDistance_;
 					}
 					length += segmentLength;
 				}
-				p = v;
 			}
 			return offset;
 		}
 
 		private bool CalcSplineLengthToNearestPointHelper(SplinePoint v1, SplinePoint v2, Vector2 point, out float minDistance, out float offset)
 		{
-			const int SegmentCount = 10;
+			const int InnerSegmentCount = 10;
 			float ta = 0;
 			Vector2 a = Interpolate( v1, v2, ta );
 			minDistance = float.MaxValue;
 			offset = 0;
-			for (int i = 0; i < SegmentCount; ++i) {
-				float tb = (float)(i + 1) / SegmentCount;
+			for (int i = 0; i < InnerSegmentCount; ++i) {
+				float tb = (float)(i + 1) / InnerSegmentCount;
 				Vector2 b = Interpolate(v1, v2, tb);
 				float minDistance_, offset_;
 				if (ProjectPointToLine(a, b, point, out minDistance_, out offset_ )) {
