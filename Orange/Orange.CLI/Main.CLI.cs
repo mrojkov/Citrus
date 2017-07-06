2288e5194609b342908943f34b49f1b567c9ddc2
Denis Rotanov <mapholameth@gmail.com>
2017-07-06 15:22:59 +1000

    [Orange][Win][CLI] fix compilation

--- Orange/Orange.CLI/Main.CLI.cs
+++ Orange/Orange.CLI/Main.CLI.cs
@@ -4,9 +4,14 @@ namespace Orange
 {
 	class MainClass
 	{
+#if WIN
+		[STAThread]
+#endif
 		static void Main(string[] args)
 		{
+#if MAC
 			Lime.Application.Initialize();
+#endif
 			var culture = System.Globalization.CultureInfo.InvariantCulture;
 			System.Threading.Thread.CurrentThread.CurrentCulture = culture;
 			PluginLoader.RegisterAssembly(typeof(MainClass).Assembly);
