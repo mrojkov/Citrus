980de8c537eb01c9582ae3dc9af1aa2154dc8dab
Artem Sokolov <sokolov.artem@hotmail.com>
2017-07-10 11:23:59 +1000

    Fix cheats menu.

--- Samples/EmptyProject/EmptyProject.Game/Debug/RainbowDash.cs
+++ Samples/EmptyProject/EmptyProject.Game/Debug/RainbowDash.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Lime;
-using Lime.PopupMenu;
 
 //                                     __         __----__
 //                                    /  \__..--'' `-__-__''-_
@@ -33,6 +32,56 @@ using Lime.PopupMenu;
 
 namespace RainbowDash
 {
+	public class RoundedRectangle : Widget
+	{
+		public float CornerRadius = 25;
+		public Color4 InnerColor = Color4.White;
+		public Color4 OuterColor = Color4.Black;
+
+		private Polyline outerFrame = new Polyline();
+		private Polyline innerFrame = new Polyline();
+		private Image innerRect = new Image();
+
+		public RoundedRectangle()
+		{
+			this.AddNode(innerRect);
+			this.AddNode(innerFrame);
+			this.AddNode(outerFrame);
+		}
+
+		protected override void SelfUpdate(float delta)
+		{
+			SetupInnerRect();
+			SetupBorder(outerFrame, Width, Height, CornerRadius, -2);
+			SetupBorder(innerFrame, Width, Height, CornerRadius, 0);
+			innerFrame.Color = InnerColor;
+			outerFrame.Color = OuterColor;
+		}
+
+		private void SetupInnerRect()
+		{
+			innerRect.Shader = ShaderId.Silhuette;
+			innerRect.Position = CornerRadius * Vector2.One;
+			innerRect.Size = Size - 2 * CornerRadius * Vector2.One;
+			innerRect.Color = InnerColor;
+		}
+
+		private static void SetupBorder(Polyline border, float width, float height,
+			float cornerRadius, float padding)
+		{
+			padding += cornerRadius;
+			border.Thickness = cornerRadius * 2;
+			border.Points = new List<Vector2> {
+				new Vector2(padding, padding),
+				new Vector2(width - padding, padding),
+				new Vector2(width - padding, height - padding),
+				new Vector2(padding, height - padding),
+				new Vector2(padding, padding)
+			};
+		}
+	}
+
+
 	public class FillLayout : CommonLayout, ILayout
 	{
 		public FillLayout()
