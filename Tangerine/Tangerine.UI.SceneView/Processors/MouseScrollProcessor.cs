ec8e587046730e591b6f96e70b85382665dcd706
rshutov <rshutov@game-forest.com>
2017-07-11 11:29:54 +1000

    [TAN-60] Add scrolling shortcut

--- Tangerine/Tangerine.UI.SceneView/Processors/MouseScrollProcessor.cs
+++ Tangerine/Tangerine.UI.SceneView/Processors/MouseScrollProcessor.cs
@@ -8,9 +8,11 @@ namespace Tangerine.UI.SceneView
 	class MouseScrollProcessor : Core.ITaskProvider
 	{
 		SceneView sv => SceneView.Instance;
+		static Vector2 mouseStartPos;
 
 		public IEnumerator<object> Task()
 		{
+			var prevPos = sv.Input.MousePosition;
 			while (true) {
 				if (sv.Input.WasMousePressed(0) && CommonWindow.Current.Input.IsKeyPressed(Key.Space) || sv.Input.WasMousePressed(2)) {
 					var initialMouse = sv.Input.MousePosition;
@@ -24,17 +26,27 @@ namespace Tangerine.UI.SceneView
 					}
 					sv.Input.ReleaseMouse();
 				}
+
+				if (sv.Input.WasKeyPressed(Key.Alt) || sv.Input.WasMousePressed(1)) {
+					mouseStartPos = sv.MousePosition;
+				}
+
 				if (sv.Input.WasKeyPressed(Key.MouseWheelDown)) {
 					ZoomCanvas(-1);
 				} else if (sv.Input.WasKeyPressed(Key.MouseWheelUp)) {
 					ZoomCanvas(1);
+				} else if (sv.Input.IsKeyPressed(Key.Alt) && sv.Input.IsKeyPressed(Key.Mouse1)) {
+					var delta = (sv.Input.MousePosition - prevPos).X;
+					var size = (float)Math.Sqrt(sv.Scene.Size.SqrLength);
+					ZoomCanvas(delta / size);
 				}
+				prevPos = sv.Input.MousePosition;
 				yield return null;
 			}
 		}
 
 		readonly List<float> zoomTable = new List<float> {
-			0.001f, 0.0025f, 0.005f, 0.01f, 0.025f, 0.05f, 0.10f, 
+			0.001f, 0.0025f, 0.005f, 0.01f, 0.025f, 0.05f, 0.10f,
 			0.15f, 0.25f, 0.5f, 0.75f, 1f, 1.5f, 2f, 3f,
 			4f, 5f, 6f, 7f, 8f, 9f, 10f, 11f,
 			12f, 13f, 14f, 15f, 16f
@@ -42,15 +54,34 @@ namespace Tangerine.UI.SceneView
 
 		void ZoomCanvas(int advance)
 		{
-			var i = zoomTable.IndexOf(sv.Scene.Scale.X);
+			var i = FindNearest(sv.Scene.Scale.X, 0, zoomTable.Count - 1);
 			if (i < 0) {
 				throw new InvalidOperationException();
 			}
-			var prevZoom = zoomTable[i];
+			var prevZoom = sv.Scene.Scale.X;
 			var zoom = zoomTable[(i + advance).Clamp(0, zoomTable.Count - 1)];
 			var p = sv.MousePosition;
 			sv.Scene.Scale = zoom * Vector2.One;
 			sv.Scene.Position -= p * (zoom - prevZoom);
+		}
+
+		void ZoomCanvas(float delta)
+		{
+			var zoom = sv.Scene.Scale.X + delta;
+			if (zoom < zoomTable.First() || zoom > zoomTable.Last()) {
+				return;
+			}
+			sv.Scene.Scale = zoom * Vector2.One;
+			sv.Scene.Position -= mouseStartPos * delta;
+		}
+
+		private int FindNearest(float x, int left, int right)
+		{
+			if (right - left == 1) {
+				return left;
+			}
+			var idx = left + (right - left) / 2;
+			return x < zoomTable[idx] ? FindNearest(x, left, idx) : FindNearest(x, idx, right);
 		}
 	}
 }
