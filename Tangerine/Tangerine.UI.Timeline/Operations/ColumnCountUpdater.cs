553f4439381adb25c02c6c751b1b640704af02f2
rshutov <rshutov@game-forest.com>
2017-09-07 11:49:29 +1000

    [CIT-78][CIT-139] Add extra frames at the end of the last marker or keyframe

--- Tangerine/Tangerine.UI.Timeline/Operations/ColumnCountUpdater.cs
+++ Tangerine/Tangerine.UI.Timeline/Operations/ColumnCountUpdater.cs
@@ -7,15 +7,22 @@ namespace Tangerine.UI.Timeline
 {
 	public class ColumnCountUpdater : SymmetricOperationProcessor
 	{
+		private const int ExtraFramesCount = 100;
 		public override void Process(IOperation op)
 		{
 			var timeline = Timeline.Instance;
 			var rows = Document.Current.Rows;
+			var container = Document.Current.Container;
 			int maxColumn = 0;
 			foreach (var row in rows) {
 				var nodeData = row.Components.Get<Core.Components.NodeRow>();
 				if (nodeData != null) {
-					maxColumn = Math.Max(maxColumn, nodeData.Node.Animators.GetOverallDuration());
+					int maxMarkerColumn = 0;
+					var maxAnimatorColumn = Math.Max(maxColumn, nodeData.Node.Animators.GetOverallDuration() + ExtraFramesCount);
+					if (container.Markers.Count > 0) {
+						maxMarkerColumn = Math.Max(maxColumn, container.Markers[container.Markers.Count - 1].Frame + ExtraFramesCount);
+					}
+					maxColumn = Math.Max(maxMarkerColumn, maxAnimatorColumn);
 				}
 			}
 			var maxVisibleColumn = ((timeline.OffsetX + timeline.Grid.Size.X) / TimelineMetrics.ColWidth).Ceiling();
