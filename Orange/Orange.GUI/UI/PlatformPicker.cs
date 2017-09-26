673f9b036f1ba19551a09359f90940f88979d34b
Denis Rotanov <mapholameth@gmail.com>
2017-09-26 16:29:16 +1000

    [Orange] Try to restore platform picker index on project reload [CIT-168]

--- Orange/Orange.GUI/UI/PlatformPicker.cs
+++ Orange/Orange.GUI/UI/PlatformPicker.cs
@@ -12,12 +12,17 @@ namespace Orange
 
 		public void Reload()
 		{
+			var savedIndex = Index;
 			Index = -1;
 			Items.Clear();
 			foreach (var target in The.Workspace.Targets) {
 				Items.Add(new Item(target.Name, target));
 			}
-			Index = 0;
+			if (savedIndex >= 0 && savedIndex < Items.Count) {
+				Index = savedIndex;
+			} else {
+				Index = 0;
+			}
 		}
 
 		public Target SelectedTarget => (Target)Items[Index].Value;
