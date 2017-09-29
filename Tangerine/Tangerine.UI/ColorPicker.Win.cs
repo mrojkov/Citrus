5fe9c387e432c68b7c53005d6b8b0f56d89e5e37
Mikhail Rozhkov <mrozhkov@game-forest.com>
2017-09-29 11:40:56 +1000

    [Tangerine] Remove redundant usings.

--- Tangerine/Tangerine.UI/ColorPicker.Win.cs
+++ Tangerine/Tangerine.UI/ColorPicker.Win.cs
@@ -1,10 +1,6 @@
 ﻿#if WIN
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Runtime.InteropServices;
-using System.Drawing;
 using Lime;
 
 namespace Tangerine.UI
