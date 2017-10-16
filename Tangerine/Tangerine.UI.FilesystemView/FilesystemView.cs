1cef225bbc25f24c31102c7e35957b1975bdfcb1
Denis Rotanov <mapholameth@gmail.com>
2017-10-16 10:58:05 +1000

    [Tan][Fs] don't display focus outline on parts of FS interface

--- Tangerine/Tangerine.UI.FilesystemView/FilesystemView.cs
+++ Tangerine/Tangerine.UI.FilesystemView/FilesystemView.cs
@@ -144,7 +144,6 @@ namespace Tangerine.UI.FilesystemView
 			foreach (var n in RootWidget.Descendants) {
 				var w = n.AsWidget;
 				if (w.TabTravesable != null) {
-					w.CompoundPostPresenter.Add(new Theme.KeyboardFocusBorderPresenter(2.0f));
 					w.HitTestTarget = true;
 				}
 			}
