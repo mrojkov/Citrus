b9ed738662b84a9d2c57edd8f6a013f7566d5b9b
Nika Burimenko <bolya2003@bk.ru>
2018-01-16 10:59:47 +1000

    [CIT-137]: Fix Orange leaves open stream after cooking.

--- Orange/Source/AssetCooker/CodeCooker.cs
+++ Orange/Source/AssetCooker/CodeCooker.cs
@@ -17,36 +17,39 @@ namespace Orange
 			foreach (var bundleName in bundles) {
 				assetBundles.Add(bundleName, new PackedAssetBundle(The.Workspace.GetBundlePath(bundleName)));
 			}
-			AssetBundle.Current = new AggregateAssetBundle(assetBundles.Values.Cast<AssetBundle>().ToArray());
 
-			Func<string, bool> filter = (path) => scenesExtensions.Contains(Path.GetExtension(path));
-			var scenes = new Dictionary<string, Node>();
-			var sceneToBundleMap = new Dictionary<string, string>();
-			foreach (var bundle in assetBundles) {
-				var bundleScenesFiles = bundle.Value
-					.EnumerateFiles()
-					.Where(filter)
-					.ToList();
+			try {
+				AssetBundle.Current = new AggregateAssetBundle(assetBundles.Values.Cast<AssetBundle>().ToArray());
 
-				foreach (var sceneFile in bundleScenesFiles) {
-					scenes.Add(sceneFile, Node.CreateFromAssetBundle(sceneFile));
-					sceneToBundleMap.Add(sceneFile, bundle.Key);
+				Func<string, bool> filter = (path) => scenesExtensions.Contains(Path.GetExtension(path));
+				var scenes = new Dictionary<string, Node>();
+				var sceneToBundleMap = new Dictionary<string, string>();
+				foreach (var bundle in assetBundles) {
+					var bundleScenesFiles = bundle.Value
+						.EnumerateFiles()
+						.Where(filter)
+						.ToList();
+
+					foreach (var sceneFile in bundleScenesFiles) {
+						scenes.Add(sceneFile, Node.CreateFromAssetBundle(sceneFile));
+						sceneToBundleMap.Add(sceneFile, bundle.Key);
+					}
 				}
-			}
 
-			if (scenes.Count == 0) {
+				if (scenes.Count == 0) {
+					return;
+				}
+				new ScenesCodeCooker(
+					The.Workspace.ProjectDirectory,
+					The.Workspace.Title,
+					CookingRulesBuilder.MainBundleName,
+					sceneToBundleMap,
+					scenes
+				).Start();
+			} finally {
+				AssetBundle.Current.Dispose();
 				AssetBundle.Current = null;
-				return;
 			}
-			new ScenesCodeCooker(
-				The.Workspace.ProjectDirectory,
-				The.Workspace.Title,
-				CookingRulesBuilder.MainBundleName,
-				sceneToBundleMap,
-				scenes
-			).Start();
-
-			AssetBundle.Current = null;
 		}
 	}
 }
