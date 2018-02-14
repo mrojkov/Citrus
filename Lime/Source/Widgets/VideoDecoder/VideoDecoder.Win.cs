938f1b1ecd16762150f992048a0a304b96f3069e
MukhinAlexey <mukhin.a.v.93@gmail.com>
2018-02-14 10:21:55 +1000

    [VideoPlayer][Win]: First attempt for win video decoder (No audio)

---
+++ Lime/Source/Widgets/VideoDecoder/VideoDecoder.Win.cs
@@ -0,0 +1,354 @@
+﻿#if WIN
+using MFDecoder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lime
+{
+	public class VideoDecoder : IDisposable
+	{
+		public int Width => width;
+		public int Height => height;
+		public bool HasNewTexture = false;
+		public bool Looped = false;
+		public ITexture Texture => texture;
+		private Texture2D lumaTexture;
+		private Texture2D chromaTexture;
+		private RenderTexture texture;
+		private static YUVtoRGBProgram program;
+		private static Mesh mesh;
+
+		private MFDecoder.Decoder decoder;
+		private int width;
+		private int height;
+		private CancellationTokenSource stopDecodeCancelationTokenSource;
+		private ManualResetEvent checkVideo;
+		private ManualResetEvent checkAudio;
+		private ManualResetEvent checkQueue;
+
+		private Queue<Sample> audioQueue;
+		private Queue<Sample> videoQueue;
+		private const int MaxAudioQueue = 100;
+		private const int MaxVideoQueue = 100;
+
+		private long currentPosition;
+
+		private Sample currentVideoSample;
+
+		private State state;
+
+		private enum State
+		{
+			Initialized,
+			Started,
+			Stoped,
+			Paused,
+			Finished
+		}
+
+		private class Initializer
+		{
+			public Initializer()
+			{
+				MFDecoder.MediaFoundation.Initialize();
+			}
+
+			~Initializer()
+			{
+				MediaFoundation.Shutdown();
+			}
+		}
+
+		private static Initializer finalizer = new Initializer();
+
+		public VideoDecoder(string path)
+		{
+			decoder = new MFDecoder.Decoder(path);
+			checkVideo = new ManualResetEvent(false);
+			checkAudio = new ManualResetEvent(false);
+			checkQueue = new ManualResetEvent(false);
+			state = State.Initialized;
+			width = decoder.GetWidth();
+			height = decoder.GetHeight();
+			lumaTexture = new Texture2D();
+			chromaTexture = new Texture2D();
+			texture = new RenderTexture(width, height);
+			audioQueue = new Queue<Sample>();
+			videoQueue = new Queue<Sample>();
+			if (program == null) {
+				program = new YUVtoRGBProgram();
+				mesh = new Mesh() {
+					IndexBuffer = new IndexBuffer {
+						Data = new ushort[] {
+							0, 1, 2, 2, 3, 0
+						},
+					},
+					VertexBuffers = new[] {
+						new VertexBuffer<Vector2> {
+							Data = new Vector2 [] {
+								new Vector2 (-1,  1),
+								new Vector2( 1,  1),
+								new Vector2( 1, -1),
+								new Vector2(-1, -1)
+							},
+						},
+						new VertexBuffer<Vector2> {
+							Data = new Vector2 [] {
+								new Vector2(0, 0),
+								new Vector2(1, 0),
+								new Vector2(1, 1),
+								new Vector2(0, 1)
+							},
+						}
+					},
+					Attributes = new[] {
+						new [] { ShaderPrograms.Attributes.Pos1 },
+						new [] { ShaderPrograms.Attributes.UV1 }
+					}
+				};
+			}
+		}
+
+		public async System.Threading.Tasks.Task Start()
+		{
+			if (state == State.Started) {
+				return;
+			}
+			stopDecodeCancelationTokenSource = new CancellationTokenSource();
+			var token = stopDecodeCancelationTokenSource.Token;
+			do {
+				if (state == State.Finished) {
+					currentPosition = 0;
+					decoder.SetPosition(0);
+				}
+				state = State.Started;
+				bool queueTaskCompleted = false;
+				var queueTask = System.Threading.Tasks.Task.Run(() => {
+					try {
+						var audioIsEnded = false;
+						var videoIsEnded = false;
+						while (!audioIsEnded || !videoIsEnded) {
+							var sample = decoder.ReadSample();
+							if (sample == null) {
+								Debug.Write("VideoDecoder: Sample is null");
+								break;
+							}
+							Queue<Sample> queue;
+							int maxItems;
+							ManualResetEvent resetEvent;
+							if (sample.IsAudio) {
+								queue = audioQueue;
+								maxItems = MaxAudioQueue;
+								resetEvent = checkAudio;
+								if (sample.IsEos) {
+									audioIsEnded = true;
+									continue;
+								}
+							} else {
+								queue = videoQueue;
+								maxItems = MaxVideoQueue;
+								resetEvent = checkVideo;
+								if (sample.IsEos) {
+									videoIsEnded = true;
+									continue;
+								}
+							}
+							while (queue.Count >= maxItems) {
+								checkQueue.WaitOne();
+								checkQueue.Reset();
+								token.ThrowIfCancellationRequested();
+							}
+							queue.Enqueue(sample);
+							resetEvent.Set();
+						}
+					} finally {
+						queueTaskCompleted = true;
+					}
+				}, token);
+
+				var videoTask = System.Threading.Tasks.Task.Run(() => {
+					while (!queueTaskCompleted || videoQueue.Count > 0) {
+						while (videoQueue.Count > 0) {
+							var sample = videoQueue.Dequeue();
+							while (currentPosition < sample.PresentationTime) {
+								checkVideo.WaitOne();
+								checkVideo.Reset();
+								token.ThrowIfCancellationRequested();
+							}
+							currentVideoSample = sample;
+							HasNewTexture = true;
+							checkQueue.Set();
+						}
+					}
+				}, token);
+
+				var audioTask = System.Threading.Tasks.Task.Run(() => {
+					while (!queueTaskCompleted || audioQueue.Count > 0) {
+						while (audioQueue.Count > 0) {
+							var sample = audioQueue.Dequeue();
+							while (currentPosition < sample.PresentationTime) {
+								checkAudio.WaitOne();
+								checkAudio.Reset();
+								token.ThrowIfCancellationRequested();
+							}
+							//currentVideoSample = sample;
+							checkQueue.Set();
+						}
+					}
+				}, token);
+				try {
+					await queueTask;
+					await System.Threading.Tasks.Task.WhenAll(audioTask, videoTask);
+				} catch (System.OperationCanceledException e) {
+					Debug.Write("VideoPlayer: queueTask canceled!");
+				}
+				
+				if (!token.IsCancellationRequested) {
+					state = State.Finished;
+				}
+			} while (Looped && !token.IsCancellationRequested);
+		}
+
+		public void Stop()
+		{
+			if (state == State.Stoped || state == State.Finished || state == State.Initialized) {
+				return;
+			}
+			Pause();
+			state = State.Stoped;
+			currentPosition = 0;
+			decoder.SetPosition(0);
+			audioQueue.Clear();
+			videoQueue.Clear();
+		}
+
+		public void Pause()
+		{
+			if (state == State.Started) {
+				state = State.Paused;
+				stopDecodeCancelationTokenSource.Cancel();
+				checkAudio.Set();
+				checkVideo.Set();
+				checkQueue.Set();
+			}
+		}
+
+		public void Update(float delta)
+		{
+			if (state == State.Started) {
+				currentPosition += (long)(delta * 10000000);
+				checkAudio.Set();
+				checkVideo.Set();
+			}
+		}
+
+		public void UpdateTexture()
+		{
+			if (HasNewTexture) {
+				HasNewTexture = false;
+				var sample = currentVideoSample;
+				var pinnedArray = GCHandle.Alloc(sample.Data, GCHandleType.Pinned);
+				var pointer = pinnedArray.AddrOfPinnedObject();
+				var pixelInternalFormat = (OpenTK.Graphics.ES20.PixelInternalFormat.Luminance) ; //OpenTK.Graphics.ES20.PixelInternalFormat.Luminance
+				lumaTexture.LoadImage(pointer, width, height, pixelInternalFormat, OpenTK.Graphics.ES20.PixelFormat.Luminance);
+
+
+				pixelInternalFormat = (OpenTK.Graphics.ES20.PixelInternalFormat.LuminanceAlpha);
+				var pixelFormat = (OpenTK.Graphics.ES20.PixelFormat.LuminanceAlpha);
+				chromaTexture.LoadImage(pointer + width * height, width / 2, height / 2, pixelInternalFormat, pixelFormat);
+				pinnedArray.Free();
+
+
+				var savedViewport = Renderer.Viewport;
+				Renderer.Viewport = new WindowRect { X = 0, Y = 0, Width = texture.ImageSize.Width, Height = texture.ImageSize.Height };
+				texture.SetAsRenderTarget();
+				PlatformRenderer.SetTexture(lumaTexture, 0);
+				PlatformRenderer.SetTexture(chromaTexture, 1);
+				program.Use();
+				PlatformRenderer.DrawTriangles(mesh, 0, mesh.IndexBuffer.Data.Length);
+
+				texture.RestoreRenderTarget();
+				Renderer.Viewport = savedViewport;
+			}
+		}
+
+		public void Dispose()
+		{
+		}
+
+
+		private class YUVtoRGBProgram : ShaderProgram
+		{
+			private const string VertexShader = @"
+				attribute vec4 a_Position;
+				attribute vec2 a_UV;
+				varying vec2 v_UV;
+				void main()
+				{
+					gl_Position = a_Position;
+					v_UV = vec2(a_UV.x, 1.0 - a_UV.y);
+				}
+			";
+
+			private const string FragmentShader = @"
+				
+				uniform sampler2D u_SamplerY;
+				uniform sampler2D u_SamplerUV;
+				varying highp vec2 v_UV;
+
+				void main()
+				{
+					mediump vec3 yuv;
+					lowp vec3 rgb;
+					yuv.x = texture2D(u_SamplerY, v_UV).r;
+					yuv.yz = texture2D(u_SamplerUV, v_UV).ra - vec2(0.5, 0.5);
+					// Using BT.709 which is the standard for HDTV
+					
+					rgb = mat3( 1, 1, 1,
+								0, -0.18732, 1.8556,
+								1.57481, -0.46813, 0
+					) * yuv;
+					
+					gl_FragColor = vec4(rgb, 1);
+				}
+			";
+
+			private const int LumaTextureStage = 0;
+			private const int ChromaTextureStage = 1;
+
+			public YUVtoRGBProgram() : base(GetShaders(), GetAttribLocations(), GetSamplers())
+			{
+			}
+
+			private static Shader[] GetShaders()
+			{
+				return new Shader[] {
+					new VertexShader(VertexShader),
+					new FragmentShader(FragmentShader)
+				};
+			}
+
+			private static AttribLocation[] GetAttribLocations()
+			{
+				return new AttribLocation[] {
+					new AttribLocation { Name = "a_Position", Index = ShaderPrograms.Attributes.Pos1 },
+					new AttribLocation { Name = "a_UV", Index = ShaderPrograms.Attributes.UV1 }
+				};
+			}
+
+			private static Sampler[] GetSamplers()
+			{
+				return new Sampler[] {
+					new Sampler { Name = "u_SamplerY", Stage = LumaTextureStage },
+					new Sampler { Name = "u_SamplerUV", Stage = ChromaTextureStage }
+				};
+			}
+		}
+	}
+}
+#endif
--- Lime/Source/Widgets/VideoDecoder/VideoDecoder.cs
+++ Lime/Source/Widgets/VideoDecoder/VideoDecoder.cs
@@ -1,4 +1,4 @@
-﻿#if !ANDROID && !iOS
+﻿#if !ANDROID && !iOS && !WIN
 using System;
 using System.Collections.Generic;
 using System.Linq;
