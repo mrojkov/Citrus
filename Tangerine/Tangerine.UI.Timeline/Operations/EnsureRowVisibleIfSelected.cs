6be3f506121e65037b8e967420dd20600621f02c
Anton Tkachev <misterio_anton@mail.ru>
2018-03-19 18:14:38 +0200

    [Tangerine] Allow guaranteed click on EnterButton in Timeline when row is partly visible

--- Tangerine/Tangerine.UI.Timeline/Operations/EnsureRowVisibleIfSelected.cs
+++ Tangerine/Tangerine.UI.Timeline/Operations/EnsureRowVisibleIfSelected.cs
@@ -9,6 +9,9 @@ namespace Tangerine.UI.Timeline
 	{
 		protected override void InternalRedo(Core.Operations.SelectRow op)
 		{
+			// To allow click on EnterButton when row is partly visible.
+			if (Timeline.Instance.RootWidget.Input.IsMousePressed())
+				return;
 			if (op.Select) {
 				var timeline = Timeline.Instance;
 				timeline.EnsureRowVisible(op.Row);
--- Tangerine/Tangerine.UI.Timeline/Processors/RollPane/RollMouseScrollProcessor.cs
+++ Tangerine/Tangerine.UI.Timeline/Processors/RollPane/RollMouseScrollProcessor.cs
@@ -11,8 +11,13 @@ namespace Tangerine.UI.Timeline
 		public IEnumerator<object> Task()
 		{
 			var input = timeline.Roll.RootWidget.Input;
+			bool wasPressed = false; 
 			while (true) {
 				if (input.IsMousePressed()) {
+					// To allow click on EnterButton when row is partly visible.
+					if (!wasPressed) {
+						yield return 0.2;
+					}
 					var s = TimelineMetrics.DefaultRowHeight;
 					if (input.LocalMousePosition.Y > timeline.Roll.RootWidget.Height - s / 2) {
 						timeline.OffsetY += s;
@@ -20,6 +25,9 @@ namespace Tangerine.UI.Timeline
 						timeline.OffsetY -= s;
 					}
 					Window.Current.Invalidate();
+					wasPressed = true;
+				} else {
+					wasPressed = false;
 				}
 				yield return 0.1f;
 			}
