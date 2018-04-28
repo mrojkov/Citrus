7ccbbe294e6ae4421d66e61e4c0c00b34092b6fc
Anton Tkachev <misterio_anton@mail.ru>
2018-04-28 12:31:41 +0200

    [Tangerine] Fix photoshop like info for Blending modes

--- Tangerine/Tangerine.UI/PropertyEditor.cs
+++ Tangerine/Tangerine.UI/PropertyEditor.cs
@@ -1104,13 +1104,13 @@ namespace Tangerine.UI
 	{
 
 		private static readonly Dictionary<string, string> blendingToPhotoshopAnalog = new Dictionary<string, string> {
-			{Blending.Alpha.ToString(), "aka \"Normal\""},
-			{Blending.Add.ToString(), "aka \"Linear Dodge\""},
-			{Blending.Glow.ToString(), "aka \"Normal\" with Brightness"},
-			{Blending.Modulate.ToString(), "aka \"Multiply\" without Transparency"},
-			{Blending.Burn.ToString(), "aka \"Multiply\""},
-			{Blending.Darken.ToString(), "aka \"Normal\" with Darkness"},
-			{Blending.Opaque.ToString(), "aka \"Normal\" without Transparency"},
+			{Blending.Alpha.ToString(), "Normal"},
+			{Blending.Add.ToString(), "Linear Dodge"},
+			{Blending.Glow.ToString(), "Normal with Brightness"},
+			{Blending.Modulate.ToString(), "Multiply without Transparency"},
+			{Blending.Burn.ToString(), "Multiply"},
+			{Blending.Darken.ToString(), "Normal with Darkness"},
+			{Blending.Opaque.ToString(), "Normal without Transparency"},
 		};
 
 		public BlendingPropertyEditor(IPropertyEditorParams editorParams) : base(editorParams)
