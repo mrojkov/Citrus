14661aea2cd0d4c7b07a58615bcc1a7f94666ab0
Denis Rotanov <mapholameth@gmail.com>
2018-04-28 15:43:02 +1000

    [Launcher] make launcher behave as win app without cl args, and like one with them

--- Orange/Launcher/Main.cs
+++ Orange/Launcher/Main.cs
@@ -1,8 +1,4 @@
 using System;
-using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using McMaster.Extensions.CommandLineUtils;
 #if WIN
 using System.Windows.Forms;
@@ -29,16 +25,58 @@ namespace Launcher
 	internal static class MainClass
 	{
 #if WIN
-		[DllImport("kernel32.dll", SetLastError = true)]
-		private static extern bool AttachConsole(int dwProcessId);
+		[StructLayout(LayoutKind.Sequential)]
+		internal struct PROCESS_INFORMATION
+		{
+			public IntPtr hProcess;
+			public IntPtr hThread;
+			public int dwProcessId;
+			public int dwThreadId;
+		}
+
+		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+		public struct STARTUPINFO
+		{
+			public Int32 cb;
+			public string lpReserved;
+			public string lpDesktop;
+			public string lpTitle;
+			public Int32 dwX;
+			public Int32 dwY;
+			public Int32 dwXSize;
+			public Int32 dwYSize;
+			public Int32 dwXCountChars;
+			public Int32 dwYCountChars;
+			public Int32 dwFillAttribute;
+			public Int32 dwFlags;
+			public Int16 wShowWindow;
+			public Int16 cbReserved2;
+			public IntPtr lpReserved2;
+			public IntPtr hStdInput;
+			public IntPtr hStdOutput;
+			public IntPtr hStdError;
+		}
+
 		[DllImport("kernel32.dll", SetLastError = true)]
 		private static extern IntPtr GetStdHandle(StandardHandle nStdHandle);
-		[DllImport("kernel32.dll", SetLastError = true)]
-		private static extern bool SetStdHandle(StandardHandle nStdHandle, IntPtr handle);
-		[DllImport("kernel32.dll", SetLastError = true)]
-		private static extern FileType GetFileType(IntPtr handle);
-		[DllImport("kernel32.dll", SetLastError = true)]
-		private static extern bool FreeConsole();
+		[DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+		static extern IntPtr GetCommandLineW();
+		[DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+		static extern bool CreateProcessW(
+			IntPtr lpApplicationName,
+			IntPtr lpCommandLine,
+			IntPtr lpProcessAttributes,
+			IntPtr lpThreadAttributes,
+			bool bInheritHandles,
+			uint dwCreationFlags,
+			IntPtr lpEnvironment,
+			IntPtr lpCurrentDirectory,
+			[In] ref STARTUPINFO lpStartupInfo,
+			out PROCESS_INFORMATION lpProcessInformation);
+
+
+		const int SW_HIDE = 0;
+		const int SW_SHOW = 5;
 
 		private enum StandardHandle : uint
 		{
@@ -47,28 +85,17 @@ namespace Launcher
 			Error = unchecked((uint)-12)
 		}
 
-		private enum FileType : uint
-		{
-			Unknown = 0x0000,
-			Disk = 0x0001,
-			Char = 0x0002,
-			Pipe = 0x0003
-		}
+		const int STARTF_USESHOWWINDOW = 0x00000001;
+		const int STARTF_USESIZE = 0x00000002;
+		const int STARTF_USEPOSITION = 0x00000004;
+		const int STARTF_USECOUNTCHARS = 0x00000008;
+		const int STARTF_USEFILLATTRIBUTE = 0x00000010;
+		const int STARTF_RUNFULLSCREEN = 0x00000020;  // ignored for non-x86 platforms
+		const int STARTF_FORCEONFEEDBACK = 0x00000040;
+		const int STARTF_FORCEOFFFEEDBACK = 0x00000080;
+		const int STARTF_USESTDHANDLES = 0x00000100;
 
-		private static bool IsRedirected(IntPtr handle)
-		{
-			FileType fileType = GetFileType(handle);
-			return (fileType == FileType.Disk) || (fileType == FileType.Pipe);
-		}
-
-		public static void Redirect()
-		{
-			bool errorRedirected = IsRedirected(GetStdHandle(StandardHandle.Error));
-			AttachConsole(-1);
-			if (!errorRedirected) {
-				SetStdHandle(StandardHandle.Error, GetStdHandle(StandardHandle.Output));
-			}
-		}
+		private static IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);
 #endif // WIN
 
 		private static CommonBuilder builder;
@@ -76,14 +103,12 @@ namespace Launcher
 		[STAThread]
 		public static int Main(string[] args)
 		{
-#if WIN
-			Redirect();
-#endif // WIN
 #if MAC
 			args = args.Where(s => !s.StartsWith("-psn")).ToArray();
 #endif // MAC
 			var cli = new CommandLineApplication();
 			cli.Name = "Orange";
+			cli.Description = "Orange Launcher";
 			cli.HelpOption("-h --help");
 			var optionConsole = cli.Option<bool>("-c --console", "Console mode.", CommandOptionType.NoValue);
 			var optionJustBuild = cli.Option<bool>("-j --justbuild", "Build project without running executable.", CommandOptionType.NoValue);
@@ -91,8 +116,32 @@ namespace Launcher
 			var optionRunProjectPath = cli.Option<string>("-r --run <EXECUTABLE_PATH>", "Executable path, default: \"Orange/bin/%Platform%/Release/%PlatformExecutable%\".", CommandOptionType.SingleValue);
 			var optionRunArgs = cli.Option<string>("-a --runargs <ARGUMENTS>", "Args to pass to executable.", CommandOptionType.SingleValue);
 
-			cli.OnExecute(() =>
-			{
+			cli.OnExecute(() => {
+#if WIN
+				var stdoutHandle = GetStdHandle(StandardHandle.Output);
+				if (args.Length == 0 && stdoutHandle != INVALID_HANDLE_VALUE) {
+					PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+					STARTUPINFO si = new STARTUPINFO();
+					si.cb = Marshal.SizeOf(si);
+					si.dwFlags = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
+					si.wShowWindow = SW_SHOW;
+					si.hStdOutput = INVALID_HANDLE_VALUE;
+					si.hStdInput = INVALID_HANDLE_VALUE;
+					si.hStdError = INVALID_HANDLE_VALUE;
+					CreateProcessW(IntPtr.Zero,
+						GetCommandLineW(),
+						IntPtr.Zero,
+						IntPtr.Zero,
+						true,
+						0x00000008, // DETACHED_PROCESS
+						IntPtr.Zero,
+						IntPtr.Zero,
+						ref si,
+						out pi
+					);
+					return 0;
+				}
+#endif // WIN
 				RunExecutable = !optionJustBuild.HasValue();
 				builder = new Builder {
 					SolutionPath = optionBuildProjectPath.ParsedValue,
@@ -114,7 +163,6 @@ namespace Launcher
 				Console.WriteLine(e.Message);
 				return 1;
 			}
-			FreeConsole();
 			return 0;
 		}
 
