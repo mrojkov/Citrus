a6e37fb5f0f4bba2da6ce29bfda5e0f9d1433281
Anton Tkachev <misterio_anton@mail.ru>
2018-05-10 11:18:32 +0200

    [Launcher][Tangerine] Fix Tangerine.bat to Release version of the Orange

--- Tangerine.bat
+++ Tangerine.bat
@@ -1,3 +1,1 @@
-@echo off
-SET CMD=""Run Tangerine""
-start Orange\bin\Win\Debug\Orange.exe -b Tangerine\Tangerine.Win.sln -r Tangerine\bin\Release\Tangerine.exe
+start Orange\bin\Win\Release\Orange.exe -b Tangerine\Tangerine.Win.sln -r Tangerine\bin\Release\Tangerine.exe
