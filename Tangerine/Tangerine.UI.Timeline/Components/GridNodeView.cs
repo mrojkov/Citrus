79419a6b97051bacc9e6e2746e572bd59760a497
Roman Anoshin <anoshin97@gmail.com>
2018-06-27 11:19:48 +1000

    Added a figure "sector", if draw the type of interpolation Spline.

--- Tangerine/Tangerine.UI.Timeline/Components/GridNodeView.cs
+++ Tangerine/Tangerine.UI.Timeline/Components/GridNodeView.cs
@@ -109,7 +109,7 @@ namespace Tangerine.UI.Timeline.Components
 			}
 		}
 
-		static Vertex[] staticVertices = new Vertex[4];
+		static Vertex[] vertices;
 
 		protected void DrawFigure(Vector2 a, Vector2 b, KeyFunction func, Color4 color)
 		{
@@ -117,24 +117,39 @@ namespace Tangerine.UI.Timeline.Components
 			var segmentHeight = b.Y - a.Y;
 			switch (func) {
 				case KeyFunction.Linear:
-					var quadrangle = new Quadrangle {
-						V1 = new Vector2(a.X + 0.5f, b.Y - 0.5f),
-						V2 = new Vector2(b.X - 0.5f, a.Y - 0.5f),
-						V3 = new Vector2(b.X - 0.5f, b.Y - 0.5f),
-						V4 = new Vector2(b.X - 0.5f, b.Y - 0.5f)
-					};
-					Renderer.DrawQuadrangle(quadrangle, color);
+					vertices = new Vertex[3];
+					vertices[0].Pos = new Vector2(a.X, b.Y - 0.5f);
+					vertices[1].Pos = new Vector2(b.X, a.Y);
+					vertices[2].Pos = new Vector2(b.X, b.Y - 0.5f);
+					for(int i = 0; i < vertices.Count(); i++) {
+						vertices[i].Color = color;
+					}
+					Renderer.DrawTriangleFan(vertices, vertices.Count());
 					break;
 				case KeyFunction.Steep:
 					var rightBigVertexA = new Vector2(a.X + segmentWidth / 2, a.Y + 0.5f);
-					var rightBigVertexB = new Vector2(b.X - 0.5f, b.Y - 0.5f);
+					var rightBigVertexB = new Vector2(b.X, b.Y - 0.5f);
 					Renderer.DrawRect(rightBigVertexA, rightBigVertexB, color);
 					var leftSmallVertexA = new Vector2(a.X + 0.5f, a.Y + segmentHeight / 2);
 					var leftSmallVertexB = new Vector2(a.X + segmentWidth / 2, b.Y - 0.5f);
 					Renderer.DrawRect(leftSmallVertexA, leftSmallVertexB, color);
 					break;
 				case KeyFunction.Spline:
-
+					var numSegments = 10;
+					var center = b;
+					var radius = 0f;
+					if (segmentWidth < segmentHeight) {
+						radius = segmentWidth;
+					} else {
+						radius = segmentHeight;
+					}
+					vertices = new Vertex[numSegments + 1];
+					vertices[0] = new Vertex { Pos = center, Color = color };
+					for (int i = 0; i < numSegments; i++) {
+						vertices[i + 1].Pos = Vector2.CosSin(i * Mathf.HalfPi / (numSegments - 1)) * (-1)* radius + center;
+						vertices[i + 1].Color = color;
+					}
+					Renderer.DrawTriangleFan(vertices, numSegments + 1);
 					break;
 				case KeyFunction.ClosedSpline:
 					var circleCenter = new Vector2(a.X + segmentWidth / 2, a.Y + segmentHeight / 2);
