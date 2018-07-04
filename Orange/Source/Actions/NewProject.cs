b5488f3dc3cc5be488a2360810d9930c7b8d4dfc
Denis Rotanov <mapholameth@gmail.com>
2018-07-04 15:48:49 +1000

    [NewProject] use citrus as submodules; bring some githooks; init repo

--- Orange/Source/Actions/NewProject.cs
+++ Orange/Source/Actions/NewProject.cs
@@ -12,6 +12,32 @@ namespace Orange
 {
 	public static class NewProject
 	{
+		private static string newCitrusDirectory;
+		private static string targetDirectory;
+
+		private static void Exec(string name, string args)
+		{
+			var process = new System.Diagnostics.Process {
+				StartInfo = {
+					FileName = name,
+					Arguments = args,
+					UseShellExecute = false,
+					WorkingDirectory = targetDirectory,
+				}
+			};
+			process.Start();
+			process.WaitForExit();
+			if (process.ExitCode != 0) {
+				Console.WriteLine($"Process {name} {args} exited with code {process.ExitCode}");
+				throw new InvalidOperationException();
+			}
+		}
+
+		private static void Git(string gitArgs)
+		{
+			Exec("git", gitArgs);
+		}
+
 		[Export(nameof(OrangePlugin.MenuItems))]
 		[ExportMetadata("Label", "New Project")]
 		[ExportMetadata("Priority", 3)]
@@ -20,15 +46,16 @@ namespace Orange
 			Application.InvokeOnMainThread(() => {
 				var citrusPath = Toolbox.CalcCitrusDirectory();
 				var dlg = new FileDialog {
-					InitialDirectory = citrusPath,
 					AllowedFileTypes = new string[] { "" },
 					Mode = FileDialogMode.SelectFolder
 				};
 				if (dlg.RunModal()) {
+					targetDirectory = dlg.FileName;
+					newCitrusDirectory = Path.Combine(dlg.FileName, "Citrus");
 					var projectName = Path.GetFileName(dlg.FileName);
 					var newProjectApplicationName = String.Join(" ", Regex.Split(projectName, @"(?<!^)(?=[A-Z])"));
 					Console.WriteLine($"New project name is \"{projectName}\"");
-					HashSet<string> textfileExtensions = new HashSet<string> { ".cs", ".xml", ".csproj", ".sln", ".citproj", ".projitems", ".shproj", ".txt", ".plist", ".strings" };
+					HashSet<string> textfileExtensions = new HashSet<string> { ".cs", ".xml", ".csproj", ".sln", ".citproj", ".projitems", ".shproj", ".txt", ".plist", ".strings", ".gitignore", };
 					using (var dc = new DirectoryChanger($"{citrusPath}/Samples/EmptyProject/")) {
 						var fe = new FileEnumerator(".");
 						foreach (var f in fe.Enumerate()) {
@@ -41,14 +68,19 @@ namespace Orange
 								text = text.Replace("EmptyProject", projectName);
 								text = text.Replace("Empty Project", newProjectApplicationName);
 								text = text.Replace("emptyproject", projectName.ToLower());
-								var citrusUri = new Uri(citrusPath);
+								var citrusUri = new Uri(newCitrusDirectory);
 								var fileUri = new Uri(targetPath);
 								var relativeUri = fileUri.MakeRelativeUri(citrusUri);
+								// TODO: apply only to .sln and .csproj file
 								text = text.Replace("..\\..\\..", relativeUri.ToString());
 								File.WriteAllText(targetPath, text);
 							}
 						}
 					}
+					Git("init");
+					Git("add -A");
+					Git("submodule add https://gitlab.game-forest.com:8888/Common/Citrus.git Citrus");
+					Git("commit -m\"Initial commit.\"");
 				}
 			});
 		}
