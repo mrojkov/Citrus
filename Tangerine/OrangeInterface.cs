f4882e800b49bd13575c48e15bf9fe02261a8acd
Mikhail Rozhkov <mrozhkov@game-forest.com>
2018-07-30 13:12:05 +1000

    [Tangerine] Refactoring.

--- Tangerine/OrangeInterface.cs
+++ Tangerine/OrangeInterface.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using Lime;
@@ -9,7 +10,7 @@ namespace Tangerine
 {
 	public class OrangeInterface : Orange.UserInterface
 	{
-		public OrangePluginUIBuidler PluginUIBuilder;
+		public OrangePluginUIBuilder PluginUIBuilder;
 
 		public readonly PlatformPicker PlatformPicker = new PlatformPicker();
 
@@ -45,7 +46,7 @@ namespace Tangerine
 
 		public override IPluginUIBuilder GetPluginUIBuilder()
 		{
-			PluginUIBuilder = new OrangePluginUIBuidler();
+			PluginUIBuilder = new OrangePluginUIBuilder();
 			return PluginUIBuilder;
 		}
 
@@ -55,6 +56,67 @@ namespace Tangerine
 		public override void OnWorkspaceOpened()
 		{
 			PlatformPicker.Reload();
+		}		
+	}
+	
+	public class PlatformPicker : ThemedDropDownList
+	{
+		public PlatformPicker()
+		{
+			Reload();
+		}
+
+		public void Reload()
+		{
+			var savedIndex = Index;
+			Index = -1;
+			Items.Clear();
+			foreach (var target in The.Workspace.Targets) {
+				Items.Add(new Item(target.Name, target));
+			}
+			if (savedIndex >= 0 && savedIndex < Items.Count) {
+				Index = savedIndex;
+			} else {
+				Index = 0;
+			}
+		}
+
+		public Target SelectedTarget => (Target)Items[Index].Value;
+	}
+	
+	public class OrangePluginUIBuilder : IPluginUIBuilder
+	{
+		public IPluginPanel SidePanel { get; } = new OrangePluginPanel();
+	}
+
+	public class OrangePluginPanel :  IPluginPanel
+	{
+		public class PluginCheckBox : ICheckBox
+		{
+			public string Label { get; }
+			public bool Active { get; set; }
+			public event EventHandler Toggled;
+
+			public PluginCheckBox(string label)
+			{
+				Label = label;
+			}
+
+			public void Toogle()
+			{
+				Toggled?.Invoke(this, EventArgs.Empty);
+			}
+		}
+
+		public bool Enabled { get; set; }
+		public string Title { get; set; }
+		public List<PluginCheckBox> CheckBoxes { get; } = new List<PluginCheckBox>();
+
+		public ICheckBox AddCheckBox(string label)
+		{
+			var checkBox = new PluginCheckBox(label);
+			CheckBoxes.Add(checkBox);
+			return checkBox;
 		}
 	}
 }
