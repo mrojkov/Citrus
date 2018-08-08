c5955483ade54598506182f3f8ea3526cb2bfce8
kuzymov <konstantinreed@gmail.com>
2018-08-08 16:09:20 +1000

    [Tangerine][NineGridLinePresenter] Code style fixes

--- Tangerine/Tangerine.UI.SceneView/Presenters/NineGridLinePresenter.cs
+++ Tangerine/Tangerine.UI.SceneView/Presenters/NineGridLinePresenter.cs
@@ -1,21 +1,18 @@
-using Lime;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Lime;
 
 namespace Tangerine.UI.SceneView
 {
-
-	class NineGridLinePresenter
+	public class NineGridLinePresenter
 	{
 		public NineGridLinePresenter(SceneView sceneView)
 		{
 			sceneView.Frame.CompoundPostPresenter.Add(new DelegatePresenter<Widget>(Render));
 		}
 
-		private void Render(Widget canvas)
+		private static void Render(Widget canvas)
 		{
 			var grids = Core.Document.Current.SelectedNodes().Editable().OfType<NineGrid>();
 			foreach (var grid in grids) {
@@ -26,53 +23,53 @@ namespace Tangerine.UI.SceneView
 		}
 	}
 
-	class NineGridLine
+	public class NineGridLine
 	{
-		public NineGrid Owner { get; private set; }
-		public string PropertyName => PropertyNames[index];
-		public float Value => PropertyGetters[index](Owner);
-		public float TextureSize => TextureSizeGetters[index % 2](Owner);
-		public float GridSize => NineGridSizeGetters[index % 2](Owner);
+		public NineGrid Owner { get; }
+		public string PropertyName => propertyNames[index];
+		public float Value => propertyGetters[index](Owner);
+		public float TextureSize => textureSizeGetters[index % 2](Owner);
+		public float GridSize => nineGridSizeGetters[index % 2](Owner);
 		public float MaxValue => GridSize / TextureSize;
-		public float Scale => NineGridScaleGetters[index % 2](Owner);
+		public float Scale => nineGridScaleGetters[index % 2](Owner);
 
 		private readonly int index;
-		private int IndexA => Indexes[index].Item1;
-		private int IndexB => Indexes[index].Item2;
+		private int IndexA => indexes[index].Item1;
+		private int IndexB => indexes[index].Item2;
 		private Vector2 A => Owner.Parts[IndexA].Rect.A;
 		private Vector2 B => Owner.Parts[IndexB].Rect.B;
 
-		private readonly Tuple<int, int>[] Indexes = {
-			Tuple.Create(5, 2), Tuple.Create(7, 1),
-			Tuple.Create(1, 6), Tuple.Create(2, 8)
+		private static readonly Tuple<int, int>[] indexes = {
+			Tuple.Create(5, 2),
+			Tuple.Create(7, 1),
+			Tuple.Create(1, 6),
+			Tuple.Create(2, 8)
 		};
-
-		private readonly string[] PropertyNames = {
-			nameof(NineGrid.LeftOffset),  nameof(NineGrid.TopOffset),
-			nameof(NineGrid.RightOffset), nameof(NineGrid.BottomOffset)
+		private static readonly string[] propertyNames = {
+			nameof(NineGrid.LeftOffset),
+			nameof(NineGrid.TopOffset),
+			nameof(NineGrid.RightOffset),
+			nameof(NineGrid.BottomOffset)
 		};
-
-		private readonly Func<NineGrid, float>[] PropertyGetters = {
-			g => g.LeftOffset, g => g.TopOffset,
-			g => g.RightOffset, g => g.BottomOffset
+		private static readonly Func<NineGrid, float>[] propertyGetters = {
+			g => g.LeftOffset,
+			g => g.TopOffset,
+			g => g.RightOffset,
+			g => g.BottomOffset
 		};
-
-		private readonly Func<NineGrid, float>[] TextureSizeGetters = {
+		private static readonly Func<NineGrid, float>[] textureSizeGetters = {
 			g => g.Texture.ImageSize.Width,
 			g => g.Texture.ImageSize.Height
 		};
-
-		private readonly Func<NineGrid, float>[] NineGridSizeGetters = {
+		private static readonly Func<NineGrid, float>[] nineGridSizeGetters = {
 			g => g.Size.X,
 			g => g.Size.Y
 		};
-
-		private readonly Func<NineGrid, float>[] NineGridScaleGetters = {
+		private static readonly Func<NineGrid, float>[] nineGridScaleGetters = {
 			g => g.Scale.X,
 			g => g.Scale.Y
 		};
-
-		private readonly Vector2[] Directions = {
+		private static readonly Vector2[] directions = {
 			new Vector2(1, 0), new Vector2(0, 1),
 			new Vector2(-1, 0), new Vector2(0, -1)
 		};
@@ -86,45 +83,45 @@ namespace Tangerine.UI.SceneView
 		public void Render(Widget canvas)
 		{
 			var matrix = Owner.CalcTransitionToSpaceOf(canvas);
-			var A = matrix.TransformVector(this.A);
-			var B = matrix.TransformVector(this.B);
-			Renderer.DrawLine(A, B, Color4.Red, 2);
+			var a = matrix.TransformVector(A);
+			var b = matrix.TransformVector(B);
+			Renderer.DrawLine(a, b, Color4.Red, 2);
 		}
 
 		public bool HitTest(Vector2 point, Widget canvas, float radius = 20)
 		{
 			var matrix = Owner.CalcTransitionToSpaceOf(canvas);
-			var A = matrix.TransformVector(this.A);
-			var B = matrix.TransformVector(this.B);
-			var length = (A - B).Length;
+			var a = matrix.TransformVector(A);
+			var b = matrix.TransformVector(B);
+			var length = (a - b).Length;
 			return
-				DistanceFromPointToLine(A, B, point, out Vector2 linePoint) <= radius &&
-				(A - linePoint).Length <= length &&
-				(B - linePoint).Length <= length;
+				DistanceFromPointToLine(a, b, point, out var linePoint) <= radius &&
+				(a - linePoint).Length <= length &&
+				(b - linePoint).Length <= length;
 		}
 
 		public Vector2 GetDirection()
 		{
 			var matrix = Owner.CalcLocalToParentTransform();
-			return (matrix * Directions[index] - matrix * Vector2.Zero).Normalized;
+			return (matrix * directions[index] - matrix * Vector2.Zero).Normalized;
 		}
 
-		private static float DistanceFromPointToLine(Vector2 A, Vector2 B, Vector2 P, out Vector2 point)
+		private static float DistanceFromPointToLine(Vector2 a, Vector2 b, Vector2 p, out Vector2 point)
 		{
-			var a = A.Y - B.Y;
-			var b = B.X - A.X;
-			var c = B.Y * A.X - A.Y * B.X;
+			var i = a.Y - b.Y;
+			var j = b.X - a.X;
+			var k = b.Y * a.X - a.Y * b.X;
 			point = new Vector2 {
-				X = b * (b * P.X - a * P.Y) - a * c,
-				Y = a * (-b * P.X + a * P.Y) - b * c
+				X = j * (j * p.X - i * p.Y) - i * k,
+				Y = i * (-j * p.X + i * p.Y) - j * k
 			};
-			point /= a * a + b * b;
-			return Mathf.Abs(P.X * a + P.Y * b + c) / Mathf.Sqrt(a * a + b * b);
+			point /= i * i + j * j;
+			return Mathf.Abs(p.X * i + p.Y * j + k) / Mathf.Sqrt(i * i + j * j);
 		}
 
 		public static IEnumerable<NineGridLine> GetForNineGrid(NineGrid nineGrid)
 		{
-			for (int i = 0; i < 4; ++i) {
+			for (var i = 0; i < 4; ++i) {
 				yield return new NineGridLine(i, nineGrid);
 			}
 		}
