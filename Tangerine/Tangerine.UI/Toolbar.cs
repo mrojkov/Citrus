dc6793cc96fc5e0e39619f344387dafeb9448702
Tyshchenko Andrey <ggoldenfeniks@gmail.com>
2018-08-16 15:20:25 +1000

    [CIT-501] Fix ToolbarPanel dragging

--- Tangerine/Tangerine.UI/Toolbar.cs
+++ Tangerine/Tangerine.UI/Toolbar.cs
@@ -29,19 +29,25 @@ namespace Tangerine
 		public void Rebuild()
 		{
 			widget.Nodes.Clear();
-			foreach (var row in toolbarLayout.Rows) {
+			for (int i = 0; i < toolbarLayout.Rows.Count; ++i) {
+				var row = toolbarLayout.Rows[i];
 				var rowWidget = new Widget {
 					MinMaxHeight = Metrics.ToolbarHeight,
 					LayoutCell = new LayoutCell { StretchY = 0 },
 					Layout = new HBoxLayout()
 				};
-				foreach (var panel in row.Panels) {
+				for (int j = 0; j < row.Panels.Count; ++j) {
+					var panel = row.Panels[j];
 					var panelWidget = new Widget {
 						Layout = new HBoxLayout { Spacing = 1, CellDefaults = new LayoutCell(Alignment.LeftCenter) },
 						LayoutCell = new LayoutCell { StretchY = 0 },
 					};
 					if (panel.Draggable) {
-						panelWidget.Awoke += PanelAwake;
+						int rowIndex = i;
+						int panelIndex = j;
+						panelWidget.Awoke += n => {
+							PanelAwake(n, rowIndex, panelIndex);
+						};
 					}
 					foreach (var id in panel.CommandIds) {
 						if (CommandRegister.TryGetCommand(id, out ICommand command)) {
@@ -58,11 +64,12 @@ namespace Tangerine
 					}
 					rowWidget.AddNode(panelWidget);
 				}
+				rowWidget.AddNode(new Widget());
 				widget.AddNode(rowWidget);
 			}
 		}
 
-		private void PanelAwake(Node node)
+		private void PanelAwake(Node node, int rowIndex, int panelIndex)
 		{
 			var drag = new Image {
 				Texture = IconPool.GetTexture("Tools.ToolbarSeparator"),
@@ -70,13 +77,78 @@ namespace Tangerine
 				MinMaxSize = new Vector2(16),
 				HitTestTarget = true
 			};
-			drag.Clicked += () => node.Tasks.Add(DragTask);
+			node.Tasks.Add(() => DragTask(node.AsWidget, drag.Input, rowIndex, panelIndex));
 			node.Nodes.Insert(0, drag);
 		}
 
-		private IEnumerator<object> DragTask()
+		private IEnumerator<object> DragTask(Widget panelContainer, WidgetInput input, int rowIndex, int panelIndex)
 		{
-			yield return null;
+			int newRowIndex = rowIndex;
+			int newPanelIndex = panelIndex;
+			var currentPanel = panelContainer;
+			while (true) {
+				if (!input.WasMousePressed()) {
+					yield return null;
+					continue;
+				}
+				widget.CompoundPostPresenter.Add(new DelegatePresenter<Widget>(w => {
+					w.PrepareRendererState();
+					var pos = currentPanel.CalcPositionInSpaceOf(w);
+					Renderer.DrawRect(pos, pos + panelContainer.Size, Color4.Blue.Transparentify(0.7f));
+					Renderer.DrawRectOutline(pos, pos + panelContainer.Size, Color4.Blue);
+				}));
+				while (input.IsMousePressed()) {
+					Utils.ChangeCursorIfDefault(MouseCursor.Hand);
+					for (int i = 0; i < widget.Nodes.Count; ++i) {
+						var rowWidget = (Widget)widget.Nodes[i];
+						if (!IsMouseOver(rowWidget, out Vector2 pos)) {
+							continue;
+						}
+						for (int j = 0; j < rowWidget.Nodes.Count; ++j) {
+							var panelWidget = (Widget)rowWidget.Nodes[j];
+							if (!IsMouseOver(panelWidget, out pos)) {
+								continue;
+							}
+							if (newRowIndex == i && newPanelIndex == j) {
+								goto Next;
+							}
+							bool isDifferentRow = newRowIndex != i;
+							bool isBefore = newPanelIndex < j && pos.X >= panelWidget.Width / 2;
+							bool isAfter = newPanelIndex > j && pos.X <= panelWidget.Width / 2;
+							if (isDifferentRow || isBefore || isAfter) {
+								currentPanel = panelWidget;
+								newRowIndex = i;
+								newPanelIndex = j;
+							}
+							goto Next;
+						}
+					}
+					Next:
+					yield return null;
+				}
+				widget.CompoundPostPresenter.RemoveAt(widget.CompoundPostPresenter.Count - 1);
+				if (newRowIndex == rowIndex && newPanelIndex == panelIndex) {
+					continue;
+				}
+				var row = toolbarLayout.Rows[newRowIndex];
+				var panel = toolbarLayout.Rows[rowIndex].Panels[panelIndex];
+				if (newRowIndex != rowIndex) {
+					toolbarLayout.RemovePanel(panel);
+					toolbarLayout.InsertPanel(row, panel, newPanelIndex);
+				} else {
+					toolbarLayout.SwapPanels(row.Panels[newPanelIndex], panel);
+				}
+				rowIndex = newRowIndex;
+				panelIndex = newPanelIndex;
+				toolbarLayout.RefreshAfterLoad();
+				Rebuild();
+			}
+		}
+
+		private static bool IsMouseOver(Widget widget, out Vector2 pos)
+		{
+			pos = widget.LocalMousePosition();
+			return pos.Y >= 0 && pos.Y <= widget.Height && pos.X >= 0 && pos.X <= widget.Width;
 		}
 
 		static void DecorateToolbar(Widget widget)
