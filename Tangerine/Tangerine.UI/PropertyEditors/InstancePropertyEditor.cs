ccd5fe225deba446087d09b0b50eb21ebf3d1b50
Denis Rotanov <mapholameth@gmail.com>
2018-08-23 11:04:20 +1000

    [Tangerine] cache [type -> derived types] in instance property editor

--- Tangerine/Tangerine.UI/PropertyEditors/InstancePropertyEditor.cs
+++ Tangerine/Tangerine.UI/PropertyEditors/InstancePropertyEditor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Lime;
@@ -9,6 +10,8 @@ namespace Tangerine.UI
 	public class InstancePropertyEditor<T> : ExpandablePropertyEditor<T>
 	{
 		public DropDownList Selector { get; }
+
+		private static Dictionary<Type, List<Type>> derivedTypesCache = new Dictionary<Type,List<Type>>();
 
 		public InstancePropertyEditor(IPropertyEditorParams editorParams, Action<Widget> OnValueChanged) : base(editorParams)
 		{
@@ -27,18 +30,25 @@ namespace Tangerine.UI
 			if (!propertyType.IsInterface) {
 				Selector.Items.Add(new CommonDropDownList.Item(propertyType.Name, propertyType));
 			}
-			// TODO: cache derived types if this somehow proves to be slow enough to notice
-			foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies()) {
-				var assignables = assembly
-					.GetTypes()
-					.Where(t =>
-						!t.IsInterface &&
-						t.GetCustomAttribute<TangerineIgnoreAttribute>(false) == null &&
-						t != propertyType &&
-						propertyType.IsAssignableFrom(t));
-				foreach (var type in assignables) {
-					Selector.Items.Add(new CommonDropDownList.Item(type.Name, type));
+			// TODO: invalidate cache on loading new assemblies at runtime
+			if (!derivedTypesCache.TryGetValue(propertyType, out List<Type> derivedTypes)) {
+				derivedTypes = new List<Type>();
+				foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies()) {
+					var assignables = assembly
+						.GetTypes()
+						.Where(t =>
+							!t.IsInterface &&
+							t.GetCustomAttribute<TangerineIgnoreAttribute>(false) == null &&
+							t != propertyType &&
+							propertyType.IsAssignableFrom(t));
+					foreach (var type in assignables) {
+						derivedTypes.Add(type);
+					}
 				}
+				derivedTypesCache.Add(propertyType, derivedTypes);
+			}
+			foreach (var t in derivedTypes) {
+				Selector.Items.Add(new CommonDropDownList.Item(t.Name, t));
 			}
 			Selector.Changed += a => {
 				if (a.ChangedByUser) {
