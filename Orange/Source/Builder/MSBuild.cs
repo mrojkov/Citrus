63315a5c8d766a9efb299334aec1b7d7a8672f0f
Artem Popov <apopov@game-forest.com>
2018-08-27 11:32:09 +0200

    Update MSBuild build system
    1. Add the search for pure MSBuild 15 (from MSBuild Tools);
    2. Remove the search for rudimentary MSBuild 14.

--- Orange/Source/Builder/MSBuild.cs
+++ Orange/Source/Builder/MSBuild.cs
@@ -62,24 +62,35 @@ namespace Orange.Source
 
 		private static bool TryGetMSBuildPath(out string path)
 		{
-			var visualStudioRegistryPath = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7");
-			if (visualStudioRegistryPath != null) {
-				var vsPath = visualStudioRegistryPath.GetValue("15.0", string.Empty) as string;
-				var msBuild15Path = Path.Combine(vsPath, "MSBuild", "15.0", "Bin", "MSBuild.exe");
-				if (File.Exists(msBuild15Path)) {
-					path = msBuild15Path;
-					return true;
-				}
-			}
+			return TryGetPureMSBuild15Path(out path) || TryGetVsMSBuild15Path(out path);
+		}
 
-			// MSBuild from path obtained with RuntimeEnvironment.GetRuntimeDirectory() is unable to compile C#6.0
-			var msBuild14Path = Path.Combine(@"C:\Program Files (x86)\MSBuild\14.0\Bin\", "MSBuild.exe");
-			if (File.Exists(msBuild14Path)) {
-				path = msBuild14Path;
+		private static bool TryGetPureMSBuild15Path(out string path)
+		{
+			const string msBuildPath =
+				@"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe";
+
+			if (File.Exists(msBuildPath)) {
+				path = msBuildPath;
 				return true;
 			}
-
 			path = null;
+			return false;
+		}
+
+		private static bool TryGetVsMSBuild15Path(out string path)
+		{
+			path = null;
+			var visualStudioRegistryPath = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7");
+			if (visualStudioRegistryPath == null) {
+				return false;
+			}
+			var vsPath = visualStudioRegistryPath.GetValue("15.0", string.Empty) as string;
+			var msBuild15Path = Path.Combine(vsPath, "MSBuild", "15.0", "Bin", "MSBuild.exe");
+			if (File.Exists(msBuild15Path)) {
+				path = msBuild15Path;
+				return true;
+			}
 			return false;
 		}
 	}
