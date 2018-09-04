dcc7dac5b027c3bff32a49211bd4b997d8aba6ef
Alexander Gomenyuk <gomeniuk.aa@gmail.com>
2018-09-04 16:21:22 +1000

    [CIT-729] Add abbility to search colors in Theme tab in Preferences window

--- Tangerine/Tangerine/Dialogs/ColorThemeEditor.cs
+++ Tangerine/Tangerine/Dialogs/ColorThemeEditor.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Reflection;
 using Lime;
+using Tangerine.Core;
 using Tangerine.UI;
 
 namespace Tangerine.Dialogs
 {
 	public class ColorThemeEditor: Widget
 	{
+		private ThemedScrollView pane;
+		private ThemedEditBox searchBox;
+
 		public int Version { get; private set; }
 
 		public ColorThemeEditor()
@@ -17,7 +21,7 @@ namespace Tangerine.Dialogs
 		public void Rebuild()
 		{
 			Nodes.Clear();
-			var pane = new ThemedScrollView();
+			pane = new ThemedScrollView();
 			pane.Content.Layout = new VBoxLayout { Spacing = 4 };
 			pane.Content.Padding = new Thickness(10);
 			var flags =
@@ -52,7 +56,34 @@ namespace Tangerine.Dialogs
 					);
 				}
 			}
+			AddNode(CreateSearchBox());
 			AddNode(pane);
+		}
+
+		private Widget CreateSearchBox()
+		{
+			searchBox = new ThemedEditBox();
+			var firstCall = true;
+			searchBox.AddChangeWatcher(
+				() => searchBox.Text,
+				_ => {
+					if (firstCall) {
+						firstCall = false;
+						return;
+					}
+					ShowSearchedColors();
+				}
+			);
+			return new Widget {
+				Layout = new HBoxLayout(),
+				LayoutCell = new LayoutCell { StretchX = 2 },
+				Nodes = {
+					new ThemedSimpleText("Search: ") {
+						LayoutCell = new LayoutCell(Alignment.LeftCenter)
+					},
+					searchBox
+				}
+			};
 		}
 
 		private void CreateColorEditor(ThemedScrollView container, object source, string propertyName, string displayName, Func<object> valueGetter)
@@ -73,6 +104,23 @@ namespace Tangerine.Dialogs
 		private void Editor_Changed()
 		{
 			Version++;
+		}
+
+		private void ShowSearchedColors()
+		{
+			pane.ScrollPosition = 0;
+			var isShowingAll = searchBox.Text.Length == 0;
+			var filter = searchBox.Text.ToLower();
+			foreach (var node in pane.Content.Nodes) {
+				switch (node) {
+					case ThemedFrame tf:
+						break;
+					case Widget w:
+						var colorName = (w.Nodes[0].Nodes[1] as ThemedSimpleText).Text.ToLower();
+						w.Visible = colorName.Contains(filter) || isShowingAll;
+						break;
+				}
+			}
 		}
 	}
 }
