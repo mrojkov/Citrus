d24605059204f31c7e3281557a92f153bb2e2d83
Mikhail Rozhkov <mrozhkov@game-forest.com>
2018-09-04 16:43:26 +1000

    [Tangerine][Mac] Skip Xamarin debugger specific argument

--- Tangerine/Main.Mac.cs
+++ Tangerine/Main.Mac.cs
@@ -1,4 +1,5 @@
 ﻿#if MAC
+using System.Linq;
 using AppKit;
 using Foundation;
 using Tangerine.Core;
@@ -9,6 +10,7 @@ namespace Tangerine
 	{
 		static void Main(string[] args)
 		{
+			args = args.Where(i => !i.StartsWith("-psn")).ToArray(); // Skip Xamarin debugger specific arguments.
 			Lime.Application.Initialize();
             NSApplication.SharedApplication.DidFinishLaunching += (sender, e) => TangerineApp.Initialize(args);
 			Lime.Application.Run();
