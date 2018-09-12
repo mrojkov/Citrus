2d926b8b7903dda3f5b5f6b69bc2f2177376562f
Denis Rotanov <mapholameth@gmail.com>
2018-09-12 11:58:01 +1000

    [Lime][Orange] move code for generating lime deserializers to Orange; add post build action to Lime project to generate binary deserializers

--- Orange/Source/Actions/GenerateLimeDeserializers.cs
+++ Orange/Source/Actions/GenerateLimeDeserializers.cs
@@ -1,8 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using Lime;
+using Yuzu.Binary;
+using Yuzu.Metadata;
 
 namespace Orange
 {
@@ -13,8 +18,72 @@ namespace Orange
 		[ExportMetadata("Priority", 5)]
 		public static void GenerateLimeDeserializersAction()
 		{
-			Lime.Serialization.GenerateBinaryDeserializers();
+			GenerateBinaryDeserializers();
 			Console.WriteLine("Done. Please rebuild Orange.");
+		}
+
+		public static void GenerateDeserializers(string filename, string rootNamespace, List<Type> types)
+		{
+			var yjdg = new BinaryDeserializerGenerator(rootNamespace);
+			using (var ms = new MemoryStream())
+			using (var sw = new StreamWriter(ms)) {
+				yjdg.GenWriter = sw;
+				yjdg.GenerateHeader();
+				foreach (var generate in types
+					.Select(t => yjdg.GetType()
+						.GetMethod("Generate")
+						.MakeGenericMethod(t))) {
+					generate.Invoke(yjdg, new object[] { });
+				}
+				yjdg.GenerateFooter();
+				sw.Flush();
+				ms.WriteTo(new FileStream(filename, FileMode.Create));
+			}
+		}
+
+		public static void GenerateBinaryDeserializers()
+		{
+			var jd = new BinaryDeserializerGenerator("GeneratedDeserializersBIN");
+			using (var ms = new MemoryStream())
+			using (var sw = new StreamWriter(ms)) {
+				jd.GenWriter = sw;
+				jd.GenerateHeader();
+
+				var types = new List<Type>();
+				var assembly = AppDomain.CurrentDomain.GetAssemblies().Where(a => a.FullName.StartsWith("Lime", StringComparison.OrdinalIgnoreCase)).First();
+				foreach (var t in assembly.GetTypes()) {
+					if (t.GetCustomAttribute<YuzuDontGenerateDeserializerAttribute>(false) != null) {
+						continue;
+					}
+					if (t.IsGenericType) {
+						if (t == typeof(Keyframe<>) || t == typeof(Animator<>)) {
+							foreach (var specializationType in AnimatorRegistry.Instance.EnumerateRegisteredTypes()) {
+								var specializedType = t.MakeGenericType(new[] { specializationType });
+								types.Add(specializedType);
+							}
+						} else {
+							foreach (var specializationType in t.GetCustomAttributes<YuzuSpecializeWithAttribute>().Select(a => a.Type)) {
+								var specializedType = t.MakeGenericType(new[] { specializationType });
+								types.Add(specializedType);
+							}
+						}
+					} else {
+						var meta = Yuzu.Metadata.Meta.Get(t, Serialization.YuzuCommonOptions);
+						if (meta.Items.Count != 0) {
+							types.Add(t);
+						}
+					}
+				}
+				types.Sort((a, b) => a.FullName.CompareTo(b.FullName));
+				foreach (var t in types) {
+					jd.Generate(t);
+					Console.WriteLine(t.FullName);
+				}
+
+				jd.GenerateFooter();
+				sw.Flush();
+				ms.WriteTo(new FileStream(Path.Combine(Toolbox.CalcCitrusDirectory(), "Lime", "Source", "GeneratedDeserializersBIN.cs"), FileMode.Create));
+			}
 		}
 	}
 }
