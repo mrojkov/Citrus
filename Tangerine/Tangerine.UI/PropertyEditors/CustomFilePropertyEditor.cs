4ce04f31a7b2b75eef7347e8c0745d7988f94649
Denis Rotanov <mapholameth@gmail.com>
2018-09-25 18:34:35 +1000

    [Tan][CustomFilePropertyEditor] imply T is string when no converters between string and object are provided

--- Tangerine/Tangerine.UI/PropertyEditors/CustomFilePropertyEditor.cs
+++ Tangerine/Tangerine.UI/PropertyEditors/CustomFilePropertyEditor.cs
@@ -10,9 +10,9 @@ namespace Tangerine.UI
 			this.filePropertyAttribute = attribute;
 		}
 
-		protected override string ValueToStringConverter(T value) => filePropertyAttribute?.ValueToStringConverter(EditorParams.Type, value) ?? "";
+		protected override string ValueToStringConverter(T value) => filePropertyAttribute?.ValueToStringConverter(EditorParams.Type, value) ?? value?.ToString() ?? string.Empty;
 
-		protected override T StringToValueConverter(string path) => filePropertyAttribute != null ? filePropertyAttribute.StringToValueConverter<T>(EditorParams.Type, path) : default;
+		protected override T StringToValueConverter(string path) => filePropertyAttribute != null ? filePropertyAttribute.StringToValueConverter<T>(EditorParams.Type, path) : (T)(object)path;
 
 		protected override void AssignAsset(string path)
 		{
