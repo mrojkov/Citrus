6e40a024912d88e2f2d869d5382057f97672f2ba
Denis Rotanov <mapholameth@gmail.com>
2018-10-18 14:53:33 +1000

    [Mac][Lime] support rename events in filesystem watcher on mac

--- Lime/Source/Application/FileSystemWatcher/FileSystemWatcher.Mac.cs
+++ Lime/Source/Application/FileSystemWatcher/FileSystemWatcher.Mac.cs
@@ -8,6 +8,7 @@ namespace Lime
 	public class FileSystemWatcher : IFileSystemWatcher
 	{
 		private FSEventStream fsEventStream;
+		private FSEvent? lastRenameEvent;
 
 		public event Action<string> Changed;
 		public event Action<string> Created;
@@ -41,19 +42,40 @@ namespace Lime
 				}
 				if ((e.Flags & FSEventStreamEventFlags.ItemCreated) != 0) {
 					if (Filter?.Invoke(e.Path) ?? true) {
-						Changed?.Invoke(e.Path);
+						Created?.Invoke(e.Path);
 					}
 				}
 				if ((e.Flags & FSEventStreamEventFlags.ItemRemoved) != 0) {
 					if (Filter?.Invoke(e.Path) ?? true) {
-						Changed?.Invoke(e.Path);
+						Deleted?.Invoke(e.Path);
 					}
 				}
 				if ((e.Flags & FSEventStreamEventFlags.ItemRenamed) != 0) {
-					if (Filter?.Invoke(e.Path) ?? true) {
-						Changed?.Invoke(/* TODO: pass previous path before rename here */, e.Path);
+					if (((e.Flags & FSEventStreamEventFlags.ItemIsFile) != 0 && System.IO.File.Exists(e.Path)) ||
+					    ((e.Flags & FSEventStreamEventFlags.ItemIsDir) != 0 && System.IO.Directory.Exists(e.Path))) {
+						if (lastRenameEvent != null && lastRenameEvent.Value.Id == e.Id - 1) {
+							Renamed?.Invoke (lastRenameEvent.Value.Path, e.Path);
+							lastRenameEvent = null;
+						} else {
+							Created?.Invoke(e.Path);
+						}
+					} else {
+						CleanLastRenameEvent();
+						lastRenameEvent = e;
+						continue;
 					}
 				}
+				CleanLastRenameEvent();
+			}
+		}
+
+		private void CleanLastRenameEvent() {
+			if (lastRenameEvent != null) {
+				var path = lastRenameEvent.Value.Path;
+				if (Filter?.Invoke(lastRenameEvent.Value.Path) ?? true) {
+					Deleted?.Invoke(lastRenameEvent.Value.Path);
+				}
+				lastRenameEvent = null;
 			}
 		}
 
