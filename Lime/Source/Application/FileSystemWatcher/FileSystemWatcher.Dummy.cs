012868e2fdddf0881db2f618156a887e9c00e861
Anton Tkachev <atkachev@game-forest.com>
2018-10-24 17:09:28 +0200

    [Lime] Add Dummy FileSystemWatcher

---
+++ Lime/Source/Application/FileSystemWatcher/FileSystemWatcher.Dummy.cs
@@ -0,0 +1,22 @@
+﻿#if !WIN && !MAC
+using System;
+
+namespace Lime
+{
+	public class FileSystemWatcher : IFileSystemWatcher
+	{
+		public event Action<string> Changed;
+		public event Action<string> Created;
+		public event Action<string> Deleted;
+		public event Action<string, string> Renamed;
+
+		public FileSystemWatcher(string path, bool includeSubdirectories)
+		{
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+}
+#endif
