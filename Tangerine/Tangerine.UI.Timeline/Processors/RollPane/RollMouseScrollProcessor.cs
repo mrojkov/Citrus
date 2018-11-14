mrojkov/Citrus

commit 541148404465d5937928d45f58ffb28f5c3491b2

[CIT-346] Scroll after expanding nodes

Tangerine/Tangerine.UI.Timeline/Processors/RollPane/RollMouseScrollProcessor.cs
@@ -18,12 +18,6 @@ namespace Tangerine.UI.Timeline
 					if (!wasPressed) {
 						yield return 0.2;
 					}
-					var s = TimelineMetrics.DefaultRowHeight;
-					if (rollWidget.LocalMousePosition().Y > timeline.Roll.RootWidget.Height - s / 2) {
-						timeline.OffsetY += s;
-					} else if (rollWidget.LocalMousePosition().Y < s / 2) {
-						timeline.OffsetY -= s;
-					}
 					Window.Current.Invalidate();
 					wasPressed = true;
 				} else {

Tangerine/Tangerine.UI.Timeline/Components/RollBoneView.cs
@@ -46,7 +46,9 @@ namespace Tangerine.UI.Timeline
 							boneData.Bone.Parent.AsWidget.Nodes.OfType<Bone>().ToList(),
 							boneData.ChildrenExpanded);
 					}
-
+					if (boneData.ChildrenExpanded) {
+						Timeline.Instance.EnsureRowChildsVisible(row);
+					}
 					Document.Current.History.CommitTransaction();
 				}
 			};

Tangerine/Tangerine.UI.Timeline/Components/RollFolderView.cs
@@ -122,7 +122,12 @@ namespace Tangerine.UI.Timeline.Components
 				() => folder.Items.Count != 0,
 				i => button.Visible = folder.Items.Count != 0
 			);
-			button.AddTransactionClickHandler(() => Core.Operations.SetProperty.Perform(folder, nameof(Folder.Expanded), !folder.Expanded));
+			button.AddTransactionClickHandler(() => {
+				Core.Operations.SetProperty.Perform(folder, nameof(Folder.Expanded), !folder.Expanded);
+				if (folder.Expanded) {
+					Timeline.Instance.EnsureRowChildsVisible(row);
+				}
+			});
 			return button;
 		}
 

Tangerine/Tangerine.UI.Timeline/Components/RollNodeView.cs
@@ -233,6 +233,9 @@ namespace Tangerine.UI.Timeline.Components
 			);
 			button.AddTransactionClickHandler(() => {
 				Core.Operations.SetProperty.Perform(nodeData, nameof(NodeRow.Expanded), !nodeData.Expanded, isChangingDocument: false);
+				if (nodeData.Expanded) {
+					Timeline.Instance.EnsureRowChildsVisible(row);
+				}
 			});
 			return button;
 		}

Tangerine/Tangerine.UI.Timeline/Components/RollPropertyView.cs
@@ -50,7 +50,12 @@ namespace Tangerine.UI.Timeline.Components
 			var s = propRow.Animator.EditorState();
 			button.AddChangeWatcher(() => s.CurvesShown,
 				i => button.Texture = IconPool.GetTexture(i ? "Timeline.Expanded" : "Timeline.Collapsed"));
-			button.Clicked += () => Core.Operations.SetProperty.Perform(s, nameof(AnimatorEditorState.CurvesShown), !s.CurvesShown);
+			button.Clicked += () => {
+				Core.Operations.SetProperty.Perform(s, nameof(AnimatorEditorState.CurvesShown), !s.CurvesShown);
+				if (s.CurvesShown) {
+					Timeline.Instance.EnsureRowChildsVisible(row);
+				}
+			};
 			return button;
 		}
 

Tangerine/Tangerine.UI.Timeline/Timeline.cs
@@ -210,6 +210,27 @@ namespace Tangerine.UI.Timeline
 			}
 		}
 
+		public void EnsureRowChildsVisible(Row row)
+		{
+			var first = row.RollWidget();
+			var lastRow = row.Rows.Last();
+			while (lastRow.Rows.Count > 0) {
+				lastRow = lastRow.Rows.Last();
+			}
+			var last = lastRow.RollWidget();
+			float bottom = last.Bottom();
+			float top = first.Top();
+			float d = bottom - top;
+			float height = Roll.RootWidget.Height;
+			if (d > height) {
+				OffsetY = top;
+			} else if (bottom > OffsetY + height) {
+				OffsetY += bottom - OffsetY - height;
+			} else if (top < OffsetY) {
+				OffsetY = top;
+			}
+		}
+
 		public bool IsColumnVisible(int col)
 		{
 			var pos = col * TimelineMetrics.ColWidth - Offset.X;

commit bf04293997ef4b79a99f17418f7d0657a1ded447

[Tan][CIT-881] fix timeline roll scroll by dragging mouse outside of timeline; regressed in 541148404465d5937928d45f58ffb28f5c3491b2 CIT-346

Tangerine/Tangerine.UI.Timeline/Processors/RollPane/RollMouseScrollProcessor.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Lime;
 
@@ -17,6 +17,12 @@ namespace Tangerine.UI.Timeline
 					// To allow click on EnterButton when row is partly visible.
 					if (!wasPressed) {
 						yield return 0.2;
+					}
+					var s = TimelineMetrics.DefaultRowHeight;
+					if (rollWidget.LocalMousePosition().Y > timeline.Roll.RootWidget.Height - s / 2) {
+						timeline.OffsetY += s;
+					} else if (rollWidget.LocalMousePosition().Y < s / 2) {
+						timeline.OffsetY -= s;
 					}
 					Window.Current.Invalidate();
 					wasPressed = true;