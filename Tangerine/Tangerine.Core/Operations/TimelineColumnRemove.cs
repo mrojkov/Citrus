1088d008d91617f4a6308947c7ff963153aca4f7
Dmitry Terekhov <keltarhelviett@gmail.com>
2018-11-29 10:07:59 +1000

    Optimize timeline column remove

--- Tangerine/Tangerine.Core/Operations/TimelineColumnRemove.cs
+++ Tangerine/Tangerine.Core/Operations/TimelineColumnRemove.cs
@@ -3,63 +3,103 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lime;
 
 namespace Tangerine.Core.Operations
 {
-	public static class TimelineColumnRemove
+	public class TimelineColumnRemove : Operation
 	{
+		public override bool IsChangingDocument => true;
+
+		private readonly int column;
+		private int keyRemoveAt = -1;
+		private int markerRemoveAt = -1;
+
+		private TimelineColumnRemove(int column)
+		{
+			this.column = column;
+		}
+
 		public static void Perform(int column)
 		{
-			foreach (var node in Document.Current.Container.Nodes) {
-				var occupied = new HashSet<int>();
-				if (node.Animators.Count == 0) {
-					continue;
-				}
-				foreach (var animator in node.Animators.Where(i => i.AnimationId == Document.Current.AnimationId)) {
-					foreach (var k in animator.ReadonlyKeys) {
-						occupied.Add(k.Frame);
+			Document.Current.History.Perform(new TimelineColumnRemove(column));
+		}
+
+		public class Processor : OperationProcessor<TimelineColumnRemove>
+		{
+			protected override void InternalRedo(TimelineColumnRemove op)
+			{
+				int max;
+				foreach (var node in Document.Current.Container.Nodes) {
+					if (node.Animators.Count == 0) {
+						continue;
 					}
+					var occupied = new HashSet<int>();
+						max = 0;
+						foreach (var animator in node.Animators.Where(i => i.AnimationId == Document.Current.AnimationId)) {
+							foreach (var k in animator.ReadonlyKeys) {
+								occupied.Add(k.Frame);
+								max = k.Frame > max ? k.Frame : max;
+							}
+						}
+						op.keyRemoveAt = max + 1;
+						for (var i = op.column; ; ++i) {
+							if (!occupied.Contains(i)) {
+								op.keyRemoveAt = i;
+								break;
+							}
+						}
+					foreach (var animator in node.Animators.Where(i => i.AnimationId == Document.Current.AnimationId)) {
+						foreach (var k in animator.ReadonlyKeys) {
+							if (k.Frame != 0 && k.Frame >= op.keyRemoveAt) {
+								k.Frame -= 1;
+							}
+						}
+						animator.ResetCache();
+					}
+					node.Animators.Invalidate();
 				}
-				int removeAt = occupied.Max() + 1;
-				for (int i = column - 1; ; ++i) {
-					if (!occupied.Contains(i)) {
-						removeAt = i;
+				var markers = Document.Current.Animation.Markers;
+				if (markers.Count == 0) {
+					return;
+				}
+				var markersOccupied = new HashSet<int>();
+				max = 0;
+				foreach (var m in markers) {
+					markersOccupied.Add(m.Frame);
+					max = m.Frame > max ? m.Frame : max;
+				}
+				op.markerRemoveAt = max + 1;
+				for (var i = op.column; ; ++i) {
+					if (!markersOccupied.Contains(i)) {
+						op.markerRemoveAt = i;
 						break;
 					}
 				}
-				foreach (var animator in node.Animators.Where(i => i.AnimationId == Document.Current.AnimationId).ToList()) {
-					var keys = animator.ReadonlyKeys.ToList();
-					foreach (var k in keys) {
-						if (k.Frame != 0 && k.Frame >= removeAt) {
-							var k1 = k.Clone();
-							k1.Frame -= 1;
-							SetKeyframe.Perform(animator, k1);
-							RemoveKeyframe.Perform(animator, k.Frame);
-						}
+				foreach (var m in markers) {
+					if (m.Frame != 0 && m.Frame >= op.markerRemoveAt) {
+						m.Frame -= 1;
 					}
 				}
 			}
-			var markers = Document.Current.Animation.Markers.ToList();
-			if (markers.Count == 0) {
-				return;
-			}
-			var markersOccupied = new HashSet<int>();
-			foreach (var m in markers) {
-				markersOccupied.Add(m.Frame);
-			}
-			int markersRemoveAt = markersOccupied.Max() + 1;
-			for (int i = column - 1; ; ++i) {
-				if (!markersOccupied.Contains(i)) {
-					markersRemoveAt = i;
-					break;
+
+			protected override void InternalUndo(TimelineColumnRemove op)
+			{
+				foreach (var node in Document.Current.Container.Nodes) {
+					foreach (var animator in node.Animators.Where(i => i.AnimationId == Document.Current.AnimationId)) {
+						foreach (var k in animator.ReadonlyKeys.Reverse()) {
+							if (k.Frame >= op.keyRemoveAt) {
+								k.Frame += 1;
+							}
+						}
+						animator.ResetCache();
+					}
+					node.Animators.Invalidate();
 				}
-			}
-			foreach (var m in markers) {
-				if (m.Frame != 0 && m.Frame >= markersRemoveAt) {
-					var m1 = m.Clone();
-					m1.Frame -= 1;
-					DeleteMarker.Perform(m, false);
-					SetMarker.Perform(m1, false);
+				foreach (var m in Document.Current.Animation.Markers.Reverse()) {
+					if (m.Frame >= op.markerRemoveAt) {
+						m.Frame += 1;
+					}
 				}
 			}
 		}
