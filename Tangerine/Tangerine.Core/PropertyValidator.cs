0f540bbb39b51881654978c6fd59b805b444aa34
Mikhail Rozhkov <mrozhkov@game-forest.com>
2018-11-30 16:39:29 +1000

    [Tangerine] Various fixes for property validation.

--- Tangerine/Tangerine.Core/PropertyValidator.cs
+++ Tangerine/Tangerine.Core/PropertyValidator.cs
@@ -6,18 +6,15 @@ namespace Tangerine.Core
 {
 	public static class PropertyValidator
 	{
-		public static bool TryValidateValue(object value, Type type, string property)
+		public static bool ValidateValue(object value, Type type, string property)
 		{
-			if (value == null) {
-				return true;
-			}
 			var attr = PropertyAttributes<TangerineValidationAttribute>.Get(type, property);
 			return attr == null || attr.IsValid(value);
 		}
 
-		public static bool TryValidateValue(object value, PropertyInfo propertyInfo)
+		public static bool ValidateValue(object value, PropertyInfo propertyInfo)
 		{
-			return TryValidateValue(value, propertyInfo.DeclaringType, propertyInfo.Name);
+			return ValidateValue(value, propertyInfo.DeclaringType, propertyInfo.Name);
 		}
 	}
 }
