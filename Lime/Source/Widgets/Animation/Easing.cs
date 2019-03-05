07702385a794b795ea24494cfdc1684dd097fd10
Mike Rojkov <mikhail.rozhkov@gmail.com>
2019-03-05 18:49:42 +1000

    [Lime] Remove redudant comment.

--- Lime/Source/Widgets/Animation/Easing.cs
+++ Lime/Source/Widgets/Animation/Easing.cs
@@ -317,11 +317,6 @@ namespace Lime
 			return 1 - (f * f * f - f * Mathf.Sin(f * Mathf.Pi));
 		}
 
-		/// <summary>
-		/// Modeled after the piecewise overshooting cubic function:
-		/// y = (1/2)*((2x)^3-(2x)*sin(2*x*pi))           ; [0, 0.5)
-		/// y = (1/2)*(1-((1-x)^3-(1-x)*sin((1-x)*pi))+1) ; [0.5, 1]
-		/// </summary>
 		public static float BackEaseInOut(float p)
 		{
 			if (p < 0.5f) {
