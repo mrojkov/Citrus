a41a6debca0b09ddf75abeb8094eb5d36356fd4f
Mike Rojkov <mikhail.rozhkov@gmail.com>
2019-04-06 09:52:35 +1000

    [Tangerine][GridAnimationTrackView] Better clips and weight curve rendering

--- Tangerine/Tangerine.UI.Timeline/Components/GridAnimationTrackView.cs
+++ Tangerine/Tangerine.UI.Timeline/Components/GridAnimationTrackView.cs
@@ -1,5 +1,5 @@
-using Lime;
 using System;
+using Lime;
 using Tangerine.Core;
 using Tangerine.Core.Components;
 
@@ -16,16 +16,17 @@ namespace Tangerine.UI.Timeline.Components
 		public AwakeBehavior GridWidgetAwakeBehavior => GridWidget.Components.GetOrAdd<AwakeBehavior>();
 		public AwakeBehavior OverviewWidgetAwakeBehavior => OverviewWidget.Components.GetOrAdd<AwakeBehavior>();
 
-		private GridKeyframesRenderer keyframesRenderer = new GridKeyframesRenderer();
-
 		public GridAnimationTrackView(Row row)
 		{
 			this.row = row;
-			track = row.Components.Get<Core.Components.AnimationTrackRow>().Track;
+			track = row.Components.Get<AnimationTrackRow>().Track;
 			GridWidget = new Widget {
 				LayoutCell = new LayoutCell { StretchY = 0 },
 				MinHeight = TimelineMetrics.DefaultRowHeight,
 				Presenter = new SyncDelegatePresenter<Widget>(Render)
+			};
+			GridWidgetAwakeBehavior.Action += _ => {
+				GridWidget.AddChangeWatcher(() => CalcLabelsHashCode(track), __ => RefreshLabels(GridWidget, track));
 			};
 			OverviewWidget = new Widget {
 				LayoutCell = new LayoutCell { StretchY = 0 },
@@ -34,28 +35,129 @@ namespace Tangerine.UI.Timeline.Components
 			};
 		}
 
+		private static int CalcLabelsHashCode(AnimationTrack track)
+		{
+			unchecked {
+				var r = -511344;
+				foreach (var clip in track.Clips) {
+					r = r * -1521134295 + clip.AnimationIdComparisonCode;
+					r = r * -1521134295 + clip.BeginFrame;
+					r = r * -1521134295 + clip.EndFrame;
+				}
+				return r;
+			}
+		}
+
+		private void RefreshLabels(Widget widget, AnimationTrack track)
+		{
+			widget.Nodes.Clear();
+			foreach (var clip in track.Clips) {
+				widget.AddNode(new SimpleText {
+					Position = new Vector2((clip.BeginFrame + 0.5f) * TimelineMetrics.ColWidth, 0),
+					Size = new Vector2((clip.DurationInFrames + 0.5f) * TimelineMetrics.ColWidth, widget.Height),
+					Text = clip.AnimationId,
+					Padding = new Thickness(4),
+					VAlignment = VAlignment.Center,
+					OverflowMode = TextOverflowMode.Ellipsis,
+					Color = ColorTheme.Current.Basic.BlackText
+				});
+			}
+		}
+
 		private void Render(Widget widget)
 		{
 			widget.PrepareRendererState();
 			Renderer.DrawRect(Vector2.Zero, widget.ContentSize, ColorTheme.Current.TimelineGrid.PropertyRowBackground);
-			keyframesRenderer.ClearCells();
-			keyframesRenderer.GenerateCells(track.Animators, Document.Current.AnimationId);
-			keyframesRenderer.RenderCells(widget);
+			RenderClips(widget);
+			RenderBlendWeightCurve(widget);
+		}
+
+		private void RenderClips(Widget widget)
+		{
 			var spans = row.Components.GetOrAdd<GridSpanListComponent>().Spans.GetNonOverlappedSpans();
 			foreach (var clip in track.Clips) {
-				var a = new Vector2(clip.Begin * TimelineMetrics.ColWidth, 0);
-				var b = new Vector2(clip.End * TimelineMetrics.ColWidth, widget.Height);
+				var a = new Vector2((clip.BeginFrame + .5f) * TimelineMetrics.ColWidth + .5f, 0);
+				var b = new Vector2((clip.EndFrame + .5f) * TimelineMetrics.ColWidth + .5f, widget.Height);
 				Renderer.DrawRect(a, b, ColorTheme.Current.TimelineGrid.AnimationClip);
 				Renderer.DrawRectOutline(a, b, ColorTheme.Current.TimelineGrid.AnimationClipBorder);
 				if (clip.IsSelected) {
 					Renderer.DrawRect(a, b, ColorTheme.Current.TimelineGrid.Selection);
 					Renderer.DrawRectOutline(a, b, ColorTheme.Current.TimelineGrid.SelectionBorder);
 				}
-				var textHeight = Mathf.Min(Theme.Metrics.TextHeight, widget.Height);
-				a.X += 1;
-				a.Y = (widget.Height - textHeight) / 2;
-				Renderer.DrawTextLine(a, clip.AnimationId, textHeight, Color4.Black, 0);
 			}
+		}
+
+		private readonly Vertex[] vertices = new Vertex[4];
+
+		private void RenderBlendWeightCurve(Widget widget)
+		{
+			var color = ColorTheme.Current.TimelineGrid.AnimationTrackWeightCurve;
+			var a = CalcWeightCurvePoint(-1, track.Weight, widget.Height);
+			var b = new Vector2(widget.Width, a.Y);
+			if (track.Animators.TryFind<float>(nameof(AnimationTrack.Weight), out var animator, Document.Current.Animation.Id)) {
+				var first = true;
+				var func = KeyFunction.Steep;
+				foreach (var k in animator.ReadonlyKeys) {
+					b = CalcWeightCurvePoint(k.Frame, k.Value, widget.Height);
+					if (first) {
+						a = new Vector2(0, b.Y);
+						first = false;
+					}
+					var b2 = func == KeyFunction.Steep ? new Vector2(b.X, a.Y) : b;
+					DrawQuad(a, b2, widget.Height, color);
+					func = k.Function;
+					a = b;
+				}
+				foreach (var k in animator.ReadonlyKeys) {
+					var x = (k.Frame + .5f) * TimelineMetrics.ColWidth + .5f;
+					var h = widget.Height;
+					DrawTriangle(
+						new Vector2(x - 3, 0),
+						new Vector2(x + 3, 0),
+						new Vector2(x, 3),
+						ColorTheme.Current.TimelineGrid.AnimationTrackWeightCurveKey);
+					DrawTriangle(
+						new Vector2(x - 3, h),
+						new Vector2(x + 3, h),
+						new Vector2(x, h - 3),
+						ColorTheme.Current.TimelineGrid.AnimationTrackWeightCurveKey);
+				}
+			}
+			DrawQuad(a, new Vector2(widget.Width, b.Y), widget.Height, color);
+		}
+
+		private void DrawTriangle(Vector2 a, Vector2 b, Vector2 c, Color4 color)
+		{
+			vertices[0].Pos = a;
+			vertices[0].Color = color;
+			vertices[1].Pos = b;
+			vertices[1].Color = color;
+			vertices[2].Pos = c;
+			vertices[2].Color = color;
+			Renderer.DrawTriangleFan(vertices, 3);
+		}
+
+		private Vector2 CalcWeightCurvePoint(int frame, float weight, float widgetHeight)
+		{
+			return new Vector2(
+				(frame + .5f) * TimelineMetrics.ColWidth,
+				Mathf.Lerp(weight.Clamp(0, 100) * .01f, widgetHeight, 0));
+		}
+
+		private void DrawQuad(Vector2 prevKeyPos, Vector2 keyPos, float widgetHeight, Color4 color)
+		{
+			if (keyPos.Y >= widgetHeight && prevKeyPos.Y >= widgetHeight) {
+				return;
+			}
+			vertices[0].Color = color;
+			vertices[0].Pos = prevKeyPos;
+			vertices[1].Color = color;
+			vertices[1].Pos = keyPos;
+			vertices[2].Color = color;
+			vertices[2].Pos = new Vector2(keyPos.X, widgetHeight);
+			vertices[3].Color = color;
+			vertices[3].Pos = new Vector2(prevKeyPos.X, widgetHeight);
+			Renderer.DrawTriangleFan(vertices, 4);
 		}
 	}
 }
