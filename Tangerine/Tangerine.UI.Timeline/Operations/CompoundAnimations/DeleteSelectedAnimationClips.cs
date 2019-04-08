7c671b5cab4eb30b664988852006f7f58de25fc7
Mike Rojkov <mikhail.rozhkov@gmail.com>
2019-04-08 20:39:15 +1000

    [Tangerine] Clip deletion should delete overlapped weight keys either

--- Tangerine/Tangerine.UI.Timeline/Operations/CompoundAnimations/DeleteSelectedAnimationClips.cs
+++ Tangerine/Tangerine.UI.Timeline/Operations/CompoundAnimations/DeleteSelectedAnimationClips.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Lime;
 using Tangerine.Core;
+using Tangerine.Core.Operations;
 
 namespace Tangerine.UI.Timeline.Operations.CompoundAnimations
 {
@@ -23,10 +25,16 @@ namespace Tangerine.UI.Timeline.Operations.CompoundAnimations
 		{
 			Document.Current.History.DoTransaction(() => {
 				foreach (var track in Document.Current.Animation.Tracks) {
-					foreach (var clip in track.Clips.ToList()) {
-						if (clip.IsSelected) {
-							AnimationClipToolbox.RemoveClip(track, clip);
-						}
+					var clips = track.Clips.Where(i => i.IsSelected).ToList();
+					var keys = new List<IKeyframe>();
+					if (track.Animators.TryFind(nameof(AnimationTrack.Weight), out var weightAnimator, Document.Current.AnimationId)) {
+						keys = weightAnimator.ReadonlyKeys.Where(k => clips.Any(c => c.BeginFrame <= k.Frame && k.Frame <= c.EndFrame)).ToList();
+					}
+					foreach (var key in keys) {
+						RemoveKeyframe.Perform(weightAnimator, key.Frame);
+					}
+					foreach (var clip in clips) {
+						AnimationClipToolbox.RemoveClip(track, clip);
 					}
 				}
 			});
