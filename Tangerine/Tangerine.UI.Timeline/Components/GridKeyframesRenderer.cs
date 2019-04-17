457837fdaf2c5b160d1abe13271ec01fefdab667
Mike Rojkov <mikhail.rozhkov@gmail.com>
2019-04-17 14:29:56 +1000

    [Tangerine] Fix switch double indentation

--- Tangerine/Tangerine.UI.Timeline/Components/GridKeyframesRenderer.cs
+++ Tangerine/Tangerine.UI.Timeline/Components/GridKeyframesRenderer.cs
@@ -139,49 +139,49 @@ namespace Tangerine.UI.Timeline.Components
 			var segmentHeight = b.Y - a.Y;
 			switch (func) {
 				case KeyFunction.Linear: {
-						vertices[0].Pos = new Vector2(a.X, b.Y - 0.5f);
-						vertices[1].Pos = new Vector2(b.X, a.Y);
-						vertices[2].Pos = new Vector2(b.X, b.Y - 0.5f);
-						for (int i = 0; i < vertices.Length; i++) {
-							vertices[i].Color = color;
-						}
-						Renderer.DrawTriangleFan(vertices, numVertices: 3);
-						break;
+					vertices[0].Pos = new Vector2(a.X, b.Y - 0.5f);
+					vertices[1].Pos = new Vector2(b.X, a.Y);
+					vertices[2].Pos = new Vector2(b.X, b.Y - 0.5f);
+					for (int i = 0; i < vertices.Length; i++) {
+						vertices[i].Color = color;
 					}
+					Renderer.DrawTriangleFan(vertices, numVertices: 3);
+					break;
+				}
 				case KeyFunction.Steep: {
-						var leftSmallRectVertexA = new Vector2(a.X + 0.5f, a.Y + segmentHeight / 2);
-						var leftSmallRectVertexB = new Vector2(a.X + segmentWidth / 2, b.Y - 0.5f);
-						Renderer.DrawRect(leftSmallRectVertexA, leftSmallRectVertexB, color);
-						var rightBigRectVertexA = new Vector2(a.X + segmentWidth / 2, a.Y + 0.5f);
-						var rightBigRectVertexB = new Vector2(b.X, b.Y - 0.5f);
-						Renderer.DrawRect(rightBigRectVertexA, rightBigRectVertexB, color);
-						break;
-					}
+					var leftSmallRectVertexA = new Vector2(a.X + 0.5f, a.Y + segmentHeight / 2);
+					var leftSmallRectVertexB = new Vector2(a.X + segmentWidth / 2, b.Y - 0.5f);
+					Renderer.DrawRect(leftSmallRectVertexA, leftSmallRectVertexB, color);
+					var rightBigRectVertexA = new Vector2(a.X + segmentWidth / 2, a.Y + 0.5f);
+					var rightBigRectVertexB = new Vector2(b.X, b.Y - 0.5f);
+					Renderer.DrawRect(rightBigRectVertexA, rightBigRectVertexB, color);
+					break;
+				}
 				case KeyFunction.Spline: {
-						var numSegments = 5;
-						var center = new Vector2(a.X, b.Y - 0.5f);
-						vertices[0] = new Vertex { Pos = center, Color = color };
-						for (int i = 0; i < numSegments; i++) {
-							var r = Vector2.CosSin(i * Mathf.HalfPi / (numSegments - 1));
-							vertices[i + 1].Pos = new Vector2(
-								center.X + r.X * segmentWidth,
-								center.Y - r.Y * segmentHeight);
-							vertices[i + 1].Color = color;
-						}
-						Renderer.DrawTriangleFan(vertices, numSegments + 1);
-						break;
+					var numSegments = 5;
+					var center = new Vector2(a.X, b.Y - 0.5f);
+					vertices[0] = new Vertex { Pos = center, Color = color };
+					for (int i = 0; i < numSegments; i++) {
+						var r = Vector2.CosSin(i * Mathf.HalfPi / (numSegments - 1));
+						vertices[i + 1].Pos = new Vector2(
+							center.X + r.X * segmentWidth,
+							center.Y - r.Y * segmentHeight);
+						vertices[i + 1].Color = color;
 					}
+					Renderer.DrawTriangleFan(vertices, numSegments + 1);
+					break;
+				}
 				case KeyFunction.ClosedSpline: {
-						var circleCenter = new Vector2(a.X + segmentWidth / 2, a.Y + segmentHeight / 2);
-						var circleRadius = 0f;
-						if (segmentWidth < segmentHeight) {
-							circleRadius = circleCenter.X - a.X - 0.5f;
-						} else {
-							circleRadius = circleCenter.Y - a.Y - 0.5f;
-						}
-						Renderer.DrawRound(circleCenter, circleRadius, numSegments: 6, color);
-						break;
+					var circleCenter = new Vector2(a.X + segmentWidth / 2, a.Y + segmentHeight / 2);
+					var circleRadius = 0f;
+					if (segmentWidth < segmentHeight) {
+						circleRadius = circleCenter.X - a.X - 0.5f;
+					} else {
+						circleRadius = circleCenter.Y - a.Y - 0.5f;
 					}
+					Renderer.DrawRound(circleCenter, circleRadius, numSegments: 6, color);
+					break;
+				}
 			}
 		}
 	}
