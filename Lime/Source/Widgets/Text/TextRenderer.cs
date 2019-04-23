db528a1acdaa79a4b27112081dc87429e68e9a25
Igor Fomin <bytecat@mail.ru>
2019-04-23 18:00:49 +1000

    [Lime] [Text] Fix line break after bullet in RichText

--- Lime/Source/Widgets/Text/TextRenderer.cs
+++ Lime/Source/Widgets/Text/TextRenderer.cs
@@ -376,7 +376,7 @@ namespace Lime.Text
 					word.Start == 0 &&
 					t.Length > 0 &&
 					t[word.Start] > ' ' &&
-					GetLastChar(texts[fittedWords[i - 1].TextIndex]) > ' ';
+					(GetLastChar(texts[fittedWords[i - 1].TextIndex]) > ' ' || IsBullet(fittedWords[i - 1]));
 				if (isLongerThanWidth && isTextOrBullet && (wordSplitAllowed || t.HasJapaneseSymbols(word.Start, word.Length))) {
 					var fittedCharsCount = CalcFittedCharactersCount(word, maxWidth - x);
 					if (fittedCharsCount > 0) {
