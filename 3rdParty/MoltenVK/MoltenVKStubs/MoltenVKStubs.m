b4bc14206257b80b41c3223fd50d1967bdfd057e
Mikhail Rozhkov <mrozhkov@game-forest.com>
2019-05-28 17:53:12 +1000

    [Lime][iOS] Update shader compiler

---
+++ 3rdParty/MoltenVK/MoltenVKStubs/MoltenVKStubs.m
@@ -0,0 +1,13 @@
+//
+//  MoltenVKStubs.m
+//  MoltenVKStubs
+//
+//  Created by Mikhail Rozhkov on 1/31/19.
+//  Copyright © 2019 Mikhail Rozhkov. All rights reserved.
+//
+
+#import "MoltenVKStubs.h"
+
+@implementation MoltenVKStubs
+
+@end
