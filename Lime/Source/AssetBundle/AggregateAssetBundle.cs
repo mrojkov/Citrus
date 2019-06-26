841e781dd6c9025b1f652806bec98c7452dbfd8f
Grigory Aleksanin <galeksanin@game-forest.com>
2019-06-26 15:29:24 +1000

    [Lime][BRNZ-1304] Fix race condition in AggregateAssetBundle

--- Lime/Source/AssetBundle/AggregateAssetBundle.cs
+++ Lime/Source/AssetBundle/AggregateAssetBundle.cs
@@ -2,72 +2,114 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 
 namespace Lime
 {
 	public class AggregateAssetBundle : AssetBundle
 	{
 		private readonly List<AssetBundle> bundles = new List<AssetBundle>();
+		private ReaderWriterLockSlim sync = new ReaderWriterLockSlim();
 
 		public AggregateAssetBundle(params AssetBundle[] bundles)
 		{
-			this.bundles.AddRange(bundles);
+			sync.EnterWriteLock();
+			try {
+				this.bundles.AddRange(bundles);
+			} finally {
+				sync.ExitWriteLock();
+			}
 		}
 
 		public void Attach(AssetBundle bundle)
 		{
-			bundles.Add(bundle);
+			sync.EnterWriteLock();
+			try {
+				bundles.Add(bundle);
+			} finally {
+				sync.ExitWriteLock();
+			}
 		}
 
 		public void Detach(AssetBundle bundle)
 		{
-			bundles.Remove(bundle);
+			sync.EnterWriteLock();
+			try {
+				bundles.Remove(bundle);
+			} finally {
+				sync.ExitWriteLock();
+			}
 		}
 
 		public override Stream OpenFile(string path)
 		{
-			foreach (var bundle in bundles) {
-				if (bundle.FileExists(path)) {
-					return bundle.OpenFile(path);
+			sync.EnterReadLock();
+			try {
+				foreach (var bundle in bundles) {
+					if (bundle.FileExists(path)) {
+						return bundle.OpenFile(path);
+					}
 				}
+			} finally {
+				sync.ExitReadLock();
 			}
 			throw new FileNotFoundException($"File {path} not found in aggregate asset bundle.");
 		}
 
 		public override void Dispose()
 		{
-			foreach (var bundle in bundles) {
-				bundle.Dispose();
+			sync.EnterWriteLock();
+			try {
+				foreach (var bundle in bundles) {
+					bundle.Dispose();
+				}
+				bundles.Clear();
+			} finally {
+				sync.ExitWriteLock();
 			}
-			bundles.Clear();
 		}
 
 		public override DateTime GetFileLastWriteTime(string path)
 		{
-			foreach (var bundle in bundles) {
-				if (bundle.FileExists(path)) {
-					return bundle.GetFileLastWriteTime(path);
+			sync.EnterReadLock();
+			try {
+				foreach (var bundle in bundles) {
+					if (bundle.FileExists(path)) {
+						return bundle.GetFileLastWriteTime(path);
+					}
 				}
+			} finally {
+				sync.ExitReadLock();
 			}
 			throw new InvalidOperationException($"Path {path} not found in aggregate asset bundle.");
 		}
 
 		public override byte[] GetCookingRulesSHA1(string path)
 		{
-			foreach (var bundle in bundles) {
-				if (bundle.FileExists(path)) {
-					return bundle.GetCookingRulesSHA1(path);
+			sync.EnterReadLock();
+			try {
+				foreach (var bundle in bundles) {
+					if (bundle.FileExists(path)) {
+						return bundle.GetCookingRulesSHA1(path);
+					}
 				}
+			} finally {
+				sync.ExitReadLock();
 			}
 			throw new InvalidOperationException($"Path {path} not found in aggregate asset bundle.");
 		}
 
 		public override int GetFileSize(string path)
 		{
-			foreach (var bundle in bundles) {
-				if (bundle.FileExists(path)) {
-					return bundle.GetFileSize(path);
+			sync.EnterReadLock();
+			try {
+				foreach (var bundle in bundles) {
+					if (bundle.FileExists(path)) {
+						return bundle.GetFileSize(path);
+					}
 				}
+			} finally {
+				sync.ExitReadLock();
 			}
 			throw new InvalidOperationException($"Path {path} not found in aggregate asset bundle.");
 		}
@@ -79,10 +121,15 @@ namespace Lime
 
 		public override bool FileExists(string path)
 		{
-			foreach (var bundle in bundles) {
-				if (bundle.FileExists(path)) {
-					return true;
+			sync.EnterReadLock();
+			try {
+				foreach (var bundle in bundles) {
+					if (bundle.FileExists(path)) {
+						return true;
+					}
 				}
+			} finally {
+				sync.ExitReadLock();
 			}
 			return false;
 		}
@@ -95,7 +142,12 @@ namespace Lime
 
 		public override IEnumerable<string> EnumerateFiles(string path = null)
 		{
-			return bundles.SelectMany(bundle => bundle.EnumerateFiles(path));
+			sync.EnterReadLock();
+			try {
+				return bundles.SelectMany(bundle => bundle.EnumerateFiles(path));
+			} finally {
+				sync.ExitReadLock();
+			}
 		}
 	}
 }
