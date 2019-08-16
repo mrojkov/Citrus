3ab89378fd006a9faefa2365afe8ce9fb49aacf2
Dmitry Terekhov <keltarhelviett@gmail.com>
2019-08-16 11:50:59 +1000

    [CIT-1278] Update SharpFont; Add HarfBuzz; Fix whitespace rendering

--- Lime/Extensions/DynamicFont/FontRenderer.cs
+++ Lime/Extensions/DynamicFont/FontRenderer.cs
@@ -91,7 +91,7 @@ namespace Lime
 			var bearingX = (float) face.Glyph.Metrics.HorizontalBearingX;
 			bool rgbIntensity = bitmap.PixelMode == PixelMode.Lcd || bitmap.PixelMode == PixelMode.VerticalLcd;
 			var glyph = new Glyph {
-				Pixels = bitmap.BufferData,
+				Pixels = char.IsWhiteSpace(@char) ? new byte[0] : bitmap.BufferData,
 				RgbIntensity = rgbIntensity,
 				Pitch = bitmap.Pitch,
 				Width = rgbIntensity ? bitmap.Width / 3 : bitmap.Width,
