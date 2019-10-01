901d0871c31ef9e7aa178dfc1659639211363815
Dmitry Terekhov <keltarhelviett@gmail.com>
2019-10-01 18:04:54 +1000

    [CIT-1359][Tangerine] Fix git conflicts checking

--- Orange/Source/AssetCooker/CookStages/SyncUpdated.cs
+++ Orange/Source/AssetCooker/CookStages/SyncUpdated.cs
@@ -23,7 +23,8 @@ namespace Orange
 				needUpdate = needUpdate || !srcRules.SHA1.SequenceEqual(bundle.GetCookingRulesSHA1(dstPath));
 				needUpdate = needUpdate || (extraOutOfDateChecker?.Invoke(srcPath, dstPath) ?? false);
 				if (needUpdate) {
-					if (Git.HasConflicts(AssetPath.Combine(The.Workspace.AssetsDirectory, srcPath))) {
+					var fullPath = AssetPath.Combine(The.Workspace.AssetsDirectory, srcPath);
+					if (File.Exists(fullPath) && Git.HasConflicts(fullPath)) {
 						throw new InvalidOperationException($"{srcPath} has git conflicts.");
 					}
 					if (converter != null) {
