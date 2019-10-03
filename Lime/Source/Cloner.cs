553d1cf4a7f047ffe820d7a3b933b02da90a3786
kegorov <kostonke@mail.ru>
2019-10-03 13:40:23 +1000

    [Lime] Add cloning related summary

--- Lime/Source/Cloner.cs
+++ Lime/Source/Cloner.cs
@@ -2,10 +2,26 @@ using System;
 
 namespace Lime
 {
+	/// <summary>
+	/// Utility class that contain methods for cloning objects.
+	/// </summary>
 	public static class Cloner
 	{
+		/// <summary>
+		/// Clone the object. If source object implements ICloneable then the method will return object produced by ICloneable.Clone().
+		/// Otherwise Serialization.Clone() will be used.
+		/// </summary>
+		/// <typeparam name="T">A type of object that need to be returned.</typeparam>
+		/// <param name="obj">A source object to clone.</param>
+		/// <returns></returns>
 		public static T Clone<T>(T obj) => (T)Clone((object)obj);
 
+		/// <summary>
+		/// Clone the object. If source object implements ICloneable then the method will return object produced by ICloneable.Clone().
+		/// Otherwise Serialization.Clone() will be used.
+		/// </summary>
+		/// <param name="obj">A source object to clone.</param>
+		/// <returns></returns>
 		public static object Clone(object obj)
 		{
 			return obj is ICloneable cloneable ? cloneable.Clone() : Serialization.Clone(obj);
--- Lime/Source/Serialization.cs
+++ Lime/Source/Serialization.cs
@@ -280,7 +280,20 @@ namespace Lime
 		public static int CalcObjectCheckSum<T>(string path, T obj) => Yuzu.Instance.Value.CalcObjectCheckSum(path, obj);
 		public static string GetCurrentSerializationPath() => Yuzu.Instance.Value.GetCurrentSerializationPath();
 		public static bool CheckBinarySignature(Stream s) => Yuzu.Instance.Value.CheckBinarySignature(s);
+
+		/// <summary>
+		/// Clone object using serialization scheme.
+		/// </summary>
+		/// <typeparam name="T">A type of object that need to be returned.</typeparam>
+		/// <param name="obj">A source object to clone.</param>
+		/// <returns></returns>
 		public static T Clone<T>(T obj) => (T)Clone((object)obj);
+
+		/// <summary>
+		/// Clone object using serialization scheme.
+		/// </summary>
+		/// <param name="obj">A source object to clone.</param>
+		/// <returns></returns>
 		public static object Clone(object obj) => Yuzu.Instance.Value.Clone(obj);
 	}
 }
