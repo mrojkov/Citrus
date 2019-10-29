15f7c6c7ef1cec10303fc0a16ae67e0230771e74
Dmitry Terekhov <keltarhelviett@gmail.com>
2019-10-29 14:30:33 +1000

    [CIT-1373][Lime] Update UVs in Wave\Twist material on texture change

--- Lime/Source/Widgets/Materials/TwistComponent.cs
+++ Lime/Source/Widgets/Materials/TwistComponent.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Yuzu;
 
 namespace Lime
@@ -22,12 +23,37 @@ namespace Lime
 		protected override void OnOwnerChanged(Node oldOwner)
 		{
 			base.OnOwnerChanged(oldOwner);
+			if (oldOwner != null && updateUVOnTextureChangeTask != null) {
+				oldOwner.Tasks.Remove(updateUVOnTextureChangeTask);
+			}
 			if (Owner != null) {
 				var image = (Image)Owner;
-				CustomMaterial.UV0 = image.UV0;
-				CustomMaterial.UV1 = image.UV1;
-				image.Texture.TransformUVCoordinatesToAtlasSpace(ref CustomMaterial.UV0);
-				image.Texture.TransformUVCoordinatesToAtlasSpace(ref CustomMaterial.UV1);
+				updateUVOnTextureChangeTask = image.Tasks.Add(UpdateUVOnTextureChange(image));
+				UpdateUV(image);
+			}
+		}
+
+		private void UpdateUV(Image owner)
+		{
+			var uv0 = owner.UV0;
+			var uv1 = owner.UV1;
+			owner.Texture.TransformUVCoordinatesToAtlasSpace(ref uv0);
+			owner.Texture.TransformUVCoordinatesToAtlasSpace(ref uv1);
+			CustomMaterial.UV0 = uv0;
+			CustomMaterial.UV1 = uv1;
+		}
+
+		private Task updateUVOnTextureChangeTask;
+
+		private IEnumerator<object> UpdateUVOnTextureChange(Image owner)
+		{
+			ITexture texture = null;
+			while (true) {
+				if (owner.Texture != texture) {
+					texture = owner.Texture;
+					UpdateUV(owner);
+				}
+				yield return null;
 			}
 		}
 	}
--- Lime/Source/Widgets/Materials/WaveComponent.cs
+++ Lime/Source/Widgets/Materials/WaveComponent.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using Yuzu;
 
 namespace Lime
@@ -82,14 +85,37 @@ namespace Lime
 		protected override void OnOwnerChanged(Node oldOwner)
 		{
 			base.OnOwnerChanged(oldOwner);
+			if (oldOwner != null && updateUVOnTextureChangeTask != null) {
+				oldOwner.Tasks.Remove(updateUVOnTextureChangeTask);
+			}
 			if (Owner != null) {
 				var image = (Image)Owner;
-				var uv0 = image.UV0;
-				var uv1 = image.UV1;
-				image.Texture.TransformUVCoordinatesToAtlasSpace(ref uv0);
-				image.Texture.TransformUVCoordinatesToAtlasSpace(ref uv1);
-				CustomMaterial.UV0 = uv0;
-				CustomMaterial.UV1 = uv1;
+				updateUVOnTextureChangeTask = image.Tasks.Add(UpdateUVOnTextureChange(image));
+				UpdateUV(image);
+			}
+		}
+
+		private void UpdateUV(Image owner)
+		{
+			var uv0 = owner.UV0;
+			var uv1 = owner.UV1;
+			owner.Texture.TransformUVCoordinatesToAtlasSpace(ref uv0);
+			owner.Texture.TransformUVCoordinatesToAtlasSpace(ref uv1);
+			CustomMaterial.UV0 = uv0;
+			CustomMaterial.UV1 = uv1;
+		}
+
+		private Task updateUVOnTextureChangeTask;
+
+		private IEnumerator<object> UpdateUVOnTextureChange(Image owner)
+		{
+			ITexture texture = null;
+			while (true) {
+				if (owner.Texture != texture) {
+					texture = owner.Texture;
+					UpdateUV(owner);
+				}
+				yield return null;
 			}
 		}
 	}
