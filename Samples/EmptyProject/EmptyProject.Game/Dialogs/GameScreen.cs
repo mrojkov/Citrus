dcf463ee06c9b68a367a90e3c38a1029ab3215a4
Denis Rotanov <mapholameth@gmail.com>
2019-11-25 17:01:15 +1000

    [TestProject] temp testbed to play around with new gestures while migrating them

--- Samples/EmptyProject/EmptyProject.Game/Dialogs/GameScreen.cs
+++ Samples/EmptyProject/EmptyProject.Game/Dialogs/GameScreen.cs
@@ -4,10 +4,50 @@ namespace EmptyProject.Dialogs
 {
 	public class GameScreen : Dialog<Scenes.GameScreen>
 	{
+		Widget thumb;
+		DragGesture dragGesture;
+		PinchGesture pinchGesture;
+
 		public GameScreen()
 		{
+			//new KineticDragGesture(new DeceleratingKineticMotionStrategy(0.97f, 1.002f)),
+			//new PinchGesture(exclusive: true)
 			SoundManager.PlayMusic("Ingame");
 			Scene._BtnExit.It.Clicked = ReturnToMenu;
+			thumb = Root["Image"];
+			thumb.HitTestTarget = true;
+			thumb.Gestures.Add(new ClickGesture(() => {
+				thumb.Parent.RunAnimation("Click");
+			}));
+			thumb.Gestures.Add(new LongTapGesture(1.0f, () => {
+				thumb.Parent.RunAnimation("LongTap");
+			}));
+			thumb.Gestures.Add(new DoubleClickGesture(() => {
+				thumb.Parent.RunAnimation("Tap");
+			}));
+			dragGesture = new SpecialDragGesture(
+				new SpecialDragGesture.DampingMotionStrategy(0.97f, 0.998f)
+			//new SpecialDragGesture.FixedTimeMotionStrategy(0.5f)
+			);
+			thumb.Gestures.Add(dragGesture);
+			dragGesture.Changed += OnDragged;
+			pinchGesture.Changed += OnPinched;
+		}
+
+		private void OnDragged()
+		{
+			if (dragGesture.IsActive) {
+				thumb.Position += dragGesture.LastDragDistance / thumb.Scale;
+			}
+		}
+
+		private void OnPinched()
+		{
+			if (pinchGesture.IsActive) {
+				thumb.Position -= pinchGesture.LastDragDistance / thumb.Scale;
+				var zoom = Mathf.Clamp(thumb.Scale * pinchGesture.LastPinchScale, Vector2.One * 0.2f, Vector2.One * 3.0f);
+				thumb.Scale = zoom;
+			}
 		}
 
 		protected override void Update(float delta)
@@ -15,6 +55,9 @@ namespace EmptyProject.Dialogs
 			if (Input.WasKeyPressed(Key.Escape)) {
 				ReturnToMenu();
 			}
+
+			thumb.X = Mathf.Clamp(thumb.X, 0, The.World.Width);
+			thumb.Y = Mathf.Clamp(thumb.Y, 0, The.World.Height);
 		}
 
 		protected override bool HandleAndroidBackButton()
